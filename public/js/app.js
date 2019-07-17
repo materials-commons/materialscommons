c25b1c5aaf75ceb6e4798a54f4d8bfa220d83978
gtarcea <glenn.tarcea@gmail.com>
2019-07-17 05:56:58 -0400

    Add pipe-dream

--- public/js/app.js
+++ public/js/app.js
@@ -47806,8 +47806,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/gtarcea/workspace/src/bitbucket.org/a2sdl/epistimi/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/gtarcea/workspace/src/bitbucket.org/a2sdl/epistimi/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/gtarcea/workspace/src/github.com/materials-commons/materialscommons/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/gtarcea/workspace/src/github.com/materials-commons/materialscommons/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,8 +30,12 @@
 
 <body>
 <nav class="navbar navbar-dark fixed-top bg-nav p-0 shadow">
-    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="{{route('dashboard.index')}}">illuminate</a>
-    <div class="d-flex justify-content-end">
+    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="{{route('dashboard.index')}}">MaterialsCommons</a>
+{{--    <div class="d-flex col-sm-8 justify-content-end">--}}
+{{--        <a class="nav-link action col-sm-3 col-md-2 mr-0" href="{{route('dashboard.index')}}">Projects</a>--}}
+{{--        <a class="nav-link action col-sm-3 col-md-2 mr-0" href="{{route('dashboard.index')}}">Stuff</a>--}}
+{{--    </div>--}}
+    <div class="d-flex justify-content-endx">
         <input class="form-control w-75 form-rounded" type="text" placeholder="Search" aria-label="Search">
 
         <ul class="navbar-nav px-3">
@@ -51,44 +55,44 @@
             <div class="sidebar-sticky">
                 <ul class="nav flex-column mt-3">
                     <li class="nav-item">
-                        <a class="nav-link {{setActiveNav('dashboard')}}" href="{{route('dashboard.index')}}">
+                        <a class="nav-link fs-11 {{setActiveNav('dashboard')}}" href="{{route('dashboard.index')}}">
                             <span data-feather="home"></span>
                             <i class="fa-fw fas fa-binoculars mr-2"></i>
                             Dashboard
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link {{setActiveNav('teams')}}" href="{{route('teams.index')}}">
+                        <a class="nav-link fs-11 {{setActiveNav('teams')}}" href="{{route('teams.index')}}">
                             <i class="fa-fw fas fa-address-book mr-2 {{setActiveNav('teams')}}"></i>
                             Teams
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link {{setActiveNav('projects')}}" href="{{route('projects.index')}}">
+                        <a class="nav-link fs-11 {{setActiveNav('projects')}}" href="{{route('projects.index')}}">
                             <i class="fa-fw fas fa-project-diagram mr-2 "></i>
                             Projects
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link {{setActiveNav('tasks')}}" href="{{route('tasks.index')}}">
+                        <a class="nav-link fs-11 {{setActiveNav('tasks')}}" href="{{route('tasks.index')}}">
                             <i class="fa-fw fas fa-tasks mr-2"></i>
                             Tasks
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link {{setActiveNav('files')}}" href="{{route('files.index')}}">
+                        <a class="nav-link fs-11 {{setActiveNav('files')}}" href="{{route('files.index')}}">
                             <i class="fa-fw fas fa-folder mr-2 {{setActiveNav('files')}}"></i>
                             Files
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link {{setActiveNav('users')}}" href="{{route('users.index')}}">
+                        <a class="nav-link fs-11 {{setActiveNav('users')}}" href="{{route('users.index')}}">
                             <i class="fa-fw fas fa-users-cog mr-2 {{setActiveNav('users')}}"></i>
                             Users
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link {{setActiveNav('settings')}}" href="{{route('settings.index')}}">
+                        <a class="nav-link fs-11 {{setActiveNav('settings')}}" href="{{route('settings.index')}}">
                             <i class="fa-fw fas fa-cogs mr-2 {{setActiveNav('settings')}}"></i>
                             Settings
                         </a>
@@ -96,17 +100,17 @@
                 </ul>
 
                 <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-                    <span>Saved Reports</span>
+                    <span class="fs-11">Saved Reports</span>
                     <a class="d-flex align-items-center text-muted" href="#">
                     </a>
                 </h6>
 
                 <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-                    <span>Saved Searches</span>
+                    <span class="fs-11">Saved Searches</span>
                 </h6>
 
                 <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-                    <span>Quick Access</span>
+                    <span class="fs-11">Quick Access</span>
                 </h6>
                 {{--<ul class="nav flex-column mb-2">--}}
                 {{--<li class="nav-item">--}}
