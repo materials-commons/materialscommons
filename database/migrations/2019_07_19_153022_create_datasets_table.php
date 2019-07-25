cc1964d402462fc2096f75a74d6818cbeef047fe
gtarcea <glenn.tarcea@gmail.com>
2019-07-25 12:48:54 -0400

    Start adding additional fields to datasets

--- database/migrations/2019_07_19_153022_create_datasets_table.php
+++ database/migrations/2019_07_19_153022_create_datasets_table.php
@@ -17,10 +17,41 @@ class CreateDatasetsTable extends Migration
             $table->bigIncrements('id');
             $table->uuid('uuid');
             $table->string('name');
+            $table->string('license');
+            $table->funding('text');
+            $table->text('description');
+            $table->string('doi');
+            $table->date('published_on');
+            $table->date('privately_published_on');
+
             $table->text('description');
             $table->timestamps();
         });
     }
+
+    /*
+     * "authors": [ ],
+"birthtime": Wed Jul 24 2019 14:55:03 GMT+00:00 ,
+"description":  "" ,
+"doi":  "" ,
+"embargo_date":  "" ,
+"funding":  "" ,
+"id":  "b3c5b38b-23e7-44cb-9caf-4b8b2091baaa" ,
+"institution":  "" ,
+"keywords": [ ],
+"license": {
+"link":  "" ,
+"name":  ""
+} ,
+"mtime": Wed Jul 24 2019 14:55:03 GMT+00:00 ,
+"otype":  "dataset" ,
+"owner": test@test.mc, »
+"papers": [ ],
+"published": false ,
+"published_date": Wed Jul 24 2019 14:55:03 GMT+00:00 ,
+"selection_id":  "272a6a2b-1dd1-4493-93d0-0923c4239f35" ,
+"title":  "45b94927-56be-4c91-ae65-a2f7647f0c58"
+     */
 
     /**
      * Reverse the migrations.
