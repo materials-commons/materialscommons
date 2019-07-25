a6c2a0037be5da0c2e53baa1e59e92fdd315e034
gtarcea <glenn.tarcea@gmail.com>
2019-07-25 14:46:55 -0400

    Remove example json

--- database/migrations/2019_07_19_153022_create_datasets_table.php
+++ database/migrations/2019_07_19_153022_create_datasets_table.php
@@ -42,30 +42,6 @@ class CreateDatasetsTable extends Migration
         });
     }
 
-    /*
-     * "authors": [ ],
-"birthtime": Wed Jul 24 2019 14:55:03 GMT+00:00 ,
-"description":  "" ,
-"doi":  "" ,
-"embargo_date":  "" ,
-"funding":  "" ,
-"id":  "b3c5b38b-23e7-44cb-9caf-4b8b2091baaa" ,
-"institution":  "" ,
-"keywords": [ ],
-"license": {
-"link":  "" ,
-"name":  ""
-} ,
-"mtime": Wed Jul 24 2019 14:55:03 GMT+00:00 ,
-"otype":  "dataset" ,
-"owner": test@test.mc, »
-"papers": [ ],
-"published": false ,
-"published_date": Wed Jul 24 2019 14:55:03 GMT+00:00 ,
-"selection_id":  "272a6a2b-1dd1-4493-93d0-0923c4239f35" ,
-"title":  "45b94927-56be-4c91-ae65-a2f7647f0c58"
-     */
-
     /**
      * Reverse the migrations.
      *
