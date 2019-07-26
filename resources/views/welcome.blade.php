c05e6fbc0bf52ae226338eebe28530b8ba5aa29d
gtarcea <glenn.tarcea@gmail.com>
2019-07-26 12:49:22 -0400

    Home goes to app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,7 +73,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/app/dashboard') }}">Home</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
