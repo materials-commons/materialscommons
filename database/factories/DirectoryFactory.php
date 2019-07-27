a5acfe1f33db733fe6aad2663a16f1d4809ff238
gtarcea <glenn.tarcea@gmail.com>
2019-07-27 17:37:56 -0400

    Initial model, migration and factory for directories

---
+++ database/factories/DirectoryFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Directory;
+use Faker\Generator as Faker;
+
+$factory->define(Directory::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
