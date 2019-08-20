4fd342e0ea7fe4b98f79cd5868bb52ffb88b1546
gtarcea <glenn.tarcea@gmail.com>
2019-08-19 20:53:28 -0400

    Fix logout

--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,7 @@ Route::get('/', function () {
 
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('login', [LoginController::class, 'login']);
-Route::get('logout', [LoginController::class, 'logout'])->name('logout');
+Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('register', [RegisterController::class, 'register']);
 Route::get('password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
