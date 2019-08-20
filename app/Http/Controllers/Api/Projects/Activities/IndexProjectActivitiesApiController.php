dc5fbb92b345e3afd089fdfb7668706bafaef9b6
gtarcea <glenn.tarcea@gmail.com>
2019-08-19 21:18:53 -0400

    Add controller for listing all activities in a project

---
+++ app/Http/Controllers/Api/Projects/Activities/IndexProjectActivitiesApiController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api\Projects\Activities;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class IndexProjectActivitiesApiController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
