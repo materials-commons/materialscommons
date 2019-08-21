7b47f44519c7c68aa276cae06b2cf4121c257e40
gtarcea <glenn.tarcea@gmail.com>
2019-08-21 13:10:05 -0400

    Implement IndexProjects controller

--- app/Http/Controllers/Api/Projects/IndexProjectsApiController.php
+++ app/Http/Controllers/Api/Projects/IndexProjectsApiController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Api\Projects;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class IndexProjectsApiController extends Controller
 {
@@ -15,6 +17,13 @@ class IndexProjectsApiController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        $query = auth()->user()->projects()->getQuery();
+
+        $data = QueryBuilder::for($query)
+            ->allowedFilters('name', AllowedFilter::exact('project_id'))
+            ->withCount(['activities', 'entities', 'files'])
+            ->jsonPaginate();
+
+        return $data;
     }
 }
