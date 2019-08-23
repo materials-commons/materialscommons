7d795ee45ea075b5473d3aa44140531eb302d37d
gtarcea <glenn.tarcea@gmail.com>
2019-08-23 13:58:07 -0400

    Add UserCanAccessProject middleware to projects routes

--- routes/api_routes/projects/projects_api.php
+++ routes/api_routes/projects/projects_api.php
@@ -8,10 +8,13 @@ use App\Http\Controllers\Api\Projects\ShowProjectApiController;
 use App\Http\Controllers\Api\Projects\UpdateProjectApiController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/projects', CreateProjectApiController::class);
-Route::put('/projects/{project}', UpdateProjectApiController::class);
-Route::delete('/projects/{project}', DeleteProjectApiController::class);
-Route::get('/projects/{project}', ShowProjectApiController::class);
-Route::get('/projects', IndexProjectsApiController::class);
 
-Route::get('/projects/{project}/activities', IndexProjectActivitiesApiController::class);
+
+Route::middleware(\App\Http\Middleware\UserCanAccessProject::class)->group(function() {
+    Route::post('/projects', CreateProjectApiController::class);
+    Route::get('/projects', IndexProjectsApiController::class);
+    Route::put('/projects/{project}', UpdateProjectApiController::class);
+    Route::delete('/projects/{project}', DeleteProjectApiController::class);
+    Route::get('/projects/{project}', ShowProjectApiController::class);
+    Route::get('/projects/{project}/activities', IndexProjectActivitiesApiController::class);
+});
