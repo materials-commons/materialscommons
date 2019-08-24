262625dd240c105e1c5a548574aa757e0c0a10c1
gtarcea <glenn.tarcea@gmail.com>
2019-08-23 23:30:56 -0400

    Create the rest of the Resource and Request classes

---
+++ app/Http/Requests/Entities/UpdateEntityRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests\Entities;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Waavi\Sanitizer\Laravel\SanitizesInput;
+
+class UpdateEntityRequest extends FormRequest
+{
+    use SanitizesInput;
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     *  Validation rules to be applied to the input.
+     *
+     *  @return void
+     */
+    public function rules()
+    {
+        return [];
+    }
+
+    /**
+     *  Filters to be applied to the input.
+     *
+     *  @return void
+     */
+    public function filters()
+    {
+        return [];
+    }
+}
---
+++ app/Http/Resources/Directories/DirectoryResource.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources\Directories;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DirectoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
