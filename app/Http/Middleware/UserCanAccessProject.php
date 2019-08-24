f9e9ae27055a46cc0f9b39ec2af83bcc90616688
gtarcea <glenn.tarcea@gmail.com>
2019-08-23 22:48:53 -0400

    Use new GetRequestParameterId trait to replace getProjectId method

--- app/Http/Middleware/UserCanAccessProject.php
+++ app/Http/Middleware/UserCanAccessProject.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Middleware;
 
+use App\Traits\GetRequestParameterId;
 use Illuminate\Http\Request;
 use Closure;
 
 class UserCanAccessProject
 {
+    use GetRequestParameterId;
+
     /**
      * Make sure user has access to the project. $next is called if no project route, or project_id parameter
      * is found.
@@ -17,22 +20,11 @@ class UserCanAccessProject
      */
     public function handle(Request $request, Closure $next)
     {
-        $projectId = $this->getProjectId($request);
+        $projectId = $this->getParameterId('project');
         if ($projectId != '') {
             $count = auth()->user()->projects()->where('project_id', $projectId)->count();
             abort_unless($count == 1, 404, 'No such project');
         }
         return $next($request);
-    }
-
-    private function getProjectId(Request $request)
-    {
-        $projectId = $request->route('project');
-        if (gettype($projectId) == "object") {
-            $projectId = $projectId->id;
-        } elseif ($projectId == '') {
-            $projectId = $request->get('project_id');
-        }
-        return $projectId;
     }
 }
