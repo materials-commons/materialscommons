f2149d2161b400f34b24762050b633ef55bfb776
gtarcea <glenn.tarcea@gmail.com>
2019-08-23 22:55:18 -0400

    Use ActivityResource in CreateActivity

--- app/Http/Controllers/Api/Activities/CreateActivityApiController.php
+++ app/Http/Controllers/Api/Activities/CreateActivityApiController.php
@@ -5,21 +5,22 @@ namespace App\Http\Controllers\Api\Activities;
 use App\Actions\Activities\CreateActivityAction;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Activities\CreateActivityRequest;
+use App\Http\Resources\Activities\ActivityResource;
 
 class CreateActivityApiController extends Controller
 {
     /**
-     * Create an activity in a project
+     * Create an activity in a project.
      *
      * @param  CreateActivityRequest  $request
      * @param  CreateActivityAction  $createActivityAction
      *
-     * @return void
+     * @return \App\Http\Resources\Activities\ActivityResource
      */
     public function __invoke(CreateActivityRequest $request, CreateActivityAction $createActivityAction)
     {
         $validated = $request->validated();
         $activity = $createActivityAction($validated);
-        return $activity;
+        return new ActivityResource($activity);
     }
 }
