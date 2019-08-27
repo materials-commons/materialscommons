c991db3dd410c2782cff7cab6d5fb80eee6527d1
gtarcea <glenn.tarcea@gmail.com>
2019-08-27 16:31:20 -0400

    ShowFile API

--- app/Http/Controllers/Api/Files/ShowFileApiController.php
+++ app/Http/Controllers/Api/Files/ShowFileApiController.php
@@ -2,19 +2,23 @@
 
 namespace App\Http\Controllers\Api\Files;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Queries\Files\SingleFileQuery;
+use App\Http\Resources\Files\FileResource;
 
 class ShowFileApiController extends Controller
 {
     /**
-     * Handle the incoming request.
+     * Show the file.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param  \App\Http\Queries\Files\SingleFileQuery  $query
+     *
+     * @return \App\Http\Resources\Files\FileResource
      */
-    public function __invoke(Request $request)
+    public function __invoke(SingleFileQuery $query)
     {
-        //
+        $data = $query->get();
+        abort_if($data->isEmpty(), 404, "File not found");
+        return new FileResource($data[0]);
     }
 }
---
+++ app/Http/Queries/Files/SingleFileQuery.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Queries\Files;
+
+use App\Models\File;
+use App\Traits\GetRequestParameterId;
+use Illuminate\Http\Request;
+
+class SingleFileQuery extends FilesQueryBuilder
+{
+    use GetRequestParameterId;
+
+    /**
+     * Build query for a single file, don't allow directories to be looked up.
+     *
+     * @param  \Illuminate\Http\Request|null  $request
+     */
+    public function __construct(?Request $request = null)
+    {
+        $fileId = $this->getParameterId('file');
+        $query = File::where('id', $fileId)->where('mime_type', '!=', 'directory');
+        parent::__construct($query, $request);
+    }
+}
