60d07c3f4bb801599c681c8fd540d88da1f07a38
gtarcea <glenn.tarcea@gmail.com>
2019-09-03 14:27:03 -0400

    Implement adding a file to an activity

--- app/Actions/Activities/Files/AddFileToActivityAction.php
+++ app/Actions/Activities/Files/AddFileToActivityAction.php
@@ -2,9 +2,17 @@
 
 namespace App\Actions\Activities\Files;
 
+use App\Models\Activity;
+use Illuminate\Support\Facades\DB;
+
 class AddFileToActivityAction
 {
-    public function __invoke()
+    public function __invoke(Activity $activity, $fileId)
     {
+        DB::transaction(function () use ($activity, $fileId) {
+            $activity->files()->attach($fileId);
+        });
+
+        return $activity;
     }
 }
--- app/Http/Controllers/Api/Activities/Files/AddFileToActivityApiController.php
+++ app/Http/Controllers/Api/Activities/Files/AddFileToActivityApiController.php
@@ -2,9 +2,8 @@
 
 namespace App\Http\Controllers\Api\Activities\Files;
 
-use App\Actions\Activities\Files\AddFilesToActivityAction;
+use App\Actions\Activities\Files\AddFileToActivityAction;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Activies\Files\AddFilesToActivityRequest;
 use App\Http\Resources\Activities\ActivityResource;
 use App\Models\Activity;
 
@@ -13,19 +12,14 @@ class AddFileToActivityApiController extends Controller
     /**
      * Handle the incoming request.
      *
-     * @param  \App\Http\Requests\Activies\Files\AddFilesToActivityRequest  $request
-     * @param  \App\Actions\Activities\Files\AddFilesToActivityAction  $addFilesToActivityAction
+     * @param  \App\Actions\Activities\Files\AddFileToActivityAction  $addFileToActivityAction
      * @param  \App\Models\Activity  $activity
+     * @param $fileId
      * @return \App\Http\Resources\Activities\ActivityResource
      */
-    public function __invoke(
-        AddFilesToActivityRequest $request,
-        AddFilesToActivityAction $addFilesToActivityAction,
-        Activity $activity
-    )
+    public function __invoke(AddFileToActivityAction $addFileToActivityAction, Activity $activity, $fileId)
     {
-        $files = $request->input('files');
-        $activity = $addFilesToActivityAction($activity, $files);
+        $activity = $addFileToActivityAction($activity, $fileId);
         return new ActivityResource($activity);
     }
 }
