65b86bc67ffca3d0d5996599151e9f9680678423
gtarcea <glenn.tarcea@gmail.com>
2019-09-04 10:59:07 -0400

    Add datatables and font-awesome and update mix.webpack.js to copy over these files into public

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,5 +14,8 @@ require('laravel-mix-purgecss');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
+    .copy('resources/datatables/datatables.min.js', 'public/js')
+    .copy('resources/datatables/datatables.min.css', 'public/css')
+    .copyDirectory('resources/fa', 'public/css/fa')
     .purgeCss()
     .disableNotifications();
