920dcd5adf21130d266b43662bce4282f625c68a
gtarcea <glenn.tarcea@gmail.com>
2019-09-06 21:47:25 -0400

    Add support for switching experiments in the select box

--- resources/views/app/projects/experiments/show.blade.php
+++ resources/views/app/projects/experiments/show.blade.php
@@ -7,12 +7,27 @@
 @stop
 
 @section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col"></div>
+            <div class="col col-lg-4 float-right">
+                <select class="selectpicker col-lg-10" data-live-search="true" title="Switch To Experiment">
+                    @foreach($project->experiments as $entry)
+                        @if ($entry->name != $experiment->name)
+                            <option data-tokens="{{$entry->id}}" value="{{$entry->id}}">{{$entry->name}}</option>
+                        @endif
+                    @endforeach
+                </select>
+            </div>
+        </div>
+    </div>
     @component('components.card')
         @slot('header')
             Experiment {{$experiment->name}}
         @endslot
 
         @slot('body')
+
             @component('components.experiment-tabs', ['project' => $project, 'experiment' => $experiment])
             @endcomponent
 
@@ -30,4 +45,18 @@
             </div>
         @endslot
     @endcomponent
+
+    @push('scripts')
+        <script>
+            $(document).ready(() => {
+                $('select').on('change', () => {
+                    let selected = $('.selectpicker option:selected').val();
+                    window.location.href = route('projects.experiments.show', {
+                        project: "{{$project->id}}",
+                        experiment: selected
+                    }).url();
+                });
+            });
+        </script>
+    @endpush
 @stop
