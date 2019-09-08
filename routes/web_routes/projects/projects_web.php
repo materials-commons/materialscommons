c32e8609f931463cbbc07a7282b262c95c5d3111
gtarcea <glenn.tarcea@gmail.com>
2019-09-07 21:44:25 -0400

    Change order of route definitions so that /projects/create will come before /projects/{project}

--- routes/web_routes/projects/projects_web.php
+++ routes/web_routes/projects/projects_web.php
@@ -9,11 +9,11 @@ use App\Http\Controllers\Web\Projects\StoreProjectWebController;
 use App\Http\Controllers\Web\Projects\UpdateProjectWebController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/projects', IndexProjectsWebController::class)->name('projects.index');
-Route::get('/projects/{project}', ShowProjectWebController::class)->name('projects.show');
-
 Route::get('/projects/create', CreateProjectWebController::class)->name('projects.create');
 Route::post('/projects', StoreProjectWebController::class)->name('projects.store');
+
+Route::get('/projects', IndexProjectsWebController::class)->name('projects.index');
+Route::get('/projects/{project}', ShowProjectWebController::class)->name('projects.show');
 
 Route::patch('/projects/{project}', UpdateProjectWebController::class)->name('projects.update');
 Route::get('/projects/{project}/edit', EditProjectWebController::class)->name('projects.edit');
