af96286aa283c7d833296aaec5fec402136c1284
gtarcea <glenn.tarcea@gmail.com>
2019-09-09 18:13:44 -0400

    Allow description to be nullable

--- database/migrations/2019_07_17_114424_create_experiments_table.php
+++ database/migrations/2019_07_17_114424_create_experiments_table.php
@@ -1,9 +1,9 @@
 <?php
 
 use App\Enums\ExperimentStatus;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateExperimentsTable extends Migration
 {
@@ -19,7 +19,7 @@ class CreateExperimentsTable extends Migration
             $table->bigIncrements('id');
             $table->uuid('uuid')->unique();
             $table->string('name');
-            $table->text('description');
+            $table->text('description')->nullable();
 
             $table->unsignedBigInteger('project_id');
             $table->foreign('project_id')
