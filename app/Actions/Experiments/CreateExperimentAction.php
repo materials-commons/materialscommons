02a0ca1e5daf4d3e42f414f0ac21542f4e4033a4
gtarcea <glenn.tarcea@gmail.com>
2019-09-09 18:15:37 -0400

    Add experiment status

--- app/Actions/Experiments/CreateExperimentAction.php
+++ app/Actions/Experiments/CreateExperimentAction.php
@@ -2,12 +2,16 @@
 
 namespace App\Actions\Experiments;
 
+use App\Enums\ExperimentStatus;
+use App\Models\Experiment;
+
 class CreateExperimentAction
 {
     public function __invoke($data)
     {
-        $experiment = new Experiment($data);
+        $experiment           = new Experiment($data);
         $experiment->owner_id = auth()->id();
+        $experiment->status   = ExperimentStatus::InProgress;
         $experiment->save();
 
         return $experiment->fresh();
