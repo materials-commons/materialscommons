8ec326bd08f2295a41743f56bc4618632aa32a49
gtarcea <glenn.tarcea@gmail.com>
2019-09-09 18:14:49 -0400

    Add default value

--- resources/views/app/projects/create.blade.php
+++ resources/views/app/projects/create.blade.php
@@ -21,7 +21,7 @@
                 </div>
                 <div class="form-group">
                     <label for="description">Description</label>
-                    <textarea class="form-control" id="description" name="description" type="text"
+                    <textarea class="form-control" id="description" name="description" type="text" value=""
                               placeholder="Description..."></textarea>
                 </div>
                 <div class="float-right">
