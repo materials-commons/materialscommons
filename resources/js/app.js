625d45d5d6f49a77f4894f4ee5083a780acab560
gtarcea <glenn.tarcea@gmail.com>
2019-09-09 20:44:14 -0400

    Comment out Vue references since we aren't using it

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,7 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
+// window.Vue = require('vue');
 
 require('raphael');
 require('flowchart.js');
@@ -27,7 +27,7 @@ window.mcfl = require('./flowchart.js');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -35,6 +35,6 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue({
-    el: '#app'
-});
+// const app = new Vue({
+//     el: '#app'
+// });
