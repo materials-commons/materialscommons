8ab36b0ea64c3f9b35fe4f73bb9d5789d93dbf7d
gtarcea <glenn.tarcea@gmail.com>
2019-09-09 20:44:46 -0400

    Make errors test message time out a little more professional

--- resources/views/common/errors.blade.php
+++ resources/views/common/errors.blade.php
@@ -8,7 +8,7 @@
     </div>
 
     <div class="alert alert-warning alert-dismissible fade show" role="alert">
-        <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+        <strong>Testing popup</strong>
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
