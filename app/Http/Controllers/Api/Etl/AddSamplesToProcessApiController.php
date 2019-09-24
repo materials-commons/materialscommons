a36d91fd5fddfe15bbabba569f14cd5ded635328
gtarcea <glenn.tarcea@gmail.com>
2019-09-24 13:36:15 -0400

    Rename controller to match ETL api name

--- app/Http/Controllers/Api/Etl/AddSamplesToProcessApiController.php
+++ app/Http/Controllers/Api/Etl/AddSamplesToProcessApiController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api\Etl;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class AddSampleToProcessApiController extends Controller
+class AddSamplesToProcessApiController extends Controller
 {
     /**
      * Handle the incoming request.
