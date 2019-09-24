bbb63ebc59799300048af72c758a4627f8b03828
gtarcea <glenn.tarcea@gmail.com>
2019-09-24 15:31:30 -0400

    Start setting up the last 2 api calls.

--- app/Http/Controllers/Api/Etl/AddSampleAndFilesToProcessApiController.php
+++ app/Http/Controllers/Api/Etl/AddSampleAndFilesToProcessApiController.php
@@ -3,17 +3,11 @@
 namespace App\Http\Controllers\Api\Etl;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Etl\AddSampleAndFilesToProcessRequest;
 
 class AddSampleAndFilesToProcessApiController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function __invoke(Request $request)
+    public function __invoke(AddSampleAndFilesToProcessRequest $request)
     {
         //
     }
--- routes/api_routes/etl_api.php
+++ routes/api_routes/etl_api.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\Activities\CreateActivityApiController;
-use App\Http\Controllers\Api\Etl\CreateSampleApiController;
+use App\Http\Controllers\Api\Entities\CreateEntityApiController;
+use App\Http\Controllers\Api\Etl\AddMeasurementsToSampleInProcessApiController;
+use App\Http\Controllers\Api\Etl\AddSampleAndFilesToProcessApiController;
 use App\Http\Controllers\Api\Etl\GetFileByPathInProjectApiController;
 use App\Http\Controllers\Api\Etl\UpdateExperimentProgressStatusApiController;
 use App\Http\Controllers\Api\Experiments\CreateExperimentApiController;
@@ -22,7 +24,13 @@ Route::post('/etl/updateExperimentProgressStatus',
     UpdateExperimentProgressStatusApiController::class)->name('api.etl.updateExperimentProgressStatus');
 
 // Add a second path to create an entity that matches the expected path for the ETL api
-Route::post('/etl/createSample', CreateSampleApiController::class)->name('api.etl.createSample');
+Route::post('/etl/createSample', CreateEntityApiController::class)->name('api.etl.createSample');
 
 // Add a second path to create an activity more closely matches the expected for the ETL api
 Route::post('/etl/createProcess', CreateActivityApiController::class)->name('api.etl.createProcess');
+
+Route::post('/etl/addSampleAndFilesToProcess',
+    AddSampleAndFilesToProcessApiController::class)->name('api.etl.addSampleAndFilesToProcess');
+
+Route::post('/etl/addMeasurementsToSampleInProcess',
+    AddMeasurementsToSampleInProcessApiController::class)->name('api.etl.addMeasurementsToSampleInProcess');
