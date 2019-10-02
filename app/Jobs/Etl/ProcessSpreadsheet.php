f0d0368168c1f1fdb535eaf26fbcc55b5be40ed7
gtarcea <glenn.tarcea@gmail.com>
2019-10-02 18:41:12 -0400

    Create a job to load a spreadsheet

---
+++ app/Jobs/Etl/ProcessSpreadsheet.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Jobs\Etl;
+
+use App\Imports\Etl\EntityActivityImporter;
+use App\Models\File;
+use App\Traits\PathFromUUID;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Maatwebsite\Excel\Facades\Excel;
+
+class ProcessSpreadsheet implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels, PathFromUUID;
+
+    private $projectId;
+    private $experimentId;
+    private $userId;
+    private $fileId;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($projectId, $experimentId, $userId, $fileId)
+    {
+        $this->projectId = $projectId;
+        $this->experimentId = $experimentId;
+        $this->userId = $userId;
+        $this->fileId = $fileId;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $file = File::find($this->fileId);
+        $uuid = $file->uses_uuid ?? $file->uuid;
+        $uuidPath = $this->filePathFromUuid($uuid);
+        $importer = new EntityActivityImporter($this->projectId, $this->experimentId, $this->userId);
+        Excel::import($importer, storage_path("app/{$uuidPath}"), null, \Maatwebsite\Excel\Excel::XLSX);
+    }
+}
