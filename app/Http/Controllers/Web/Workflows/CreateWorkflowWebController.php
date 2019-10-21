7ce67aec517f1143788038df72d001127ba82fff
gtarcea <glenn.tarcea@gmail.com>
2019-10-21 15:44:42 -0400

    Skaffold out workflow pages

---
+++ app/Http/Controllers/Web/Workflows/CreateWorkflowWebController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Web\Workflows;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CreateWorkflowWebController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+
+    }
+}
