2eba8f2babc923d8eecaf094b1fb3b7b4a5a6130
gtarcea <glenn.tarcea@gmail.com>
2019-10-30 11:02:38 -0400

    Dispatch background job to build out activities and files lists plus construct the zip file

--- app/Actions/Datasets/PublishDatasetAction.php
+++ app/Actions/Datasets/PublishDatasetAction.php
@@ -2,6 +2,7 @@
 
 namespace App\Actions\Datasets;
 
+use App\Jobs\PublishDatasetJob;
 use App\Models\Dataset;
 use Carbon\Carbon;
 
@@ -10,6 +11,9 @@ class PublishDatasetAction
     public function __invoke(Dataset $dataset)
     {
         $dataset->update(['published_at' => Carbon::now()]);
+
+        $publishDatasetJob = new PublishDatasetJob($dataset->id);
+        dispatch($publishDatasetJob);
 
         return $dataset->fresh();
     }
