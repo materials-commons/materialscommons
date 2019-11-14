517e53809b70a2624759ab52ee0252e12345534e
gtarcea <glenn.tarcea@gmail.com>
2019-11-14 11:30:14 -0500

    Don't show email addresses for users in a project

--- resources/views/app/projects/users/index.blade.php
+++ resources/views/app/projects/users/index.blade.php
@@ -23,14 +23,12 @@
                 <thead>
                 <tr>
                     <th>Name</th>
-                    <th>Email</th>
                 </tr>
                 </thead>
                 <tbody>
                 @foreach($project->users as $user)
                     <tr>
                         <td>{{$user->name}}</td>
-                        <td>{{$user->email}}</td>
                     </tr>
                 @endforeach
                 </tbody>
