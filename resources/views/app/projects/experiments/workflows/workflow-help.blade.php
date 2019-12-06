53066e1a8810dc9443e4171cb13aa7b9740eddef
gtarcea <glenn.tarcea@gmail.com>
2019-12-06 16:43:23 -0500

    Add information around spaces in the workflow language

--- resources/views/app/projects/experiments/workflows/workflow-help.blade.php
+++ resources/views/app/projects/experiments/workflows/workflow-help.blade.php
@@ -7,11 +7,19 @@
         Workflows are specified using a simple language. Each step in your workflow is separated
         by a '->'. Conditional steps have names that contain a '?' in them.
     </p>
+    <p>You <u><b>must not</b></u> have spaces between nodes. The following is invalid (because there is a space before
+        and after the '->'.
+    </p>
+    <pre>
+        Heat Treatment -> Analyze
+    </pre>
     <p>
         When specifying a step you can optionally give a direction in parathensis. When specifying
         a conditional step, you can specify the text on the connecting arrow. Below is the code and result for
         an example workflow.
     </p>
+
+
     <pre>
         Heat Treat Sample?(yes, right)->Heat Treat at 400c/3h->SEM(right)->Analyze
         Heat Treat Sample?(no)->SEM->Analyze
