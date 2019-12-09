081983e5455bcce7f560ccf34d505122fd17bbf6
gtarcea <glenn.tarcea@gmail.com>
2019-12-09 14:57:30 -0500

    Comment out popup test

--- resources/views/common/errors.blade.php
+++ resources/views/common/errors.blade.php
@@ -7,12 +7,12 @@
         </ul>
     </div>
 
-    <div class="alert alert-warning alert-dismissible fade show" role="alert">
-        <strong>Testing popup</strong>
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
+    {{--    <div class="alert alert-warning alert-dismissible fade show" role="alert">--}}
+    {{--        <strong>Testing popup</strong>--}}
+    {{--        <button type="button" class="close" data-dismiss="alert" aria-label="Close">--}}
+    {{--            <span aria-hidden="true">&times;</span>--}}
+    {{--        </button>--}}
+    {{--    </div>--}}
 
     <script>
         // setTimeout(() => $(".alert").alert('close'), 2000);
