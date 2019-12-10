e28af2b3a768c79020d927f7f68f0a8daf35ccc8
gtarcea <glenn.tarcea@gmail.com>
2019-12-10 16:16:07 -0500

    Start working on constructing controller for globus uploads

--- app/Http/Controllers/Web/Projects/Globus/GlobusGetProjectUploadLinkWebController.php
+++ app/Http/Controllers/Web/Projects/Globus/GlobusGetProjectUploadLinkWebController.php
@@ -4,11 +4,22 @@ namespace App\Http\Controllers\Web\Projects\Globus;
 
 use App\Http\Controllers\Controller;
 use App\Models\Project;
+use Ramsey\Uuid\Uuid;
 
 class GlobusGetProjectUploadLinkWebController extends Controller
 {
     public function __invoke(Project $project)
     {
+        $endpointId = env('MC_GLOBUS_ENDPOINT_ID');
+        $uuid = Uuid::uuid4()->toString();
+        $path = storage_path("app/__globus_uploads/{$uuid}");
+        $globusPath = "/__globus_uploads/{$uuid}";
 
+        if (!is_dir($path)) {
+            mkdir($path);
+        }
+
+
+        GlobusUrl::globusUploadUrl($endpointId, $globusPath);
     }
 }
