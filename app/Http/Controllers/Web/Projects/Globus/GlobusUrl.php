84a0961287aeeef64f97a752745950f96658e4ba
gtarcea <glenn.tarcea@gmail.com>
2019-12-10 16:15:47 -0500

    Make methods static and add namespace to GlobusUrl

--- app/Http/Controllers/Web/Projects/Globus/GlobusUrl.php
+++ app/Http/Controllers/Web/Projects/Globus/GlobusUrl.php
@@ -1,25 +1,27 @@
 <?php
 
+namespace App\Http\Controllers\Web\Projects\Globus;
+
 class GlobusUrl
 {
     const GlobusBaseUrl = "https://app.globus.org/file-manager";
 
-    public function globusUploadUrl($endpointId, $path)
+    public static function globusUploadUrl($endpointId, $path)
     {
         return self::GlobusBaseUrl."?destination_id={$endpointId}&destination_path={$path}";
     }
 
-    public function globusDownloadUrl($endpointId, $path)
+    public static function globusDownloadUrl($endpointId, $path)
     {
         return self::GlobusBaseUrl."?origin_id={$endpointId}&origin_path={$path}";
     }
 
-    public function uploadEndpointPath($id)
+    public static function uploadEndpointPath($id)
     {
         return "/__globus_uploads/{$id}/";
     }
 
-    public function downloadEndpointPath($id)
+    public static function downloadEndpointPath($id)
     {
         return "/__download_staging/";
     }
