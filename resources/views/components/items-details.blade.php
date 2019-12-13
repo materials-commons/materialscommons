e9d52e4cdf95209184cb45a7db8cb2097407996a
gtarcea <glenn.tarcea@gmail.com>
2019-12-13 16:01:59 -0500

    Change size of details under description (made smaller so they don't stand out)

--- resources/views/components/items-details.blade.php
+++ resources/views/components/items-details.blade.php
@@ -5,7 +5,7 @@
                   placeholder="Description..." readonly>{{$item->description}}</textarea>
     </div>
     <div class="form-row">
-        <div class="col h5">
+        <div class="col h6">
             <span>Owner: {{$item->owner->name}}</span>
             <span class="ml-4">Last Updated {{$item->updated_at->diffForHumans()}}</span>
             {{$slot}}
