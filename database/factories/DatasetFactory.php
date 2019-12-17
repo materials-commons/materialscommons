36f787cc4fe2f1b5817a6374a01671d308b3381a
gtarcea <glenn.tarcea@gmail.com>
2019-12-17 14:51:13 -0500

    Remove funding and institution fields

--- database/factories/DatasetFactory.php
+++ database/factories/DatasetFactory.php
@@ -13,14 +13,12 @@ $factory->define(Dataset::class, function(Faker $faker) {
             return factory(App\Models\User::class)->create()->id;
         },
         'uuid'           => $faker->uuid,
-        'funding'        => "Dataset funding",
         'project_id'     => function() {
             return factory(App\Models\Project::class)->create()->id;
         },
         'license'        => 'Public Domain Dedication and License (PDDL)',
         'license_link'   => 'http://opendatacommons.org/licenses/pddl/summary/',
         'doi'            => $faker->url,
-        'institution'    => "Dataset institution",
         'authors'        => $faker->name,
         'published_at'   => $faker->dateTime,
         'file_selection' => [
