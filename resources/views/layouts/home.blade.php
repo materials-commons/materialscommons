1b68c18e5990b8057b3f11ed2926ab9db3a9a9ed
gtarcea <glenn.tarcea@gmail.com>
2019-12-18 22:16:04 -0500

    Show MaterialsCommons 2 as the name

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -31,7 +31,7 @@
         <nav class="navbar navbar-dark navbar-expand-md bg-nav p-0 shadow">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'MaterialsCommons') }}
+                    {{ config('app.name', 'MaterialsCommons 2') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
