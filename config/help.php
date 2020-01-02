ef4e74c6688475c912e01da94f4285e23fb03898
gtarcea <glenn.tarcea@gmail.com>
2020-01-02 15:12:24 -0500

    Set default help path to have an extra level of docs reference

--- config/help.php
+++ config/help.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'site' => env('MC_HELP', 'https://materialscommons.miserver.it.umich.edu/docs'),
+    'site' => env('MC_HELP', 'https://materialscommons.miserver.it.umich.edu/docs/docs'),
 ];
 
