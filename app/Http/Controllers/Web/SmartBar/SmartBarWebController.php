5c6df2d90356df4340a5a6c03cf405baed8122ad
gtarcea <glenn.tarcea@gmail.com>
2020-01-01 20:56:03 -0500

    Create controller for the smartbar

---
+++ app/Http/Controllers/Web/SmartBar/SmartBarWebController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Web\SmartBar;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SmartBarWebController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
