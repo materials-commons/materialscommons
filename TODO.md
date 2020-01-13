15126a6fb45babce8a6fe7dc364d5fbd9281d2ed
gtarcea <glenn.tarcea@gmail.com>
2020-01-13 16:20:50 -0500

    First cut at file handling

--- TODO.md
+++ TODO.md
@@ -1,3 +1,4 @@
 ## Importing Data into Rethinkdb
    - [ ] How to handle notes in experiment
    - [ ] Note fields contain HTML - what to do with them...
+   - [ ] How to handle parent in datafiles, or can it safely be ignored?
--- app/Actions/Migration/MigrateRethinkdbDataAction.php
+++ app/Actions/Migration/MigrateRethinkdbDataAction.php
@@ -373,8 +373,55 @@ class MigrateRethinkdbDataAction
         return $dir;
     }
 
+    /*
+     * {
+    "uploaded": 7048,
+    "description": "",
+    "parent": "",
+    "checksum": "71a87031b30ebbaee9c2df78c6bdf6fa",
+    "mtime": {
+      "timezone": "+00:00",
+      "$reql_type$": "TIME",
+      "epoch_time": 1492059869.188
+    },
+    "mediatype": {
+      "mime": "text/plain",
+      "description": "Text"
+    },
+    "name": "traj.txt",
+    "current": true,
+    "otype": "datafile",
+    "usesid": "f42a2e3e-2bf3-47d5-a489-bd375f5415e7",
+    "owner": "anirudh@engineering.ucsb.edu",
+    "birthtime": {
+      "timezone": "+00:00",
+      "$reql_type$": "TIME",
+      "epoch_time": 1492059869.188
+    },
+    "atime": {
+      "timezone": "+00:00",
+      "$reql_type$": "TIME",
+      "epoch_time": 1492059869.188
+    },
+    "id": "000000b4-e3dd-4d5c-b7b5-a077366135d0",
+    "size": 7048
+  },
+
+     */
     private function loadDataForFile($data)
     {
+        $modelData = $this->createModelDataForKnownItems($data);
+        if ($modelData == null) {
+            return null;
+        }
+
+        $modelData['current'] = $data['current'];
+        $modelData['mime_type'] = $data['mediatype']['mime'];
+        $modelData['media_type_description'] = $data['mediatype']['description'];
+        $modelData['size'] = $data['size'];
+        $modelData['current'] = $data['current'];
+        $modelData['uses_uuid'] = $data['usesid'];
+        $modelData['checksum'] = $data['checksum'];
         return true;
     }
 
