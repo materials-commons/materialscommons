b8eeda0f57c9ac257165f3ad4567db69b94829b7
gtarcea <glenn.tarcea@gmail.com>
2020-01-15 07:39:31 -0500

    Add stub importer for Entity States

---
+++ app/Actions/Migration/ImportEntityStates.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Actions\Migration;
+
+class ImportEntityStates extends AbstractImporter
+{
+
+    protected function setup()
+    {
+        // TODO: Implement setup() method.
+    }
+
+    protected function cleanup()
+    {
+        // TODO: Implement cleanup() method.
+    }
+
+    protected function loadData($data)
+    {
+        // TODO: Implement loadData() method.
+    }
+}
