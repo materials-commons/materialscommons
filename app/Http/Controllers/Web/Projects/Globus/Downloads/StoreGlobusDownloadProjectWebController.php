a940f7fb5fbbad699b11ef4cfddbe9d54f1deb0a
gtarcea <glenn.tarcea@gmail.com>
2020-01-17 16:23:22 -0500

    Rename

---
+++ app/Http/Controllers/Web/Projects/Globus/Downloads/StoreGlobusDownloadProjectWebController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Web\Projects\Globus\Downloads;
+
+use App\Actions\Globus\Downloads\CreateGlobusDownloadForProjectAction;
+use App\Actions\Globus\GlobusApi;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Globus\CreateGlobusProjectUploadDownloadRequest;
+use App\Models\Project;
+
+class StoreGlobusDownloadProjectWebController extends Controller
+{
+    public function __invoke(CreateGlobusProjectUploadDownloadRequest $request, Project $project)
+    {
+        $validated = $request->validated();
+        $createGlobusDownloadForProjectAction = new CreateGlobusDownloadForProjectAction(GlobusApi::createGlobusApi());
+        $globusDownload = $createGlobusDownloadForProjectAction($validated, $project->id, auth()->user());
+    }
+}
