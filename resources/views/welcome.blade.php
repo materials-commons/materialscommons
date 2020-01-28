f76ff298a75a0b097da40d010664f174daa0024b
gtarcea <glenn.tarcea@gmail.com>
2020-01-28 15:07:48 -0500

    Add published datasets reference and re-arrange button order

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,13 +70,22 @@
                     your assets and workflow. Easily share with colleagues or publish online.</p>
 
                 <div class="flex my-10">
+                    <a href="#" title="About MaterialsCommons"
+                       class="bg-blue-500 hover:bg-blue-600 font-normal text-white hover:text-white rounded mr-4 py-2 px-6">
+                        View Published Datasets
+                    </a>
+
                     <a href="/docs/getting-started" title="Materials Commons getting started"
-                       class="bg-blue-500 hover:bg-blue-600 font-normal text-white hover:text-white rounded mr-4 py-2 px-6">Read
-                        the docs</a>
+                       class="bg-blue-500 hover:bg-blue-600 font-normal text-white hover:text-white rounded mr-4 py-2 px-6">
+                        Read the docs
+                    </a>
 
                     <a href="#" title="About MaterialsCommons"
-                       class="bg-gray-400 hover:bg-gray-600 text-blue-900 font-normal hover:text-white rounded py-2 px-6">About
-                        MaterialsCommons</a>
+                       class="bg-gray-400 hover:bg-gray-600 text-blue-900 font-normal hover:text-white rounded py-2 px-6">
+                        About Materials Commons
+                    </a>
+
+
                 </div>
             </div>
 
