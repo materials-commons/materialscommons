33eeebfe8ea0b8e7b633336dcd701af54be1eda2
gtarcea <glenn.tarcea@gmail.com>
2020-01-30 11:03:51 -0500

    Update workflow chart colors and fonts to match the new color scheme and to differentiate elements

--- resources/js/flowchart.js
+++ resources/js/flowchart.js
@@ -10,34 +10,49 @@ function drawFlowchart(id, code) {
         'font-size': 14,
         'font': 'normal',
         'font-family': 'Helvetica',
-        'font-weight': 'normal',
+        'font-weight': 'bold',
         'font-color': 'black',
         'line-color': 'black',
-        'element-color': 'black',
-        'fill': 'yellow',
-        'yes-text': 'yes',
-        'no-text': 'no',
+        'element-color': '#63b3ed',
+        'fill': '#2b6cb0',
+        'yes-text': 'Yes',
+        'no-text': 'No',
         'arrow-end': 'block',
         'scale': 1,
         'symbols': {
             'start': {
-                'font-color': 'red',
-                'element-color': 'green',
-                'fill': 'yellow'
+                'font-color': 'black',
+                'element-color': '#2b6cb0',
+                'fill': '#bee3f8',
+                'font-weight': 'normal',
             },
             'end': {
-                'class': 'end-element'
+                // 'class': 'end-element'
+                'font-color': 'black',
+                'element-color': '#2b6cb0',
+                'fill': '#bee3f8',
+                'font-weight': 'normal',
+            },
+            condition: {
+                'font-color': 'black',
+                'element-color': '#2b6cb0',
+                'fill': '#a0aec0'
+            },
+            operation: {
+                'font-color': 'white',
+                'element-color': '#63b3ed',
+                'fill': '#2b6cb0'
             }
         },
-        'flowstate': {
-            'past': {'fill': '#CCCCCC', 'font-size': 12},
-            'current': {'fill': 'yellow', 'font-color': 'red', 'font-weight': 'bold'},
-            'future': {'fill': '#FFFF99'},
-            'request': {'fill': 'blue'},
-            'invalid': {'fill': '#444444'},
-            'approved': {'fill': '#58C4A3', 'font-size': 12, 'yes-text': 'APPROVED', 'no-text': 'n/a'},
-            'rejected': {'fill': '#C45879', 'font-size': 12, 'yes-text': 'n/a', 'no-text': 'REJECTED'}
-        }
+        // 'flowstate': {
+        //     'past': {'fill': '#CCCCCC', 'font-size': 12},
+        //     'current': {'fill': '#bee3f8', 'font-color': 'red', 'font-weight': 'bold'},
+        //     'future': {'fill': '#FFFF99'},
+        //     'request': {'fill': 'blue'},
+        //     'invalid': {'fill': '#444444'},
+        //     'approved': {'fill': '#58C4A3', 'font-size': 12, 'yes-text': 'APPROVED', 'no-text': 'n/a'},
+        //     'rejected': {'fill': '#C45879', 'font-size': 12, 'yes-text': 'n/a', 'no-text': 'REJECTED'}
+        // }
     });
 
     return chart;
