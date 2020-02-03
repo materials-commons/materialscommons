7d18512c8f247cc410b9fdd4ce475e68fab4a90a
gtarcea <glenn.tarcea@gmail.com>
2020-02-03 15:03:21 -0500

    Add constants for ACL types

--- app/Actions/Globus/EndpointAclRule.php
+++ app/Actions/Globus/EndpointAclRule.php
@@ -4,13 +4,17 @@ namespace App\Actions\Globus;
 
 class EndpointAclRule
 {
+    public const ACLPrincipalTypeIdentity = "identity";
+    public const ACLPrincipalTypeAllAuthenticatedUsers = "all_authenticated_users";
+
     public $principalType;
     public $identityId;
     public $path;
     public $permissions;
     public $endpointId;
 
-    public function __construct($identityId, $path, $permissions, $endpointId, $principalType = "identity")
+    public function __construct($identityId, $path, $permissions, $endpointId,
+        $principalType = EndpointAclRule::ACLPrincipalTypeIdentity)
     {
         $this->principalType = $principalType;
         $this->identityId = $identityId;
