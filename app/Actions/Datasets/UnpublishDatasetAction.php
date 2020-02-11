f980287130c13f239db9476d74e7ed2fb5b89207
gtarcea <glenn.tarcea@gmail.com>
2020-02-11 12:30:15 -0500

    Remove globus and zipfiles when unpublishing a dataset

--- app/Actions/Datasets/UnpublishDatasetAction.php
+++ app/Actions/Datasets/UnpublishDatasetAction.php
@@ -3,12 +3,15 @@
 namespace App\Actions\Datasets;
 
 use App\Models\Dataset;
+use Illuminate\Support\Facades\Storage;
 
 class UnpublishDatasetAction
 {
     public function __invoke(Dataset $dataset)
     {
         $dataset->update(['published_at' => null]);
+        Storage::disk('mcfs')->deleteDirectory($dataset->publishedGlobusPathPartial());
+        Storage::disk('mcfs')->deleteDirectory($dataset->zipfileDirPartial());
 
         return $dataset->fresh();
     }
