840a84ec9f552d2ae6a321ee75c6b5b5cccc7b77
gtarcea <glenn.tarcea@gmail.com>
2020-02-17 13:53:00 -0500

    Handle conversion files in download controller

---
+++ app/Actions/Files/GetFileContentsForDisplayAction.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Actions\Files;
+
+use App\Models\File;
+use Illuminate\Support\Facades\Storage;
+
+class GetFileContentsForDisplayAction
+{
+    private $officeTypes;
+    private $imageTypes;
+
+    private $convertibleImageTypes;
+
+    public function __construct()
+    {
+        $this->officeTypes = [
+            "application/vnd.ms-excel"                                                  => true,
+            "application/vnd.ms-powerpoint"                                             => true,
+            "application/msword"                                                        => true,
+            "application/vnd.openxmlformats-officedocument.presentationml.presentation" => true,
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"         => true,
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document"   => true,
+        ];
+
+        $this->imageTypes = [
+            "image/gif"      => true,
+            "image/jpeg"     => true,
+            "image/png"      => true,
+            "image/tiff"     => true,
+            "image/x-ms-bmp" => true,
+            "image/bmp"      => true,
+        ];
+
+        $this->convertibleImageTypes = [
+            'image/tiff'     => true,
+            'image/x-ms-bmp' => true,
+            'image/bmp'      => true,
+        ];
+    }
+
+    public function execute(File $file)
+    {
+        $filePathPartial = $this->fileContentsPathPartial($file);
+        if (!Storage::disk('mcfs')->exists($filePathPartial)) {
+            return null;
+        }
+
+        try {
+            return Storage::disk('mcfs')->get($filePathPartial);
+        } catch (\Exception $e) {
+            return null;
+        }
+    }
+
+    public function getMimeTypeTakingIntoAccountConversion(File $file)
+    {
+        if (array_key_exists($file->mime_type, $this->convertibleImageTypes)) {
+            return "image/jpeg";
+        }
+
+        if (array_key_exists($file->mime_type, $this->officeTypes)) {
+            return "application/pdf";
+        }
+
+        return $file->mime_type;
+    }
+
+    private function fileContentsPathPartial(File $file)
+    {
+        $uuid = $file->uuid;
+        if ($file->uses_uuid !== null) {
+            $uuid = $file->uses_uuid;
+        }
+
+        $entries = explode('-', $uuid);
+        $entry1 = $entries[1];
+
+        $dirPath = "{$entry1[0]}{$entry1[1]}/{$entry1[2]}{$entry1[3]}";
+        $fileName = $uuid;
+
+        if (array_key_exists($file->mime_type, $this->convertibleImageTypes)) {
+            $dirPath = $dirPath."/.conversion";
+            $fileName = $fileName.".jpg";
+        }
+
+        if (array_key_exists($file->mime_type, $this->officeTypes)) {
+            $dirPath = $dirPath."/.conversion";
+            $fileName = $fileName.".pdf";
+        }
+
+        return "{$dirPath}/{$fileName}";
+    }
+}
--- app/Http/Controllers/Web/Files/DownloadFileWebController.php
+++ app/Http/Controllers/Web/Files/DownloadFileWebController.php
@@ -2,39 +2,22 @@
 
 namespace App\Http\Controllers\Web\Files;
 
+use App\Actions\Files\GetFileContentsForDisplayAction;
 use App\Http\Controllers\Controller;
 use App\Models\File;
 use App\Models\Project;
-use Illuminate\Support\Facades\File as FileFacade;
 use Illuminate\Support\Facades\Response;
-use Illuminate\Support\Facades\Storage;
 
 class DownloadFileWebController extends Controller
 {
-    public function __invoke(Project $project, File $file)
+    public function __invoke(GetFileContentsForDisplayAction $getFileContentsForDisplayAction, Project $project,
+        File $file)
     {
-        $path = $this->storagePathForFile($file);
-        if ( ! FileFacade::exists($path)) {
-            abort(404);
-        }
-
-        $f = FileFacade::get($path);
-        $type = FileFacade::mimeType($path);
+        $f = $getFileContentsForDisplayAction->execute($file);
+        abort_if(is_null($f), 404);
         $response = Response::make($f, 200);
-        $response->header("Content-Type", $type);
-
+        $response->header("Content-Type",
+            $getFileContentsForDisplayAction->getMimeTypeTakingIntoAccountConversion($file));
         return $response;
-    }
-
-    private function storagePathForFile($file)
-    {
-        $uuid = $file->uuid;
-        if ($file->uses_uuid !== null) {
-            $uuid = $file->uses_uuid;
-        }
-        $entries = explode('-', $uuid);
-        $entry1 = $entries[1];
-
-        return Storage::disk('mcfs')->path("{$entry1[0]}{$entry1[1]}/{$entry1[2]}{$entry1[3]}/{$uuid}");
     }
 }
