dbac30afb443745576726225462d2d92542d9d85
gtarcea <glenn.tarcea@gmail.com>
2020-02-19 13:16:17 -0500

    Add controllers for showing published processes and samples. These are skeletons at the moment and don't do anything

---
+++ app/Http/Controllers/Web/Published/Datasets/Activities/ShowPublishedDatasetActivityWebController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Web\Published\Datasets\Activities;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ShowPublishedDatasetActivityWebController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
