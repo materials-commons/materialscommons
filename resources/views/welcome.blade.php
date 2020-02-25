4979543f188a7f5c0ac0943310cae3ca13cbdacb
gtarcea <glenn.tarcea@gmail.com>
2020-02-25 11:49:31 -0500

    Front page changes group asked for (rough sketch)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -62,7 +62,7 @@
     <section class="container max-w-6xl mx-auto px-6 py-4 md:py-6">
         <div class="flex flex-col-reverse mb-6 lg:flex-row lg:mb-6">
             <div class="mt-4">
-                <h1 id="intro-docs-template">Materials Commons</h1>
+                <h1 id="intro-docs-template">Materials Commons 2.0</h1>
 
                 <h2 id="intro-powered-by-jigsaw" class="font-light mt-4">A site for Materials Scientists to collaborate,
                     store and publish research.</h2>
@@ -71,20 +71,27 @@
                     your results and workflow. Easily share with colleagues or publish online.</p>
 
                 <div class="flex my-10">
+
                     <a href="{{route('public.index')}}" title="About Materials Commons"
                        class="bg-blue-500 hover:bg-blue-600 font-normal text-white hover:text-white rounded mr-4 py-2 px-6">
-                        View Published Datasets
+                        Getting Started
+                    </a>
+
+                    <a href="{{route('public.index')}}" title="About Materials Commons"
+                       class="bg-blue-500 hover:bg-blue-600 font-normal text-white hover:text-white rounded mr-4 py-2 px-6">
+                        Upload and Publish Data
                     </a>
 
                     <a href="{{makeHelpUrl("getting-started")}}" title="Materials Commons getting started"
                        class="bg-blue-500 hover:bg-blue-600 font-normal text-white hover:text-white rounded mr-4 py-2 px-6">
-                        Read The Docs
+                        Browse Published Data
                     </a>
 
-                    <a href="{{route('about')}}" title="About Materials Commons"
-                       class="bg-gray-400 hover:bg-gray-600 text-blue-900 font-normal hover:text-white rounded py-2 px-6">
-                        About Materials Commons
-                    </a>
+                    {{--                    <a href="{{route('about')}}" title="About Materials Commons"--}}
+                    {{--                       class="bg-gray-400 hover:bg-gray-600 text-blue-900 font-normal hover:text-white rounded py-2 px-6">--}}
+                    {{--                        About Materials Commons--}}
+                    {{--                    </a>--}}
+
                 </div>
             </div>
 
@@ -104,15 +111,15 @@
                     connection.</p>
             </div>
 
-            <div class="mb-8 mx-3 px-2 md:w-1/3">
-                <img src="{{asset('images/icon-terminal.svg')}}" class="h-12 w-12" alt="terminal icon">
+            {{--            <div class="mb-8 mx-3 px-2 md:w-1/3">--}}
+            {{--                <img src="{{asset('images/icon-terminal.svg')}}" class="h-12 w-12" alt="terminal icon">--}}
 
-                <h3 id="intro-markdown" class="text-2xl text-blue-900 mb-0">Powerful Command Line Tool</h3>
+            {{--                <h3 id="intro-markdown" class="text-2xl text-blue-900 mb-0">Powerful Command Line Tool</h3>--}}
 
-                <p>If you prefer to work from the command prompt we've got you covered! Materials Commons comes with a
-                    powerful command line tool and a Python API that allows you to write scripts to automate
-                    processes. </p>
-            </div>
+            {{--                <p>If you prefer to work from the command prompt we've got you covered! Materials Commons comes with a--}}
+            {{--                    powerful command line tool and a Python API that allows you to write scripts to automate--}}
+            {{--                    processes. </p>--}}
+            {{--            </div>--}}
 
             <div class="mx-3 px-2 md:w-1/3">
                 <img src="{{asset('images/icon-stack.svg')}}" class="h-12 w-12" alt="stack icon">
