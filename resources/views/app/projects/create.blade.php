10c4512ecd33f5e393c98fd5df2b556c3fad86ae
gtarcea <glenn.tarcea@gmail.com>
2020-02-27 11:24:06 -0500

    Use new create project partial

--- resources/views/app/projects/create.blade.php
+++ resources/views/app/projects/create.blade.php
@@ -13,27 +13,10 @@
         @endslot
 
         @slot('body')
-            <form method="post" action="{{route('projects.store')}}" id="project-create">
-                @csrf
-                <div class="form-group">
-                    <label for="name">Name</label>
-                    <input class="form-control" id="name" name="name" type="text" value="" placeholder="Name...">
-                </div>
-                <div class="form-group">
-                    <label for="description">Description</label>
-                    <textarea class="form-control" id="description" name="description" type="text" value=""
-                              placeholder="Description..."></textarea>
-                </div>
-                <div class="float-right">
-                    <a href="{{route('projects.index')}}" class="action-link danger mr-3">
-                        Cancel
-                    </a>
-
-                    <a class="action-link" href="#" onclick="document.getElementById('project-create').submit()">
-                        Create
-                    </a>
-                </div>
-            </form>
+            @include('partials._create_project', [
+                'createProjectRoute' => route('projects.store'),
+                'cancelRoute' => route('projects.index')
+            ])
         @endslot
     @endcomponent
 
