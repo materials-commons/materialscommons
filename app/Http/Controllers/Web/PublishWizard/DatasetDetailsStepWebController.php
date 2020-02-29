a64bc1c0e21202674018f9e437fb731ef52fbc42
gtarcea <glenn.tarcea@gmail.com>
2020-02-29 17:29:33 -0500

    spec out what dataset details will look like and some of the logic

--- app/Http/Controllers/Web/PublishWizard/DatasetDetailsStepWebController.php
+++ app/Http/Controllers/Web/PublishWizard/DatasetDetailsStepWebController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers\Web\PublishWizard;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class DatasetDetailsStepWebController extends Controller
 {
     public function __invoke(Request $request)
     {
-        return view('app.publish.wizard.dataset_details');
+        $user = auth()->user();
+        $project = Project::where('name', 'Published Datasets Project')
+                          ->where('owner_id', $user->id)->first();
+        if (is_null($project)) {
+            $project = Project::create([
+                'name'     => 'Published Datasets Project',
+                'owner_id' => auth()->user()->id,
+            ]);
+        }
+        $communities = collect();
+        return view('app.publish.wizard.dataset_details', compact('project', 'communities'));
     }
 }
--- resources/views/app/publish/wizard/dataset_details.blade.php
+++ resources/views/app/publish/wizard/dataset_details.blade.php
@@ -12,7 +12,94 @@
         @endslot
 
         @slot('body')
-            dataset details
+            <form method="post" action="{{route('projects.datasets.store', [$project])}}" id="dataset_create">
+                @csrf
+                <div class="form-group">
+                    <label class="required" for="name">Name</label>
+                    <input class="form-control" id="name" name="name" type="text" value=""
+                           placeholder="Name...">
+                </div>
+
+                <div class="form-group">
+                    <label for="authors">Authors and Affiliations</label>
+                    <input class="form-control" id="authors" name="authors" type="text"
+                           value=""
+                           placeholder="Authors...">
+                </div>
+
+                <div class="form-group">
+                    <label for="description">Description</label>
+                    <textarea class="form-control" id="description" name="description" type="text"
+                              value=""
+                              placeholder="Description..."></textarea>
+                </div>
+
+                <div class="form-group">
+                    <label for="doi">DOI</label>
+                    <span class="col-8">
+                            None
+                            <a href="#" onclick="changeActionAndSubmit()" style="margin-left: 8px">
+                                Assign DOI
+                            </a>
+                        </span>
+                </div>
+
+                <div class="form-group">
+                    <label for="license">License</label>
+                    <select name="license" class="selectpicker col-lg-8" data-live-search="true"
+                            title="License">
+                        <option data-token="No License" value="No License">No License</option>
+                        <option data-token="Public Domain Dedication and License (PDDL)"
+                                value="Public Domain Dedication and License (PDDL)">
+                            Public Domain Dedication and License (PDDL)
+                        </option>
+                        <option data-token="Attribution License (ODC-By)"
+                                value="Attribution License (ODC-By)">
+                            Attribution License (ODC-By)
+                        </option>
+                        <option data-token="Open Database License (ODC-ODbL)"
+                                value="Open Database License (ODC-ODbL)">
+                            Open Database License (ODC-ODbL)
+                        </option>
+                    </select>
+                    <a href="https://opendatacommons.org/licenses/index.html" target="_blank">License Summaries</a>
+                </div>
+
+                <div class="form-group">
+                    <label for="communities">Communities</label>
+                    <select name="communities[]" class="selectpicker col-lg-8"
+                            title="communities"
+                            data-live-search="true" multiple>
+                        @foreach($communities as $community)
+                            <option data-token="{{$community->id}}" value="{{$community->id}}">
+                                {{$community->name}}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <div class="form-group">
+                    <label for="tags">Tags</label>
+                    <input class="form-control" id="tags" name="tags">
+                </div>
+
+                <input hidden id="project_id" name="project_id" value="{{$project->id}}">
+                <input type="hidden" name="action" value="" id="action"/>
+
+                <div class="float-right">
+                    <a href="{{route('projects.datasets.index', ['project' => $project->id])}}"
+                       class="action-link danger mr-3">
+                        Cancel
+                    </a>
+
+                    <a class="action-link mr-3" href="#" id="add-assets-button"
+                       onclick="setActionAndSubmit('assets')">
+                        Save And Add Files
+                    </a>
+                </div>
+            </form>
+            <br>
+            @include('common.errors')
         @endslot
     @endcomponent
 @stop
