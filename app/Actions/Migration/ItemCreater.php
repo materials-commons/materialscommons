055810139d5abdea210ee526fdd7a7a64e79cf62
gtarcea <glenn.tarcea@gmail.com>
2020-03-20 15:37:28 -0400

    Put in fix for directories that have private in their owner

--- app/Actions/Migration/ItemCreater.php
+++ app/Actions/Migration/ItemCreater.php
@@ -11,14 +11,14 @@ trait ItemCreater
             return null;
         }
 
-        $modelData = $this->createCommonModelData($data);
-
-        if ($modelData == null) {
+        $project = ItemCache::findProject($knownItems[$data['id']]);
+        if ($project == null) {
             return null;
         }
 
-        $project = ItemCache::findProject($knownItems[$data['id']]);
-        if ($project == null) {
+        $modelData = $this->createCommonModelDataWithProject($data, $project);
+
+        if ($modelData == null) {
             return null;
         }
 
@@ -50,6 +50,32 @@ trait ItemCreater
                 return null;
             }
             $modelData['owner_id'] = $user->id;
+        }
+
+        return $modelData;
+    }
+
+    private function createCommonModelDataWithProject($data, $project)
+    {
+        $modelData = [];
+        $modelData["uuid"] = $data["id"];
+        if (isset($data['name'])) {
+            $modelData["name"] = $data["name"];
+        } elseif (isset($data['title'])) {
+            $modelData['name'] = $data['title'];
+        }
+
+        if (isset($data['description'])) {
+            $modelData['description'] = $data['description'];
+        } else {
+            if (isset($data['note'])) {
+                $modelData['description'] = $data['note'];
+            }
+        }
+
+        if (isset($data['owner'])) {
+            $user = ItemCache::findUser($data['owner']);
+            $modelData['owner_id'] = $user != null ? $user->id : $project->owner_id;
         }
 
         return $modelData;
