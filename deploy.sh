82b0bd3a477f06f2594e588d8093cc9d4d8bf235
Glenn Tarcea <gtarcea@materialscommons.miserver.it.umich.edu>
2020-03-22 15:58:25 -0400

    Add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -x
+rm -rf /var/www/html/materialscommons.old
+mv /var/www/html/materialscommons /var/www/html/materialscommons.old
+cd ..
+cp -r materialscommons /var/www/html/materialscommons
+chcon -R -h -t httpd_sys_script_rw_t /var/www/html/materialscommons
+cd /var/www/html/materialscommons
+/usr/local/bin/composer install --optimize-autoloader --no-dev
+php artisan config:cache
+php artisan migrate
+cd ..
+chown -R nginx:nginx materialscommons
+systemctl restart supervisord
