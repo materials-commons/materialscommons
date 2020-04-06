7166e3429aa5ba7dc7cea5a964e578565f584cab
gtarcea <glenn.tarcea@gmail.com>
2020-04-06 16:34:42 -0400

    Enhance dataset setup to build globus files if the directory doesn't exist

--- app/Actions/Migration/Datasets/SetupMigratedPublishedDatasetsAction.php
+++ app/Actions/Migration/Datasets/SetupMigratedPublishedDatasetsAction.php
@@ -2,6 +2,7 @@
 
 namespace App\Actions\Migration\Datasets;
 
+use App\Actions\Datasets\CreateDatasetInGlobusAction;
 use App\Actions\Globus\EndpointAclRule;
 use App\Actions\Globus\GlobusApi;
 use App\Models\Dataset;
@@ -10,10 +11,12 @@ use Illuminate\Support\Facades\Storage;
 class SetupMigratedPublishedDatasetsAction
 {
     private $globusApi;
+    private $createDatsetInGlobusAction;
 
     public function __construct(GlobusApi $globusApi)
     {
         $this->globusApi = $globusApi;
+        $this->createDatsetInGlobusAction = new CreateDatasetInGlobusAction($globusApi);
     }
 
     public function __invoke($runZipLinker, $runGlobus)
@@ -23,19 +26,25 @@ class SetupMigratedPublishedDatasetsAction
             try {
                 foreach (Storage::disk('mcfs')->allFiles($dataset->zipfileDirPartial()) as $zipfile) {
                     if ($runZipLinker) {
-//                        echo "Running zipfile linker\n";
+                        echo "Running zipfile linker\n";
                         $this->linkExistingDatasetZipfileToNewName($zipfile, $dataset);
                     }
 
                     if ($runGlobus) {
-                        //                        echo "Running globus\n";
-                        $this->setupGlobusAccessForDataset($dataset);
+                        echo "Running globus\n";
+                        if (Storage::disk('mcfs')->exists($dataset->publishedGlobusPathPartial())) {
+                            echo "  Globus directory exists, setting up access...\n";
+                            $this->setupGlobusAccessForDataset($dataset);
+                        } else {
+                            echo "  Globus directory does NOT exist, creating...\n";
+                            ($this->createDatsetInGlobusAction)($dataset->id, false);
+                        }
                     }
                 }
             } catch (\Exception $e) {
-                //                echo "Failed on all files for dataset {$dataset->name}/{$dataset->uuid}\n";
+                echo "Failed on all files for dataset {$dataset->name}/{$dataset->uuid}\n";
                 $exceptionMessage = $e->getMessage();
-                //                echo "  Reason: {$exceptionMessage}\n";
+                echo "  Reason: {$exceptionMessage}\n";
             }
         });
     }
@@ -45,18 +54,18 @@ class SetupMigratedPublishedDatasetsAction
         $existingZipfilePath = Storage::disk('mcfs')->path($zipfile);
         if (Storage::disk('mcfs')->exists($dataset->zipfilePathPartial())) {
             // No need to link old zipfile to new zipfile
-            //            echo "No need to link, zipfile already exists.\n";
+            echo "No need to link, zipfile already exists.\n";
             return;
         }
         $newZipfilePath = $dataset->zipfilePath();
         try {
             if (!link($existingZipfilePath, $newZipfilePath)) {
-                //                echo "Unable to link {$existingZipfilePath} to {$newZipfilePath}\n";
+                echo "Unable to link {$existingZipfilePath} to {$newZipfilePath}\n";
             }
         } catch (\Exception $e) {
-            //            echo "Unable to link {$existingZipfilePath} to {$newZipfilePath}\n";
+            echo "Unable to link {$existingZipfilePath} to {$newZipfilePath}\n";
             $exceptionMessage = $e->getMessage();
-            //            echo "  Reason: {$exceptionMessage}\n";
+            echo "  Reason: {$exceptionMessage}\n";
         }
     }
 
@@ -78,9 +87,9 @@ class SetupMigratedPublishedDatasetsAction
                 ]);
             }
         } catch (\Exception $e) {
-            //            echo "Unable to setup globus for dataset {$dataset->name}/{$dataset->uuid}\n";
+            echo "Unable to setup globus for dataset {$dataset->name}/{$dataset->uuid}\n";
             $exceptionMessage = $e->getMessage();
-            //            echo "  Reason: {$exceptionMessage}\n";
+            echo "  Reason: {$exceptionMessage}\n";
         }
     }
 }
