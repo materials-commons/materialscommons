57bfa544ebe6d26bd12e19a722fd7f19fa7cffd4
gtarcea <glenn.tarcea@gmail.com>
2020-04-07 13:15:39 -0400

    Delete upload directory

--- app/Http/Controllers/Web/Projects/Globus/Uploads/DestroyGlobusUploadWebController.php
+++ app/Http/Controllers/Web/Projects/Globus/Uploads/DestroyGlobusUploadWebController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Models\GlobusUploadDownload;
 use App\Models\Project;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 
 class DestroyGlobusUploadWebController extends Controller
 {
@@ -15,6 +16,7 @@ class DestroyGlobusUploadWebController extends Controller
         try {
             $globusApi = GlobusApi::createGlobusApi();
             $globusApi->deleteEndpointAclRule($globusUpload->globus_endpoint_id, $globusUpload->globus_acl_id);
+            Storage::disk('mcfs')->deleteDirectory("__globus_uploads/{$globusUpload->uuid}");
         } catch (\Exception $e) {
             Log::error("Unable to delete acl");
         }
