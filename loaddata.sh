9e606a1a0f3231b3aadf363e7183d2dfe092c49e
Glenn Tarcea <gtarcea@materialscommons.miserver.it.umich.edu>
2020-04-08 13:27:35 -0400

    Updates to loaddata.sh

--- loaddata.sh
+++ loaddata.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 > $HOME/load.out
-php artisan migrate:fresh >> $HOME/load.out
+php artisan migrate >> $HOME/load.out
+MCFILE=$(ls /mcfs/gtarcea/dumps | tail -1)
+rm -rf /home/gtarcea/dump/*
+cp /mcfs/gtarcea/dumps/${MCFILE} /home/gtarcea/dump
+(cd /home/gtarcea/dump; tar zxf *.gz)
+(cd /home/gtarcea/dump; rm *.gz)
+(cd /home/gtarcea/dump; mv *dump* db)
+
 php artisan mc:import-rethinkdb-data /home/gtarcea/dump/db/materialscommons --ignore-existing >> $HOME/load.out
