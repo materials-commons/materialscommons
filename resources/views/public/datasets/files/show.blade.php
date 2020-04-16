7a9fdd0c3dfd76d4fe4d6b15e32d1459999c73fc
gtarcea <glenn.tarcea@gmail.com>
2020-04-16 17:57:52 -0400

    Move download into bar

--- resources/views/public/datasets/files/show.blade.php
+++ resources/views/public/datasets/files/show.blade.php
@@ -12,16 +12,15 @@
     @component('components.card')
         @slot('header')
             File: {{$file->name}}
+            <a class="action-link float-right"
+               href="{{route('public.datasets.download_file', [$dataset, $file])}}">
+                <i class="fas fa-download mr-2"></i>Download File
+            </a>
         @endslot
 
         @slot('body')
             @component('components.item-details', ['item' => $file])
                 <span class="ml-4">Mediatype: {{$file->mime_type}}</span>
-                @slot('bottom')
-                    <a href="{{route('public.datasets.download_file', [$dataset, $file])}}">
-                        Download file
-                    </a>
-                @endslot
             @endcomponent
             <hr>
             <br>
