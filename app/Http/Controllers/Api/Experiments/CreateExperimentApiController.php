41979b9ef68280c901c91edaa3c6c8329a88091c
gtarcea <glenn.tarcea@gmail.com>
2020-04-21 11:36:29 -0400

    Remove docblock

--- app/Http/Controllers/Api/Experiments/CreateExperimentApiController.php
+++ app/Http/Controllers/Api/Experiments/CreateExperimentApiController.php
@@ -9,16 +9,6 @@ use App\Http\Resources\Experiments\ExperimentResource;
 
 class CreateExperimentApiController extends Controller
 {
-    /**
-     * CreateExperiment
-     *
-     * Create a new experiment in a project.
-     *
-     * @param  \App\Http\Requests\Experiments\CreateExperimentRequest  $request
-     * @param  \App\Actions\Experiments\CreateExperimentAction  $createExperimentAction
-     *
-     * @return \App\Http\Resources\Experiments\ExperimentResource
-     */
     public function __invoke(CreateExperimentRequest $request, CreateExperimentAction $createExperimentAction)
     {
         $validated = $request->validated();
