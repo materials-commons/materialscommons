8473e11d9eca13ccaafdbf8dc5e0868a8f19c245
gtarcea <glenn.tarcea@gmail.com>
2020-04-22 12:48:57 -0400

    Placeholders for indexing entities and activities

---
+++ app/Http/Controllers/Api/Activities/IndexActivitiesApiController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api\Activities;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IndexActivitiesApiController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
