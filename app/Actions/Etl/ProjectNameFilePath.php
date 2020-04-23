434c097e36235336963d7addb780b091d05874ca
gtarcea <glenn.tarcea@gmail.com>
2020-04-23 17:49:43 -0400

    Update types in docblock

--- app/Actions/Etl/ProjectNameFilePath.php
+++ app/Actions/Etl/ProjectNameFilePath.php
@@ -8,8 +8,8 @@ trait ProjectNameFilePath
      * To remain compatible with the old api paths are assumed to start with the project name. This action
      * will remove the project name and just keep the starting slash as names are stored with a / for the
      * the root rather than the project name.
-     * @param $path Path containing project name
-     * @param $projectNameLength Length of project name
+     * @param $path string Path containing project name
+     * @param $projectNameLength int Length of project name
      * @return mixed
      */
     public function removeProjectNameFromPath($path, $projectNameLength)
