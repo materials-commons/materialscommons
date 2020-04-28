e8bd1680d1969192f914b318d5dd13de22855633
gtarcea <glenn.tarcea@gmail.com>
2020-04-28 14:51:05 -0400

    Add api call to get a file by path

---
+++ app/Http/Controllers/Api/Files/GetFileByPathApiController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api\Files;
+
+use App\Actions\Etl\GetFileByPathAction;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Files\GetFileByPathRequest;
+use App\Http\Resources\Files\FileResource;
+
+class GetFileByPathApiController extends Controller
+{
+    public function __invoke(GetFileByPathRequest $request, GetFileByPathAction $getFileByPathAction)
+    {
+        $validated = $request->validated();
+        $file = $getFileByPathAction($validated['project_id'], $validated['path']);
+        abort_if(is_null($file), 404, "No such file");
+        return new FileResource($file);
+    }
+}
--- routes/api_routes/files_api.php
+++ routes/api_routes/files_api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\Files\CreateFileApiController;
 use App\Http\Controllers\Api\Files\DeleteFileApiController;
 use App\Http\Controllers\Api\Files\DownloadFileApiController;
+use App\Http\Controllers\Api\Files\GetFileByPathApiController;
 use App\Http\Controllers\Api\Files\MoveFileApiController;
 use App\Http\Controllers\Api\Files\RenameFileApiController;
 use App\Http\Controllers\Api\Files\ShowFileApiController;
@@ -16,3 +17,4 @@ Route::get('/projects/{project}/files/{file}', ShowFileApiController::class);
 Route::post('/files/{file}/move', MoveFileApiController::class);
 Route::post('/files/{file}/rename', RenameFileApiController::class);
 Route::get('/projects/{project}/files/{file}/download', DownloadFileApiController::class);
+Route::post('/files/by_path', GetFileByPathApiController::class);
