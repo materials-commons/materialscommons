7fa60cb9011551e02faf5083f2f6e7bc0d5d631a
gtarcea <glenn.tarcea@gmail.com>
2020-05-05 14:12:15 -0400

    Handle cells that don't have a header

--- app/Imports/Etl/HeaderTracker.php
+++ app/Imports/Etl/HeaderTracker.php
@@ -5,19 +5,25 @@ namespace App\Imports\Etl;
 class HeaderTracker
 {
     public $headersByIndex;
+    public $headersByIndexLength;
 
     public function __construct()
     {
         $this->headersByIndex = array();
+        $this->headersByIndexLength = sizeof($this->headersByIndex);
     }
 
     public function addHeader(AttributeHeader $header)
     {
         array_push($this->headersByIndex, $header);
+        $this->headersByIndexLength = sizeof($this->headersByIndex);
     }
 
     public function getHeaderByIndex($index)
     {
+        if ($index >= $this->headersByIndexLength) {
+            return null;
+        }
         return $this->headersByIndex[$index];
     }
 }
