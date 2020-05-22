4126229cb91629d2d13f517016f881d131baf0ec
gtarcea <glenn.tarcea@gmail.com>
2020-05-22 14:36:56 -0400

    Refactor to simplify code

--- app/Traits/GetRequestParameterId.php
+++ app/Traits/GetRequestParameterId.php
@@ -6,17 +6,21 @@ trait GetRequestParameterId
 {
     public function getParameterId($parameterName)
     {
-        $parameterId = request()->route($parameterName);
+        $parameter = request()->route($parameterName);
 
         // Route Model binding will return the object rather than the id when the route uses the binding.
-        // To handle that case we check if $objectId is actually an object and if it is get the id,
-        // otherwise we check if the route has a $objectName in it.
-        if (gettype($parameterId) == "object") {
-            $parameterId = $parameterId->id;
-        } elseif ($parameterId == '') {
-            // If we are here then the route didn't have a $parameterName parameter, but there could be a {$parameterName}_id parameter.
-            $parameterId = request()->input("{$parameterName}_id");
+        // To handle that case we check if $parameter is actually an object and if it is get the id,
+        // otherwise we check if the route has a $parameterName in it.
+        if (gettype($parameter) == "object") {
+            return $parameter->id;
         }
-        return $parameterId;
+
+        if ($parameter == '') {
+            // If we are here then the route didn't have a $parameterName parameter,
+            // but there could be a {$parameterName}_id parameter.
+            return request()->input("{$parameterName}_id");
+        }
+
+        return $parameter;
     }
 }
