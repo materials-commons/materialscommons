2911184a9f2859c3eca64b6991e865a4ef60e255
gtarcea <glenn.tarcea@gmail.com>
2020-06-01 15:33:32 -0400

    Include trait ViewsAndDownloads

--- app/Http/Controllers/Api/Datasets/DownloadDatasetZipfileApiController.php
+++ app/Http/Controllers/Api/Datasets/DownloadDatasetZipfileApiController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Api\Datasets;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\Web\Published\Datasets\ViewsAndDownloads;
 use App\Models\Dataset;
 use Illuminate\Http\Request;
 
 class DownloadDatasetZipfileApiController extends Controller
 {
+    use ViewsAndDownloads;
+
     public function __invoke(Request $request, Dataset $dataset)
     {
         abort_if(is_null($dataset->published_at), 400, "No such dataset");
