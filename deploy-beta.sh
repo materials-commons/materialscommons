a19dba4c6ae0841098894d48370f24684507ee3c
Glenn Tarcea <gtarcea@materialscommons.miserver.it.umich.edu>
2020-06-10 10:59:08 -0400

    Updates for beta deploy

--- deploy-beta.sh
+++ deploy-beta.sh
@@ -6,7 +6,7 @@ cp -r materialscommons /var/www/html/beta
 cp materialscommons/.env.beta /var/www/html/beta/.env
 chcon -R -h -t httpd_sys_script_rw_t /var/www/html/beta
 cd /var/www/html/beta
-/usr/local/bin/composer install --optimize-autoloader --no-dev
+/usr/local/bin/composer install --optimize-autoloader 
 php artisan config:cache
 #php artisan migrate
 cd ..
