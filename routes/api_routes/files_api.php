4583825b4389899d6e8cf9814042664e1432727a
gtarcea <glenn.tarcea@gmail.com>
2020-06-11 13:19:30 -0400

    Add API support for file upload

--- routes/api_routes/files_api.php
+++ routes/api_routes/files_api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Api\Files\MoveFileApiController;
 use App\Http\Controllers\Api\Files\RenameFileApiController;
 use App\Http\Controllers\Api\Files\ShowFileApiController;
 use App\Http\Controllers\Api\Files\UpdateFileApiController;
+use App\Http\Controllers\Web\Files\UploadFilesWebController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/files', CreateFileApiController::class);
@@ -18,3 +19,4 @@ Route::post('/files/{file}/move', MoveFileApiController::class);
 Route::post('/files/{file}/rename', RenameFileApiController::class);
 Route::get('/projects/{project}/files/{file}/download', DownloadFileApiController::class);
 Route::post('/files/by_path', GetFileByPathApiController::class);
+Route::post('/projects/{project}/files/{file}/upload', UploadFilesWebController::class);
