dc7165e0005d1edffebf13ab04055886db4b9c9c
gtarcea <glenn.tarcea@gmail.com>
2020-06-12 08:50:19 -0400

    Add support for listing published datasets

---
+++ app/Http/Controllers/Api/Datasets/IndexPublishedDatasetsApiController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api\Datasets;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Datasets\DatasetResource;
+use App\Models\Dataset;
+
+class IndexPublishedDatasetsApiController extends Controller
+{
+    public function __invoke()
+    {
+        return DatasetResource::collection(Dataset::whereNotNull('published_at'));
+    }
+}
