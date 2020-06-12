b50738af34ed4574093b68d910784696544cc88f
gtarcea <glenn.tarcea@gmail.com>
2020-06-12 09:09:55 -0400

    Add script for building and pushing the docker container

---
+++ build-docker.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cp .env .env.save
+cp .env.sqlite .env
+php artisan migrate:fresh --seed
+podman build -t docker.io/materialscommons/materialscommons-dev .
+podman push docker.io/materialscommons/materialscommons-dev
+cp .env.save .env
+
