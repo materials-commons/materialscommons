3786d2e35af716a6d2f7ae8f27b83784b2e479b4
gtarcea <glenn.tarcea@gmail.com>
2020-06-15 12:14:04 -0400

    Increase throttle limit to 300

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -52,7 +52,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:60,1',
+            'throttle:300,1',
             'bindings',
             UserCanAccessProject::class,
             FileInProject::class,
