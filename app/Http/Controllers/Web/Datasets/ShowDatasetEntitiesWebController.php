87ee88ee6641ea1eb2deee5557250171b030cdd6
gtarcea <glenn.tarcea@gmail.com>
2020-06-18 12:55:59 -0400

    WIP Show new samples tables when showing samples for a private dataset

--- app/Http/Controllers/Web/Datasets/ShowDatasetEntitiesWebController.php
+++ app/Http/Controllers/Web/Datasets/ShowDatasetEntitiesWebController.php
@@ -2,16 +2,32 @@
 
 namespace App\Http\Controllers\Web\Datasets;
 
+use App\Actions\Entities\CreateUsedActivitiesForEntitiesAction;
 use App\Http\Controllers\Controller;
 use App\Models\Dataset;
 use App\Models\Project;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ShowDatasetEntitiesWebController extends Controller
 {
-    public function __invoke(Request $request, Project $project, $datasetId)
+    public function __invoke(CreateUsedActivitiesForEntitiesAction $createUsedActivities, Project $project, $datasetId)
     {
-        $dataset = Dataset::with(['experiments', 'entities'])->find($datasetId);
-        return view('app.projects.datasets.show', compact('project', 'dataset'));
+        $dataset = Dataset::with(['experiments', 'entities.activities'])->find($datasetId);
+        $activities = DB::table('dataset2entity')
+                        ->where('dataset_id', $datasetId)
+                        ->join('activity2entity', 'dataset2entity.entity_id', '=', 'activity2entity.entity_id')
+                        ->join('activities', 'activity2entity.activity_id', '=', 'activities.id')
+                        ->where('name', '<>', 'Create Samples')
+                        ->select('name')
+                        ->distinct()
+                        ->orderBy('name')
+                        ->get();
+        return view('app.projects.datasets.show', [
+            'project'        => $project,
+            'dataset'        => $dataset,
+            'entities'       => $dataset->entities,
+            'activities'     => $activities,
+            'usedActivities' => $createUsedActivities->execute($activities, $dataset->entities),
+        ]);
     }
 }
--- resources/views/app/projects/datasets/tabs/entities.blade.php
+++ resources/views/app/projects/datasets/tabs/entities.blade.php
@@ -4,37 +4,38 @@
     @endslot
 
     @slot('body')
-        <table id="entities" class="table table-hover" style="width:100%">
-            <thead>
-            <tr>
-                <th>Name</th>
-                <th>Description</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($dataset->entities as $entity)
-                <tr>
-                    <td>
-                        <a href="{{route('projects.entities.show', [$project, $entity])}}">
-                            {{$entity->name}}
-                        </a>
-                    </td>
-                    <td>{{$entity->description}}</td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
+        @include('partials.entities._entities-with-used-activities-table')
+        {{--        <table id="entities" class="table table-hover" style="width:100%">--}}
+        {{--            <thead>--}}
+        {{--            <tr>--}}
+        {{--                <th>Name</th>--}}
+        {{--                <th>Description</th>--}}
+        {{--            </tr>--}}
+        {{--            </thead>--}}
+        {{--            <tbody>--}}
+        {{--            @foreach($dataset->entities as $entity)--}}
+        {{--                <tr>--}}
+        {{--                    <td>--}}
+        {{--                        <a href="{{route('projects.entities.show', [$project, $entity])}}">--}}
+        {{--                            {{$entity->name}}--}}
+        {{--                        </a>--}}
+        {{--                    </td>--}}
+        {{--                    <td>{{$entity->description}}</td>--}}
+        {{--                </tr>--}}
+        {{--            @endforeach--}}
+        {{--            </tbody>--}}
+        {{--        </table>--}}
     @endslot
 @endcomponent
 
-@push('scripts')
-    <script>
-        $(document).ready(function () {
-            $(document).ready(() => {
-                $('#entities').DataTable({
-                    stateSave: true,
-                });
-            });
-        });
-    </script>
-@endpush
+{{--@push('scripts')--}}
+{{--    <script>--}}
+{{--        $(document).ready(function () {--}}
+{{--            $(document).ready(() => {--}}
+{{--                $('#entities').DataTable({--}}
+{{--                    stateSave: true,--}}
+{{--                });--}}
+{{--            });--}}
+{{--        });--}}
+{{--    </script>--}}
+{{--@endpush--}}
