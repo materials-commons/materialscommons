bd4941beb83c4eb2891278311ab7b4a0d33db670
gtarcea <glenn.tarcea@gmail.com>
2020-06-18 12:09:11 -0400

    Increase max items to 5000

--- config/globus.php
+++ config/globus.php
@@ -4,6 +4,6 @@ return [
     'cc_user'   => env('MC_GLOBUS_CC_USER'),
     'cc_token'  => env('MC_GLOBUS_CC_TOKEN'),
     'endpoint'  => env("MC_GLOBUS_ENDPOINT_ID"),
-    'max_items' => env('GLOBUS_MAX_ITEMS', 500),
+    'max_items' => env('GLOBUS_MAX_ITEMS', 5000),
 ];
 
