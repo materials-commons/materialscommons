c814ccdbfe3dd59acc8df6451130e93597aa9e2b
gtarcea <glenn.tarcea@gmail.com>
2020-06-18 09:27:08 -0400

    Check if directory already exists in path and return it instead of creating a second one.

--- app/Actions/Directories/CreateDirectoryAction.php
+++ app/Actions/Directories/CreateDirectoryAction.php
@@ -14,6 +14,19 @@ class CreateDirectoryAction
         $data['media_type_description'] = 'directory';
         $parent = File::findOrFail($data['directory_id']);
         $data['path'] = PathHelpers::normalizePath("{$parent->path}/{$data['name']}");
+
+        $dir = $this->getExistingDirIfExists($data['path'], $parent->project_id);
+        if (!is_null($dir)) {
+            return $dir;
+        }
+
         return File::create($data);
+    }
+
+    private function getExistingDirIfExists($path, $projectId)
+    {
+        return File::where('path', $path)
+                   ->where('project_id', $projectId)
+                   ->first();
     }
 }
