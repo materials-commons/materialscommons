7c372398d5a0af480c39844594eb822996223ba1
gtarcea <glenn.tarcea@gmail.com>
2020-06-18 13:19:42 -0400

    Remove commented out code

--- resources/views/app/projects/datasets/tabs/entities.blade.php
+++ resources/views/app/projects/datasets/tabs/entities.blade.php
@@ -5,37 +5,5 @@
 
     @slot('body')
         @include('partials.entities._entities-with-used-activities-table')
-        {{--        <table id="entities" class="table table-hover" style="width:100%">--}}
-        {{--            <thead>--}}
-        {{--            <tr>--}}
-        {{--                <th>Name</th>--}}
-        {{--                <th>Description</th>--}}
-        {{--            </tr>--}}
-        {{--            </thead>--}}
-        {{--            <tbody>--}}
-        {{--            @foreach($dataset->entities as $entity)--}}
-        {{--                <tr>--}}
-        {{--                    <td>--}}
-        {{--                        <a href="{{route('projects.entities.show', [$project, $entity])}}">--}}
-        {{--                            {{$entity->name}}--}}
-        {{--                        </a>--}}
-        {{--                    </td>--}}
-        {{--                    <td>{{$entity->description}}</td>--}}
-        {{--                </tr>--}}
-        {{--            @endforeach--}}
-        {{--            </tbody>--}}
-        {{--        </table>--}}
     @endslot
 @endcomponent
-
-{{--@push('scripts')--}}
-{{--    <script>--}}
-{{--        $(document).ready(function () {--}}
-{{--            $(document).ready(() => {--}}
-{{--                $('#entities').DataTable({--}}
-{{--                    stateSave: true,--}}
-{{--                });--}}
-{{--            });--}}
-{{--        });--}}
-{{--    </script>--}}
-{{--@endpush--}}
