20cf172d89d2b001630d5877bc16234a6070aa03
gtarcea <glenn.tarcea@gmail.com>
2020-07-06 15:30:07 -0400

    Fix delete experiment to also remove attributes and attribute values

--- app/Actions/Experiments/DeleteExperimentAction.php
+++ app/Actions/Experiments/DeleteExperimentAction.php
@@ -2,6 +2,9 @@
 
 namespace App\Actions\Experiments;
 
+use App\Models\Activity;
+use App\Models\Entity;
+use App\Models\EntityState;
 use App\Models\Experiment;
 use Illuminate\Support\Facades\DB;
 
@@ -10,7 +13,17 @@ class DeleteExperimentAction
     public function __invoke(Experiment $experiment)
     {
         DB::transaction(function () use ($experiment) {
+            $experiment->entities()->get()->each(function (Entity $entity) {
+                $entity->load('entityStates.attributes');
+                $entity->entityStates->each(function (EntityState $es) {
+                    $es->attributes()->delete();
+                });
+            });
             $experiment->entities()->delete();
+            $experiment->activities()->get()->each(function (Activity $activity) {
+                $activity->load('attributes');
+                $activity->attributes()->delete();
+            });
             $experiment->activities()->delete();
             $experiment->delete();
         });
