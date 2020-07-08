f7ea9273e6822efca5227108f26bf81147642ca5
gtarcea <glenn.tarcea@gmail.com>
2020-07-08 15:06:57 -0400

    Refer to fname rather than name for the file

--- resources/views/partials/activities/_activity-type-files.blade.php
+++ resources/views/partials/activities/_activity-type-files.blade.php
@@ -1,7 +1,7 @@
 <ul>
     @foreach($files as $file)
         <li>
-            <a href="{{route('projects.files.show', [$project, $file->fid])}}">{{$file->name}}</a>
+            <a href="{{route('projects.files.show', [$project, $file->fid])}}">{{$file->fname}}</a>
         </li>
     @endforeach
 </ul>
