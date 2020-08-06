fd4d9cbc07de3ce291ea96ba5e61598921e4e9ca
gtarcea <glenn.tarcea@gmail.com>
2020-08-06 11:08:43 -0400

    Add alpinejs and remove intercooler and turbolinks

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,6 +19,7 @@ window.mcutil = require('./util');
 // window.Intercooler = require('intercooler');
 window.xlsx = require('xlsx');
 window.datagrid = require('canvas-datagrid');
+require('alpinejs');
 
 window.Uppy = require('@uppy/core');
 window.UppyXHRUpload = require('@uppy/xhr-upload');
