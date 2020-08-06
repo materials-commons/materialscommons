f24a9023d296f4ec5d1519b915e5ba14509f10a1
gtarcea <glenn.tarcea@gmail.com>
2020-08-06 12:34:56 -0400

    public dataset files switched to cards

--- resources/views/public/datasets/entities/_activity-type-files.blade.php
+++ resources/views/public/datasets/entities/_activity-type-files.blade.php
@@ -1,7 +1,46 @@
 <ul>
-    @foreach($files as $f)
-        <li>
-            <a href="{{route('public.datasets.files.show', [$dataset, $f->fid])}}">{{$f->fname}}</a>
-        </li>
+    @foreach($files as $file)
+        @if($loop->iteration < 12)
+            <li>
+                <a href="{{route('public.datasets.files.show', [$dataset, $file->fid])}}">{{$file->fname}}</a>
+                @if(in_array($file->mime_type, ["image/gif", "image/jpeg", "image/png", "image/tiff", "image/x-ms-bmp","image/bmp"]))
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="text-center">
+                                    <a href="{{route('public.datasets.files.display', [$dataset, $file->fid])}}">
+                                        <img src="{{route('public.datasets.files.display', [$dataset, $file->fid])}}"
+                                             class="img-fluid">
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endif
+            </li>
+        @else
+            <li class="{{$activityType->name}}" hidden>
+                <a href="{{route('public.datasets.files.show', [$dataset, $file->fid])}}">{{$file->fname}}</a>
+                @if(in_array($file->mime_type, ["image/gif", "image/jpeg", "image/png", "image/tiff", "image/x-ms-bmp","image/bmp"]))
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="text-center">
+                                    <a href="{{route('public.datasets.files.display', [$dataset, $file->fid])}}">
+                                        <img src="{{route('public.datasets.files.display', [$dataset, $file->fid])}}"
+                                             class="img-fluid">
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endif
+            </li>
+        @endif
     @endforeach
+    @include('common.show-more-control', [
+        'items' => $files,
+        'attrName' => $activityType->name,
+        'msg' => 'files...'
+    ])
 </ul>
