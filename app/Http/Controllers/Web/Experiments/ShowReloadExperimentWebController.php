63b7f2baa5fa013061e8fcb662c88c4d092a0aff
gtarcea <glenn.tarcea@gmail.com>
2020-08-13 10:51:32 -0400

    Phase 1 for handling shared items between datasets and experiments that are affected by reload. In this phase we warn about affected unpublished datasets. If there are affected published datasets then the reload is not allowed. In both cases we show a list of the datasets that are affected.

--- app/Http/Controllers/Web/Experiments/ShowReloadExperimentWebController.php
+++ app/Http/Controllers/Web/Experiments/ShowReloadExperimentWebController.php
@@ -3,19 +3,67 @@
 namespace App\Http\Controllers\Web\Experiments;
 
 use App\Http\Controllers\Controller;
+use App\Models\Dataset;
 use App\Models\Experiment;
 use App\Models\Project;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 
 class ShowReloadExperimentWebController extends Controller
 {
     public function __invoke(Project $project, Experiment $experiment)
     {
-        $excelFiles = $project
-            ->files()
-            ->with('directory')
-            ->where('mime_type', "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
-            ->where('current', true)
-            ->get();
-        return view('app.projects.experiments.reload', compact('project', 'experiment', 'excelFiles'));
+        $datasetIds = $this->getDatasetsListSharingEntitiesWithExperiment($experiment);
+
+        return view('app.projects.experiments.reload', [
+            'project'             => $project,
+            'experiment'          => $experiment,
+            'excelFiles'          => $this->getProjectExcelFiles($project),
+            'publishedDatasets'   => $this->getAffectedPublishedDatasets($datasetIds),
+            'unpublishedDatasets' => $this->getAffectedUnpublishedDatasets($datasetIds),
+        ]);
+    }
+
+    private function getProjectExcelFiles(Project $project)
+    {
+        return $project->files()
+                       ->with('directory')
+                       ->where('mime_type', "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+                       ->where('current', true)
+                       ->get();
+    }
+
+    private function getDatasetsListSharingEntitiesWithExperiment(Experiment $experiment)
+    {
+        $experiment->load('entities');
+        return DB::table('dataset2entity')
+                 ->select('dataset_id')
+                 ->whereIn('entity_id',
+                     $experiment->entities->pluck('id')->toArray())
+                 ->distinct()
+                 ->get()
+                 ->pluck('dataset_id');
+    }
+
+    private function getAffectedPublishedDatasets(Collection $datasetIds)
+    {
+        if ($datasetIds->isEmpty()) {
+            return collect([]);
+        }
+
+        return Dataset::whereIn('id', $datasetIds->toArray())
+                      ->whereNotNull('published_at')
+                      ->get();
+    }
+
+    private function getAffectedUnpublishedDatasets(Collection $datasetIds)
+    {
+        if ($datasetIds->isEmpty()) {
+            return collect([]);
+        }
+
+        return Dataset::whereIn('id', $datasetIds->toArray())
+                      ->whereNull('published_at')
+                      ->get();
     }
 }
--- resources/views/app/projects/experiments/reload.blade.php
+++ resources/views/app/projects/experiments/reload.blade.php
@@ -15,35 +15,69 @@
         @endslot
 
         @slot('body')
-            <form method="post"
-                  action="{{route('projects.experiments.reload', [$project, $experiment])}}"
-                  id="experiment-reload">
-                @csrf
-                @method('put')
-                <label for="file_id">Reload Experiment From</label>
-                <select name="file_id" class="selectpicker col-lg-10" data-live-search="true"
-                        title="Select Spreadsheet">
-                    @foreach($excelFiles as $file)
-                        @if ($file->directory->path === "/")
-                            <option data-tokens="{{$file->id}}" value="{{$file->id}}">/{{$file->name}}</option>
-                        @else
-                            <option data-tokens="{{$file->id}}" value="{{$file->id}}">{{$file->directory->path}}
-                                /{{$file->name}}</option>
-                        @endif
-                    @endforeach
-                </select>
-                <div class="float-right">
-                    <a href="{{route('projects.experiments.show', [$project, $experiment])}}"
-                       class="action-link danger mr-3">
-                        Cancel
-                    </a>
+            @if($publishedDatasets->isEmpty())
+                <form method="post"
+                      action="{{route('projects.experiments.reload', [$project, $experiment])}}"
+                      id="experiment-reload">
+                    @csrf
+                    @method('put')
+                    <label for="file_id">Reload Experiment From</label>
+                    <select name="file_id" class="selectpicker col-lg-10" data-live-search="true"
+                            title="Select Spreadsheet">
+                        @foreach($excelFiles as $file)
+                            @if ($file->directory->path === "/")
+                                <option data-tokens="{{$file->id}}" value="{{$file->id}}">/{{$file->name}}</option>
+                            @else
+                                <option data-tokens="{{$file->id}}" value="{{$file->id}}">{{$file->directory->path}}
+                                    /{{$file->name}}</option>
+                            @endif
+                        @endforeach
+                    </select>
+                    <div class="float-right">
+                        <a href="{{route('projects.experiments.show', [$project, $experiment])}}"
+                           class="action-link danger mr-3">
+                            Cancel
+                        </a>
 
-                    <a class="action-link mr-3" href="#"
-                       onclick="document.getElementById('experiment-reload').submit()">
-                        Reload
-                    </a>
-                </div>
-            </form>
+                        <a class="action-link mr-3" href="#"
+                           onclick="document.getElementById('experiment-reload').submit()">
+                            Reload
+                        </a>
+                    </div>
+                </form>
+
+                @if($unpublishedDatasets->isNotEmpty())
+                    <br>
+                    <br>
+                    <p>
+                        The following unpublished datasets share samples with this experiment. If you choose to reload
+                        the
+                        experiments then the shared samples will need to manually be added back to the datasets.
+                    </p>
+                    <h5>Affected Unpublished Datasets</h5>
+                    <ul>
+                        @foreach($unpublishedDatasets as $ds)
+                            <li>
+                                <a href="{{route('projects.datasets.show', [$project, $ds])}}">{{$ds->name}}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+                @endif
+            @else
+                <br>
+                <p>
+                    This experiment cannot be reloaded. It contains published datasets that would be affected
+                    by the reload.
+                </p>
+                <h5>Affected Published Datasets</h5>
+                <ul>
+                    @foreach($publishedDatasets as $ds)
+                        <li>
+                            <a href="{{route('projects.datasets.show', [$project, $ds])}}">{{$ds->name}}</a>
+                        </li>
+                    @endforeach
+                </ul>
+            @endif
         @endslot
     @endcomponent
 @stop
