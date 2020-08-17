75294d9ecd78fad56c1bde4e888689a802670d54
gtarcea <glenn.tarcea@gmail.com>
2020-08-17 12:50:02 -0400

    Add prompt to deploy to ask about unit tests

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,14 @@
-#!/bin/sh -x
+#!/usr/bin/env bash
+
+read -p "Did you run all unit tests? [y/n] " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+  echo "Stopping so you can run unit tests and make sure they all pass."
+  exit 0
+fi
+
+set -x
+
 rm -rf /var/www/html/materialscommons.old
 mv /var/www/html/materialscommons /var/www/html/materialscommons.old
 cd ..
