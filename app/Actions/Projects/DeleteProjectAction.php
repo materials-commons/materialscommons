afd6b777dc12dd86abc6189e049995def3fcafb0
gtarcea <glenn.tarcea@gmail.com>
2020-08-17 14:45:00 -0400

    Make sure the team is deleted when the project is deleted

--- app/Actions/Projects/DeleteProjectAction.php
+++ app/Actions/Projects/DeleteProjectAction.php
@@ -3,6 +3,7 @@
 namespace App\Actions\Projects;
 
 use App\Models\Project;
+use Illuminate\Support\Facades\DB;
 
 class DeleteProjectAction
 {
@@ -15,6 +16,10 @@ class DeleteProjectAction
      */
     public function __invoke(Project $project)
     {
-        $project->delete();
+        DB::transaction(function () use ($project) {
+            $team = $project->team;
+            $project->delete();
+            $team->delete();
+        });
     }
 }
