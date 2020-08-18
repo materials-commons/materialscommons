3a7c6b75b4e54a47a66635b0d36c8fe395c395c7
gtarcea <glenn.tarcea@gmail.com>
2020-08-18 10:28:10 -0400

    When deleting a project explicitly delete the files

--- app/Actions/Projects/DeleteProjectAction.php
+++ app/Actions/Projects/DeleteProjectAction.php
@@ -18,6 +18,7 @@ class DeleteProjectAction
     {
         DB::transaction(function () use ($project) {
             $team = $project->team;
+            $project->files()->delete();
             $project->delete();
             $team->delete();
         });
