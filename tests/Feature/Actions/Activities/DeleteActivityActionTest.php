8b9e0bb930618985745fe12cc25afcd9e55bc6c9
gtarcea <glenn.tarcea@gmail.com>
2020-08-19 17:06:56 -0400

    WIP - Start setting up unit tests for activity delete

---
+++ tests/Feature/Actions/Activities/DeleteActivityActionTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature\Actions\Activities;
+
+use Tests\TestCase;
+
+class DeleteActivityActionTest extends TestCase
+{
+    /** @test */
+    public function delete_removes_the_activity_attributes()
+    {
+//        $response = $this->get('/');
+//
+//        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function delete_removes_activity_attributes_and_values()
+    {
+    }
+}
