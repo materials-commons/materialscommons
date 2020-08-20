a6bf4a411f053cc36fa16bf18e6935d261f1c332
gtarcea <glenn.tarcea@gmail.com>
2020-08-20 15:09:03 -0400

    Mark tests as not implemented

--- tests/Feature/Actions/Activities/DeleteActivityActionTest.php
+++ tests/Feature/Actions/Activities/DeleteActivityActionTest.php
@@ -9,13 +9,12 @@ class DeleteActivityActionTest extends TestCase
     /** @test */
     public function delete_removes_the_activity_attributes()
     {
-//        $response = $this->get('/');
-//
-//        $response->assertStatus(200);
+        $this->fail('Not implemented');
     }
 
     /** @test */
     public function delete_removes_activity_attributes_and_values()
     {
+        $this->fail('Not implemented');
     }
 }
