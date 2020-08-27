8f1c3059406dff9aa09e3546f0ec4315a55fc82e
gtarcea <glenn.tarcea@gmail.com>
2020-08-27 13:25:28 -0400

    Add changes for server info updates to deploy-beta.sh and fix references in deploy.sh to refer to .env

--- deploy-beta.sh
+++ deploy-beta.sh
@@ -30,7 +30,7 @@ cp -r materialscommons /var/www/html/beta
 cp materialscommons/.env.beta /var/www/html/beta/.env
 chcon -R -h -t httpd_sys_script_rw_t /var/www/html/beta
 cd /var/www/html/beta
-/usr/local/bin/composer install --optimize-autoloader 
+/usr/local/bin/composer install --optimize-autoloader
 php artisan config:cache
 #php artisan migrate
 cd ..
