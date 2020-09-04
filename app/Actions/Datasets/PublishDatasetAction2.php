47b215cf083613a824622017617012fde215c0c5
gtarcea <glenn.tarcea@gmail.com>
2020-09-04 11:38:48 -0400

    Create a new version of the dataset publisher that also replicates entities and their relationships

---
+++ app/Actions/Datasets/PublishDatasetAction2.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Actions\Datasets;
+
+use App\Actions\Globus\GlobusApi;
+use App\Models\Dataset;
+
+class PublishDatasetAction2
+{
+    /**
+     * @var \App\Actions\Globus\GlobusApi
+     */
+    private $globusApi;
+
+    public function __construct(GlobusApi $globusApi)
+    {
+        $this->globusApi = $globusApi;
+    }
+
+    public function execute(Dataset $dataset)
+    {
+        $this->replicateFiles($dataset);
+        $this->replicateEntitiesAndRelatedItems($dataset);
+        $this->buildGlobusDownload($dataset);
+        $this->buildZipfile($dataset);
+    }
+
+    private function replicateFiles(Dataset $dataset)
+    {
+        $createDatasetFilesTableAction = new CreateDatasetFilesTableAction();
+        $createDatasetFilesTableAction->execute($dataset);
+    }
+
+    private function replicateEntitiesAndRelatedItems(Dataset $dataset)
+    {
+        $replicator = new ReplicateDatasetEntitiesAndRelationshipsAction();
+        $replicator->execute($dataset);
+    }
+
+    private function buildGlobusDownload(Dataset $dataset)
+    {
+        $createDatasetInGlobusAction = new CreateDatasetInGlobusAction($this->globusApi);
+        $createDatasetInGlobusAction($dataset, false);
+    }
+
+    private function buildZipfile(Dataset $dataset)
+    {
+        $createDatasetZipfileAction = new CreateDatasetZipfileAction();
+        $createDatasetZipfileAction($dataset);
+    }
+}
---
+++ app/Actions/Datasets/ReplicateDatasetEntitiesAndRelationshipsAction.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Actions\Datasets;
+
+use App\Models\Activity;
+use App\Models\Attribute;
+use App\Models\AttributeValue;
+use App\Models\Dataset;
+use App\Models\Entity;
+use App\Models\EntityState;
+use Illuminate\Support\Carbon;
+use Ramsey\Uuid\Uuid;
+
+class ReplicateDatasetEntitiesAndRelationshipsAction
+{
+    public function execute(Dataset $dataset)
+    {
+        $this->replicateEntitiesAndRelatedItems($dataset);
+    }
+
+    private function replicateEntitiesAndRelatedItems(Dataset $dataset)
+    {
+        $dataset->entitiesFromTemplate()->each(function (Entity $entity) {
+            $entity->load('entityStates.attributes.values', 'activities.attributes.values');
+            $e = $entity->replicate()->fill([
+                'uuid'      => $this->uuid(),
+                'copied_at' => Carbon::now(),
+                'copied_id' => $entity->id,
+            ]);
+            $e->save();
+            $this->replicateEntityStatesAndRelationshipsForEntity($entity, $e);
+            $this->replicateActivitiesAndRelationshipsForEntity($entity, $e);
+        });
+    }
+
+    private function replicateEntityStatesAndRelationshipsForEntity(Entity $entity, Entity $e)
+    {
+        $entity->entityStates->each(function (EntityState $entityState) use ($e) {
+            $es = $entityState->replicate()->fill([
+                'uuid'      => $this->uuid(),
+                'entity_id' => $e->id,
+            ]);
+            $es->save();
+            $entityState->attributes->each(function (Attribute $attribute) use ($es) {
+                $a = $attribute->replicate()->fill([
+                    'uuid'            => $this->uuid(),
+                    'attributable_id' => $es->id,
+                ]);
+                $a->save();
+                $a->values->each(function (AttributeValue $attributeValue) use ($a) {
+                    $av = $attributeValue->replicate()->fill([
+                        'uuid'         => $this->uuid(),
+                        'attribute_id' => $a->id,
+                    ]);
+                    $av->save();
+                });
+            });
+        });
+    }
+
+    private function replicateActivitiesAndRelationshipsForEntity(Entity $entity, Entity $e)
+    {
+        $entity->activities->each(function (Activity $activity) use ($e) {
+            $newActivity = $activity->replicate()->fill([
+                'uuid'      => $this->uuid(),
+                'copied_at' => Carbon::now(),
+                'copied_id' => $activity->id,
+            ]);
+            $newActivity->save();
+            $e->activities()->attach($newActivity);
+            $activity->attributes->each(function (Attribute $attribute) use ($newActivity) {
+                $a = $attribute->replicate()->fill([
+                    'uuid'            => $this->uuid(),
+                    'attributable_id' => $newActivity->id,
+                ]);
+                $a->save();
+                $a->values->each(function (AttributeValue $attributeValue) use ($a) {
+                    $av = $attributeValue->replicate()->fill([
+                        'uuid'         => $this->uuid(),
+                        'attribute_id' => $a->id,
+                    ]);
+                    $av->save();
+                });
+            });
+        });
+    }
+
+    private function uuid()
+    {
+        return Uuid::uuid4()->toString();
+    }
+}
