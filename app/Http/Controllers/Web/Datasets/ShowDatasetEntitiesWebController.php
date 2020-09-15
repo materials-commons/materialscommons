abb3d3a4507525344b76c9e8cd2f2ca1628fd40b
gtarcea <glenn.tarcea@gmail.com>
2020-09-15 11:20:06 -0400

    Handle published and unpublished datasets

--- app/Http/Controllers/Web/Datasets/ShowDatasetEntitiesWebController.php
+++ app/Http/Controllers/Web/Datasets/ShowDatasetEntitiesWebController.php
@@ -5,41 +5,35 @@ namespace App\Http\Controllers\Web\Datasets;
 use App\Actions\Entities\CreateUsedActivitiesForEntitiesAction;
 use App\Http\Controllers\Controller;
 use App\Models\Dataset;
+use App\Models\Entity;
 use App\Models\Project;
-use Illuminate\Support\Facades\DB;
 
 class ShowDatasetEntitiesWebController extends Controller
 {
     public function __invoke(CreateUsedActivitiesForEntitiesAction $createUsedActivities, Project $project, $datasetId)
     {
-        $dataset = Dataset::with(['experiments', 'entities.activities', 'tags'])->find($datasetId);
-        $activities = DB::table('dataset2entity')
-                        ->where('dataset_id', $datasetId)
-                        ->join('activity2entity', 'dataset2entity.entity_id', '=', 'activity2entity.entity_id')
-                        ->join('activities', 'activity2entity.activity_id', '=', 'activities.id')
-                        ->where('name', '<>', 'Create Samples')
-                        ->select('name')
-                        ->distinct()
-                        ->orderBy('name')
-                        ->get();
+        $dataset = Dataset::with(['experiments', 'tags'])->find($datasetId);
+        $entities = $this->getEntitiesForDataset($dataset);
+        $activities = Entity::activityNamesForEntities($entities);
         return view('app.projects.datasets.show', [
             'project'        => $project,
             'dataset'        => $dataset,
-            'entities'       => $dataset->entities,
+            'entities'       => $entities,
             'activities'     => $activities,
-            'usedActivities' => $createUsedActivities->execute($activities, $dataset->entities),
+            'usedActivities' => $createUsedActivities->execute($activities, $entities),
         ]);
     }
+
+    private function getEntitiesForDataset($dataset)
+    {
+        // unpublished dataset so get entities from template
+        if (is_null($dataset->published_at)) {
+            return $dataset->entitiesFromTemplate();
+        }
+
+        // published dataset so get the entities that were associated with
+        // the dataset from iterating through the template
+        $dataset->load('entities');
+        return $dataset->entities;
+    }
 }
-/*
- * Example query to get entities for a dataset
- *
- select * from entities e where e.id in (
-select entity_id from experiment2entity where experiment_id in (
-    select experiment_id from item2entity_selection where item_id = 130
-))
-and e.name in
-    (select entity_name from item2entity_selection
-    where item_id = 130 and experiment_id in
-                            (select experiment_id from item2entity_selection where item_id = 130))
- */
