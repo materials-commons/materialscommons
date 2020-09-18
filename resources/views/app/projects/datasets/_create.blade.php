1eded40c42c2b91b21ebd2189b864efe0941476f
gtarcea <glenn.tarcea@gmail.com>
2020-09-18 16:37:26 -0400

    WIP - Working on author ordering and incorporating organizations

--- resources/views/app/projects/datasets/_create.blade.php
+++ resources/views/app/projects/datasets/_create.blade.php
@@ -11,8 +11,8 @@
             <label for="authors">Authors</label>
             <div class="drag-and-drop col-12" x-data="{adding: false, removing: false}">
                 <div class="drag-and-drop__container drag-and-drop__container--from">
-                    <h3 class="drag-and-drop__title">Project Members</h3>
-                    <ul class="drag-and-drop__items list-unstyled"
+                    <h3 class="drag-and-drop__title">Dataset Authors (drag to re-order)</h3>
+                    <ul class="drag-and-drop__items list-unstyled" id="dataset-authors"
                         :class="{'drag-and-drop__items--removing': removing}"
                         x-on:drop="removing = false"
                         x-on:drop.prevent="
@@ -34,9 +34,8 @@
                         "
                             x-on:dragend="dragging = false"
                             draggable="true">
-                            <span class="ml-3"><i class="fa fas fa-fw fa-user"></i></span>
-                            {{$project->owner->name}} (Owner)
-                            <input name="mc_authors[]" value="{{$project->owner->id}}" type="text" hidden>
+                            <span><i class="fa fas fa-fw fa-user"></i></span>
+                            {{$project->owner->name}} (Owner)<sup>1</sup>
                         </li>
                         @foreach($project->team->members->merge($project->team->admins)->sortBy('name') as $author)
                             @if($author->id != auth()->id())
@@ -51,40 +50,45 @@
                                 "
                                     x-on:dragend="dragging = false"
                                     draggable="true">
-
-                                    <div style="margin-left: 2.15rem;">
-                                        {{$author->name}}
-                                        <input name="mc_authors[]" value="{{$author->id}}" type="text" hidden>
-                                    </div>
+                                    {{$author->name}}<sup>1</sup>
                                 </li>
                             @endif
                         @endforeach
                     </ul>
                 </div>
-                <div class="drag-and-drop__divider">⇄</div>
-                <div class="drag-and-drop__container drag-and-drop__container--to">
-                    <h3 class="drag-and-drop__title">Dataset Authors</h3>
-                    <ul class="drag-and-drop__items list-unstyled"
-                        :class="{'drag-and-drop__items--adding': adding}"
-                        x-on:drop="adding = false"
-                        x-on:drop.prevent="
-                        const target = event.target.closest('ul');
-                        const element = document.getElementById(event.dataTransfer.getData('text/plain'));
-                        target.appendChild(element);
-                    "
-                        x-on:dragover.prevent="adding = true" ,
-                        x-on:dragleave.prevent="adding = false"
-                        id="dataset-authors">
-                    </ul>
-                </div>
             </div>
         </div>
+        <h3>Select Authors (from project and external authors)</h3>
+        <ul class="list-unstyled" id="select-authors">
+            <li>
+                <span class="ml-3"><i class="fa fas fa-fw fa-user"></i></span>
+                {{$project->owner->name}} (Owner)
+                <input name="mc_authors[]" value="{{$project->owner->id}}" type="text" hidden>
+            @foreach($project->team->members->merge($project->team->admins)->sortBy('name') as $author)
+                @if($author->id != auth()->id())
+                    <li>
+                        <div style="margin-left: 2.15rem;">
+                            <input type="checkbox" class="form-check-input"
+                                   onclick="checkboxClicked(this, '{{$author->uuid}}', '{{$author->name}}')"
+                                   value="{{$author->id}}" name="mc_authors[]" checked>
+                            {{$author->name}}
+                        </div>
+                    </li>
+                @endif
+            @endforeach
+        </ul>
+        <h3>Organizations</h3>
+        <ul class="list-unstyled" id="organizations">
+            @foreach($project->team->members->merge($project->team->admins)->pluck('affiliations')->filter()->unique() as $org)
+                <li>{{$loop->iteration}}. {{$org}}</li>
+            @endforeach
+        </ul>
     </div>
 
     <div class="form-group">
         <label for="additional_authors">Additional Authors</label>
         <div class="">
-            <a href="#" onclick="addAdditionalAuthor()"><i class="fa fas fa-fw fa-plus"></i>Add Author</a>
+            <a href="#" onclick="addAdditionalAuthor()"><i class="fa fas fa-fw fa-plus"></i>Add External Author</a>
             <ul class="list-unstyled" id="additional_authors"></ul>
         </div>
     </div>
@@ -262,7 +266,31 @@
         }
 
         function addAdditionalAuthor() {
-            $('#dataset-authors').append(`<li id="${nextAdditionalAuthorId}"
+            $('#additional_authors').append(`<li id="${nextAdditionalAuthorId}">
+                <div class="form-row mt-2">
+                    <a href="#" onclick="removeAuthor('${nextAdditionalAuthorId}')"><i class="fa fas fa-fw fa-trash"></i></a>
+                    <div class="col">
+                        <input class="form-control" name="additional_authors['id_${nextAdditionalAuthorId}'][name]" type="text" placeholder="Name...(Required)"
+                                id="name_${nextAdditionalAuthorId}" required>
+                    </div>
+                    <div class="col">
+                        <input class="form-control" name="additional_authors['id_${nextAdditionalAuthorId}'][email]" type="email"
+                            placeholder="Email...(Required)" id="email_${nextAdditionalAuthorId}" required>
+                    </div>
+                    <div class="col">
+                        <input class="form-control" name="additional_authors['id_${nextAdditionalAuthorId}'][affiliations]"
+                            type="text" placeholder="Organization...(Required)"
+                            required id="org_${nextAdditionalAuthorId}">
+                        <a class="action" href="#" onclick="addExternalUser('${nextAdditionalAuthorId}')">Done</a>
+                    </div>
+                </div>
+            </li>`);
+            nextAdditionalAuthorId++;
+        }
+
+        function checkboxClicked(element, id, name) {
+            if (element.checked) {
+                $('#dataset-authors').append(`<li id="${id}"
                                         x-data="{dragging: false}"
                                         class="drag-and-drop__item"
                                         :class="{ 'drag-and-drop__item--dragging': dragging}"
@@ -273,21 +301,44 @@
                                         "
                                         x-on:dragend="dragging = false"
                                         draggable="true">
-                <div class="form-row mt-2">
-                    <a href="#" onclick="removeAuthor('${nextAdditionalAuthorId}')"><i class="fa fas fa-fw fa-trash"></i></a>
-                    <div class="col">
-                        <input class="form-control" name="additional_authors['id_${nextAdditionalAuthorId}'][name]" type="text" placeholder="Name...(Required)" required>
-                    </div>
-                    <div class="col">
-                        <input class="form-control" name="additional_authors['id_${nextAdditionalAuthorId}'][email]" type="email" placeholder="Email...(Required)" required>
-                    </div>
-                    <div class="col">
-                        <input class="form-control" name="additional_authors['id_${nextAdditionalAuthorId}'][affiliations]" type="text" placeholder="Organization...(Required)" required>
-                    </div>
-                </div>
-            </li>`);
-            nextAdditionalAuthorId++;
+                                            ${name}
+                                    </li>`);
+            } else {
+                $(`#${id}`).remove();
+            }
         }
+
+        function addExternalUser(id) {
+            let org = $(`#org_${id}`)[0].value;
+            let name = $(`#name_${id}`)[0].value;
+            $('#organizations').append(`<li>2. ${org}</li>`);
+            $('#dataset-authors').append(`<li id="ex_${id}"
+                                        x-data="{dragging: false}"
+                                        class="drag-and-drop__item"
+                                        :class="{ 'drag-and-drop__item--dragging': dragging}"
+                                        x-on:dragstart.self="
+                                            dragging = true;
+                                            event.dataTransfer.effectAllowed = 'move';
+                                            event.dataTransfer.setData('text/plain', event.target.id);
+                                        "
+                                        x-on:dragend="dragging = false"
+                                        draggable="true">
+                            ${name}<sup>2</sup>
+                        </li>`);
+            $('#select-authors').append(`<li><a class="action ml-3" href="#"><i class="fa fa-fw fas fa-trash"></i></a> ${name} (External Author)`);
+            $(`#${id}`).hide();
+        }
+
+        /*
+        <li>
+                        <div style="margin-left: 2.15rem;">
+                            <input type="checkbox" class="form-check-input"
+                                   onclick="checkboxClicked(this, '{{$author->uuid}}', '{{$author->name}}')"
+                                   value="{{$author->id}}" name="mc_authors[]" checked>
+                            {{$author->name}}
+        </div>
+    </li>
+*/
 
     </script>
 @endpush
