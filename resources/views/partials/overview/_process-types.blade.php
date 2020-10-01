455ed6ed03007a2a53f238055d68398d2c2c9891
gtarcea <glenn.tarcea@gmail.com>
2020-10-01 13:18:45 -0400

    Only show process types if there are process types to show

--- resources/views/partials/overview/_process-types.blade.php
+++ resources/views/partials/overview/_process-types.blade.php
@@ -1,11 +1,13 @@
-<div class="form-group">
-    <label for="process-types">Process Types ({{sizeof($activitiesGroup)}})</label>
-    <ul class="list-inline">
-        @foreach($activitiesGroup as $ag)
-            <li class="list-inline-item mt-1">
-                {{--                <span class="badge badge-light fs-11">{{$ag->name}} ({{$ag->count}})</span>--}}
-                <span class="grey-5 fs-9">{{$ag->name}} ({{$ag->count}})</span>
-            </li>
-        @endforeach
-    </ul>
-</div>
+@if(isset($activitiesGroup) && sizeof($activitiesGroup) != 0)
+    <div class="form-group">
+        <label for="process-types">Process Types ({{sizeof($activitiesGroup)}})</label>
+        <ul class="list-inline">
+            @foreach($activitiesGroup as $ag)
+                <li class="list-inline-item mt-1">
+                    {{--                <span class="badge badge-light fs-11">{{$ag->name}} ({{$ag->count}})</span>--}}
+                    <span class="grey-5 fs-9">{{$ag->name}} ({{$ag->count}})</span>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+@endif
