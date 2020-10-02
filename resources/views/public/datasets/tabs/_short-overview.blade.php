c998e517ee2a253631d7fb158437dd5ddb00f0dc
gtarcea <glenn.tarcea@gmail.com>
2020-10-02 16:25:06 -0400

    Only show download links for short overview

--- resources/views/public/datasets/tabs/_short-overview.blade.php
+++ resources/views/public/datasets/tabs/_short-overview.blade.php
@@ -1,10 +1,14 @@
 <form>
-    <x-datasets.show-overview :dataset="$dataset">
-        <span class="ml-3 fs-9 grey-5">Size: {{formatBytes($dataset->total_files_size)}}</span>
-    </x-datasets.show-overview>
-    <x-datasets.show-authors :authors="$dataset->authors"/>
-    <x-datasets.show-tags :tags="$dataset->tags"/>
-    <x-show-summary :summary="$dataset->summary"/>
+    <div class="row">
+        <div class="col">
+            <x-datasets.show-download-links :dataset="$dataset"></x-datasets.show-download-links>
+        </div>
+    </div>
+    {{--    <x-datasets.show-overview :dataset="$dataset">--}}
+    {{--        <span class="ml-3 fs-9 grey-5">Size: {{formatBytes($dataset->total_files_size)}}</span>--}}
+    {{--    </x-datasets.show-overview>--}}
+    {{--    <x-datasets.show-authors-short :authors="$dataset->authors"/>--}}
+    {{--    <x-datasets.show-tags :tags="$dataset->tags"/>--}}
+    {{--    <x-show-summary :summary="$dataset->summary"/>--}}
 </form>
 <hr/>
-<br>
