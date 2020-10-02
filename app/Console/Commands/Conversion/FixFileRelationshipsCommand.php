e1ac92927e3dc5a49a0ed29fc566674db37ab104
gtarcea <glenn.tarcea@gmail.com>
2020-10-02 10:59:07 -0400

    Add command to fix file relationships from the conversion where occasionally the wrong was used. This happens when we are looking for files that share a uses_uuid and take the first one rather than matching on the project_id.

---
+++ app/Console/Commands/Conversion/FixFileRelationshipsCommand.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Console\Commands\Conversion;
+
+use App\Models\Activity;
+use App\Models\Entity;
+use App\Models\Experiment;
+use App\Models\File;
+use Illuminate\Console\Command;
+
+class FixFileRelationshipsCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'mc-convert:fix-file-relationships';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->fixActivities();
+        $this->fixEntities();
+        $this->fixExperiments();
+        return 0;
+    }
+
+    private function fixActivities()
+    {
+        foreach (Activity::cursor() as $activity) {
+            $this->fixFileRelationshipsForItem($activity);
+        }
+    }
+
+    private function fixEntities()
+    {
+        foreach (Entity::cursor() as $entity) {
+            $this->fixFileRelationshipsForItem($entity);
+        }
+    }
+
+    private function fixExperiments()
+    {
+        foreach (Experiment::cursor() as $experiment) {
+            $this->fixFileRelationshipsForItem($experiment);
+        }
+    }
+
+    private function fixFileRelationshipsForItem($item)
+    {
+        $cls = get_class($item);
+        echo "Fixing {$cls} {$item->name}/{$item->id}\n";
+        $item->load('files');
+        $item->files->each(function (File $file) use ($item, $cls) {
+            if (is_null($file->project_id)) {
+                // Replicated files in datasets may have null project_id, skip them.
+                return;
+            }
+
+            if ($file->project_id == $item->project_id) {
+                // If the projects match then there is nothing to fix.
+                return;
+            }
+
+            // If we are here then the wrong file was attached to the item.
+            echo "...{$cls} {$item->name}/{$item->id} has wrong file {$file->name}/{$file->id}\n";
+
+            $correctFile = File::where('project_id', $item->project_id)
+                               ->where('uses_uuid', $file->uses_uuid)
+                               ->first();
+
+            if (!is_null($correctFile)) {
+                echo "......Corrected to file {$correctFile->name}/{$correctFile->id}\n";
+                $item->files()->attach($correctFile);
+            }
+
+            $item->files()->detach($file);
+        });
+    }
+}
