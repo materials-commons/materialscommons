1001a4937e0a13f90471f443aa4111121da6f1a6
gtarcea <glenn.tarcea@gmail.com>
2020-10-29 10:55:35 -0400

    Fix project count to use the teams interface to get projects when on the public datasets tab for the dashboard

--- app/Http/Controllers/Web/Dashboard/ShowDashboardPublishedDatasetsWebController.php
+++ app/Http/Controllers/Web/Dashboard/ShowDashboardPublishedDatasetsWebController.php
@@ -4,17 +4,20 @@ namespace App\Http\Controllers\Web\Dashboard;
 
 use App\Http\Controllers\Controller;
 use App\Models\Dataset;
+use App\Traits\Projects\UserProjects;
 use Illuminate\Http\Request;
 
 class ShowDashboardPublishedDatasetsWebController extends Controller
 {
+    use UserProjects;
+
     public function __invoke(Request $request)
     {
         $publishedDatasets = $this->getPublishedDatasets();
         return view('app.dashboard.index', [
             'publishedDatasets'      => $publishedDatasets,
             'publishedDatasetsCount' => $publishedDatasets->count(),
-            'projectsCount'          => auth()->user()->projects()->count(),
+            'projectsCount'          => $this->getUserProjects(auth()->id())->count(),
         ]);
     }
 
