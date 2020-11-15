3b0dc234daac3d9fa67c4531cc5d01591bc3b3fb
gtarcea <glenn.tarcea@gmail.com>
2020-11-15 17:03:39 -0500

    Convert to new component format

--- resources/views/app/projects/experiments/etlrun/show.blade.php
+++ resources/views/app/projects/experiments/etlrun/show.blade.php
@@ -9,8 +9,8 @@
 @section('breadcrumbs', Breadcrumbs::render('projects.experiments.index', $project))
 
 @section('content')
-    @component('components.card')
-        @slot('header')
+    <x-card>
+        <x-slot name="header">
             Show Spreadsheet load for {{$etlRun->files[0]->name}} run on: {{$etlRun->created_at}}
             <a class="action-link float-right" style="cursor: pointer"
                hx-get="{{route('projects.experiments.etl_run.show-log', [$project, $experiment, $etlRun])}}"
@@ -18,15 +18,12 @@
                hx-swap="innerHTML scroll:bottom">
                 <i class="fas fa-plus mr-2"></i>Refresh Log
             </a>
-        @endslot
-
-        @slot('body')
+        </x-slot>
+        <x-slot name="body">
             <div hx-get="{{route('projects.experiments.etl_run.show-log', [$project, $experiment, $etlRun])}}"
                  hx-trigger="load" hx-target="#etl-log">
-                <div id="etl-log" style="overflow-y: auto; overflow-x: auto; height: 70vh">
-
-                </div>
+                <div id="etl-log" style="overflow-y: auto; overflow-x: auto; height: 70vh"></div>
             </div>
-        @endslot
-    @endcomponent
+        </x-slot>
+    </x-card>
 @endsection
