277f4bc71e76c8d06f320f8c2ab73177af429579
gtarcea <glenn.tarcea@gmail.com>
2020-12-01 09:41:48 -0500

    Fix for showing files and directories

--- app/Http/Controllers/Web/Datasets/ShowDatasetAndFolderWebController.php
+++ app/Http/Controllers/Web/Datasets/ShowDatasetAndFolderWebController.php
@@ -4,18 +4,27 @@ namespace App\Http\Controllers\Web\Datasets;
 
 use App\Actions\Datasets\GetDatasetFilesAction;
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\Web\Datasets\Traits\HasExtendedInfo;
 use App\Models\Dataset;
 use App\Models\Project;
+use App\ViewModels\Datasets\ShowDatasetOverviewViewModel;
 
 class ShowDatasetAndFolderWebController extends Controller
 {
+    use HasExtendedInfo;
+
     public function __invoke(Project $project, $datasetId, $folderId)
     {
         $dataset = Dataset::with('tags')->findOrFail($datasetId);
         $getDatasetFilesAction = new GetDatasetFilesAction($dataset->file_selection);
         $filesAndDir = $getDatasetFilesAction($project->id, $folderId);
-        $directory = $filesAndDir["directory"];
-        $files = $filesAndDir["files"];
-        return view('app.projects.datasets.show', compact('project', 'dataset', 'directory', 'files'));
+        $showDatasetOverviewViewModel = (new ShowDatasetOverviewViewModel())
+            ->withProject($project)
+            ->withDataset($dataset)
+            ->withEditRoute(route('projects.datasets.files.edit', [$project, $dataset]))
+            ->withFiles($filesAndDir["files"])
+            ->withDirectory($filesAndDir["directory"])
+            ->withEntities($this->getEntitiesForDataset($dataset));
+        return view('app.projects.datasets.show', $showDatasetOverviewViewModel);
     }
 }
