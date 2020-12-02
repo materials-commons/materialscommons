b68d6effdf58b5d830f40ca3e1c78963f213dcc1
gtarcea <glenn.tarcea@gmail.com>
2020-12-02 10:46:12 -0500

    Change dataset files to only show selected files and add a link to go up one level

--- resources/views/app/projects/datasets/tabs/files.blade.php
+++ resources/views/app/projects/datasets/tabs/files.blade.php
@@ -5,6 +5,15 @@
     @endslot
 
     @slot('body')
+        @if ($directory->path !== '/')
+            <a href="{{route('projects.datasets.show.next', [$project, $dataset, $directory->directory_id])}}"
+               class="mb-3">
+                <i class="fa-fw fas fa-arrow-alt-circle-up mr-2"></i>Go up one level
+            </a>
+            <br>
+            <br>
+        @endif
+
         <table id="files" class="table table-hover" style="width:100%">
             <thead>
             <tr>
@@ -17,32 +26,34 @@
             </thead>
             <tbody>
             @foreach($files as $file)
-                <tr>
-                    <td>
+                @if($file->selected)
+                    <tr>
+                        <td>
+                            @if ($file->mime_type === 'directory')
+                                <a href="{{route('projects.datasets.show.next', [$project, $dataset, $file])}}">
+                                    <i class="fa-fw fas mr-2 fa-folder"></i> {{$file->name}}
+                                </a>
+                            @else
+                                <a href="{{route('projects.files.show', [$project, $file])}}">
+                                    <i class="fa-fw fas mr-2 fa-file"></i>{{$file->name}}
+                                </a>
+                            @endif
+                        </td>
+                        <td>{{$file->mime_type}}</td>
                         @if ($file->mime_type === 'directory')
-                            <a href="{{route('projects.datasets.show.next', [$project, $dataset, $file])}}">
-                                <i class="fa-fw fas mr-2 fa-folder"></i> {{$file->name}}
-                            </a>
+                            <td>N/A</td>
                         @else
-                            <a href="{{route('projects.files.show', [$project, $file])}}">
-                                <i class="fa-fw fas mr-2 fa-file"></i>{{$file->name}}
-                            </a>
+                            <td>{{$file->toHumanBytes()}}</td>
                         @endif
-                    </td>
-                    <td>{{$file->mime_type}}</td>
-                    @if ($file->mime_type === 'directory')
-                        <td>N/A</td>
-                    @else
-                        <td>{{$file->toHumanBytes()}}</td>
-                    @endif
-                    <td>{{$file->size}}</td>
-                    <td>
-                        <div class="form-group form-check-inline">
-                            <input type="checkbox" class="form-check-input" id="{{$file->uuid}}"
-                                   {{$file->selected ? 'checked' : ''}} readonly onclick="return false;">
-                        </div>
-                    </td>
-                </tr>
+                        <td>{{$file->size}}</td>
+                        <td>
+                            <div class="form-group form-check-inline">
+                                <input type="checkbox" class="form-check-input" id="{{$file->uuid}}"
+                                       {{$file->selected ? 'checked' : ''}} readonly onclick="return false;">
+                            </div>
+                        </td>
+                    </tr>
+                @endif
             @endforeach
             </tbody>
         </table>
