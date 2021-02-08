2df4f57c9c19ae69ee793aef1a9f1de255f6ed38
gtarcea <glenn.tarcea@gmail.com>
2021-02-08 14:56:03 -0500

    Remove reference to mc-globus commands

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,11 +31,6 @@ class Kernel extends ConsoleKernel
                      ->everyMinute()
                      ->runInBackground()
                      ->withoutOverlapping();
-
-            $schedule->command("mc-globus:process-finished-globus-requests --background")
-                     ->everyMinute()
-                     ->runInBackground()
-                     ->withoutOverlapping();
         }
 
         if (config('backup.backup.run_backups') != 0) {
