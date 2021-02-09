6d828a4079cd86c26f88582f09474b39d9cdd535
gtarcea <glenn.tarcea@gmail.com>
2021-02-09 12:26:18 -0500

    Add index on directory_id/name

---
+++ database/migrations/2021_02_09_150556_add_directory_id_name_index_to_files_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddDirectoryIdNameIndexToFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('files', function (Blueprint $table) {
+            $table->index(['directory_id', 'name']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('files', function (Blueprint $table) {
+            //
+        });
+    }
+}
