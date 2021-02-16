41dc6743e1cab8fd9109d91d4d3d92cfccc3c6e3
gtarcea <glenn.tarcea@gmail.com>
2021-02-16 15:47:38 -0500

    Should drop the index when migrate down

--- database/migrations/2021_02_09_150556_add_directory_id_name_index_to_files_table.php
+++ database/migrations/2021_02_09_150556_add_directory_id_name_index_to_files_table.php
@@ -26,7 +26,7 @@ class AddDirectoryIdNameIndexToFilesTable extends Migration
     public function down()
     {
         Schema::table('files', function (Blueprint $table) {
-            //
+//            $table->dropIndex()
         });
     }
 }
