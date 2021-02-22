02aab7647dee79b1a1436268ea3ab3f2707e898e
gtarcea <glenn.tarcea@gmail.com>
2021-02-22 16:29:15 -0500

    Fix selection of files to include in a dataset

--- app/Actions/Datasets/UpdateDatasetFileSelectionAction.php
+++ app/Actions/Datasets/UpdateDatasetFileSelectionAction.php
@@ -9,7 +9,6 @@ class UpdateDatasetFileSelectionAction
 {
     public function __invoke($selection, $dataset)
     {
-        ray("__invoke with selection", $selection);
         DB::transaction(function () use ($selection, $dataset) {
             $fileSelection = collect($dataset->file_selection);
             foreach ($selection as $selectionKey => $path) {
@@ -18,33 +17,28 @@ class UpdateDatasetFileSelectionAction
                         $fileSelection = $this->addIncludeFile($fileSelection, $path);
                         break;
                     case "remove_include_file":
-                        $fileSelection = $this->removeIncludeFile($fileSelection, $path, $dataset);
+                        $fileSelection = $this->removeIncludeFile($fileSelection, $path);
                         break;
                     case "include_dir":
-                        $fileSelection = $this->addIncludeDir($fileSelection, $path, $dataset);
+                        $fileSelection = $this->addIncludeDir($fileSelection, $path);
                         break;
                     case "remove_include_dir":
-                        $fileSelection = $this->removeIncludeDir($fileSelection, $path, $dataset);
+                        $fileSelection = $this->removeIncludeDir($fileSelection, $path);
                         break;
                     case "exclude_file":
-                        $fileSelection = $this->addExcludeFile($fileSelection, $path, $dataset);
+                        $fileSelection = $this->addExcludeFile($fileSelection, $path);
                         break;
                     case "remove_exclude_file":
-                        $fileSelection = $this->removeExcludeFile($fileSelection, $path, $dataset);
+                        $fileSelection = $this->removeExcludeFile($fileSelection, $path);
                         break;
                     case "exclude_dir":
-                        $fileSelection = $this->addExcludeDir($fileSelection, $path, $dataset);
+                        $fileSelection = $this->addExcludeDir($fileSelection, $path);
                         break;
                     case "remove_exclude_dir":
-                        $fileSelection = $this->removeExcludeDir($fileSelection, $path, $dataset);
+                        $fileSelection = $this->removeExcludeDir($fileSelection, $path);
                         break;
                 }
             }
-
-//            $fileSelection = $this->addRemoveIfExists($fileSelection, 'include_files', $selection, 'include_file');
-//            $fileSelection = $this->addRemoveIfExists($fileSelection, 'exclude_files', $selection, 'exclude_file');
-//            $fileSelection = $this->addRemoveIfExists($fileSelection, 'include_dirs', $selection, 'include_dir');
-//            $fileSelection = $this->addRemoveIfExists($fileSelection, 'exclude_dirs', $selection, 'exclude_dir');
 
             $dataset->update(['file_selection' => $fileSelection]);
         });
@@ -52,87 +46,83 @@ class UpdateDatasetFileSelectionAction
         return $dataset->fresh();
     }
 
-    private function addRemoveIfExists($fileSelection, $fileSelectionKey, $selection, $selectionKey)
-    {
-        if (array_key_exists($selectionKey, $selection)) {
-            $fileSelection = $this->mergeSelectionEntry($fileSelection, $fileSelectionKey, [$selection[$selectionKey]]);
-        }
-
-        if (array_key_exists("remove_{$selectionKey}", $selection)) {
-            ray("remove entry");
-            $fileSelection = $this->removeSelectionEntry($fileSelection, $fileSelectionKey,
-                $selection["remove_{$selectionKey}"]);
-        }
-
-        return $fileSelection;
-    }
-
-    private function mergeSelectionEntry($fileSelection, $fileSelectionKey, $selection)
-    {
-        $selectionEntry = collect($fileSelection->get($fileSelectionKey));
-        $selectionEntry = $selectionEntry->merge($selection)->unique();
-        $fileSelection->put($fileSelectionKey, $selectionEntry->toArray());
-        return $fileSelection;
-    }
-
-    private function removeSelectionEntry($fileSelection, $fileSelectionKey, $entryToRemove)
-    {
-        $files = collect($fileSelection->get($fileSelectionKey))->reject(function ($value) use ($entryToRemove) {
-            return $value == $entryToRemove;
-        });
-        $fileSelection->put($fileSelectionKey, $files);
-        return $fileSelection;
-    }
-
     private function addIncludeFile(Collection $fileSelection, $path): Collection
     {
         // add include file
-        $includeFiles = collect($fileSelection->get('include_files'));
-        $fileSelection->put('include_files', $includeFiles->merge($path)
-                                                          ->unique()
-                                                          ->toArray());
+        $fileSelection = $this->addItemToSelectionKey($fileSelection, 'include_files', $path);
 
         // remove from exclude files if in there
-        if ($fileSelection->has('exclude_files')) {
-            $excludeFiles = collect($fileSelection->get('exclude_files'));
-            $fileSelection->put('exclude_files', $excludeFiles->reject(fn($pathEntry) => $pathEntry == $path)
-                                                              ->toArray());
-        }
+        $fileSelection = $this->removeItemFromSelectionKey($fileSelection, 'exclude_files', $path);
+
         return $fileSelection;
     }
 
-    private function removeIncludeFile(Collection $fileSelection, $path, $dataset): Collection
+    private function removeIncludeFile(Collection $fileSelection, $path): Collection
     {
+        // remove from include_files
+        $fileSelection = $this->removeItemFromSelectionKey($fileSelection, 'include_files', $path);
+
+        // Add to exclude files
+        $fileSelection = $this->addItemToSelectionKey($fileSelection, 'exclude_files', $path);
+
         return $fileSelection;
     }
 
-    private function addIncludeDir(Collection $fileSelection, $path, $dataset): Collection
+    private function addIncludeDir(Collection $fileSelection, $path): Collection
     {
+        $fileSelection = $this->addItemToSelectionKey($fileSelection, 'include_dirs', $path);
+
+        // Remove from exclude list
+        $fileSelection = $this->removeItemFromSelectionKey($fileSelection, 'exclude_dirs', $path);
         return $fileSelection;
     }
 
-    private function removeIncludeDir(Collection $fileSelection, $path, $dataset): Collection
+    private function removeIncludeDir(Collection $fileSelection, $path): Collection
     {
+        $fileSelection = $this->removeItemFromSelectionKey($fileSelection, 'include_dirs', $path);
+        $fileSelection = $this->addItemToSelectionKey($fileSelection, 'exclude_dirs', $path);
         return $fileSelection;
     }
 
-    private function addExcludeFile(Collection $fileSelection, $path, $dataset): Collection
+    private function addExcludeFile(Collection $fileSelection, $path): Collection
     {
+        $fileSelection = $this->addItemToSelectionKey($fileSelection, 'exclude_files', $path);
+        $fileSelection = $this->removeItemFromSelectionKey($fileSelection, 'include_files', $path);
         return $fileSelection;
     }
 
-    private function removeExcludeFile(Collection $fileSelection, $path, $dataset): Collection
+    private function removeExcludeFile(Collection $fileSelection, $path): Collection
     {
+        $fileSelection = $this->removeItemFromSelectionKey($fileSelection, 'exclude_files', $path);
         return $fileSelection;
     }
 
-    private function addExcludeDir(Collection $fileSelection, $path, $dataset): Collection
+    private function addExcludeDir(Collection $fileSelection, $path): Collection
     {
+        // Not needed at the moment for the api
         return $fileSelection;
     }
 
-    private function removeExcludeDir(Collection $fileSelection, $path, $dataset): Collection
+    private function removeExcludeDir(Collection $fileSelection, $path): Collection
     {
+        // Not needed at the moment for the api
+        return $fileSelection;
+    }
+
+    private function addItemToSelectionKey(Collection $fileSelection, $fileSelectionKey, $path): Collection
+    {
+        $fileSelectionItem = collect($fileSelection->get($fileSelectionKey));
+        $fileSelection->put($fileSelectionKey, $fileSelectionItem->merge($path)
+                                                                 ->unique()
+                                                                 ->toArray());
+        return $fileSelection;
+    }
+
+    private function removeItemFromSelectionKey(Collection $fileSelection, $fileSelectionKey, $path): Collection
+    {
+        $fileSelectionItem = collect($fileSelection->get($fileSelectionKey));
+        $fileSelection->put($fileSelectionKey, $fileSelectionItem->reject(fn($pathEntry) => $pathEntry == $path)
+                                                                 ->toArray());
         return $fileSelection;
     }
 }
