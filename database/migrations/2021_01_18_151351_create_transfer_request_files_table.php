a08fcd64caf151c079e64a51aee12e857ff20619
gtarcea <glenn.tarcea@gmail.com>
2021-02-26 14:45:00 -0500

    Fix migration class name to match migration file

--- database/migrations/2021_01_18_151351_create_transfer_request_files_table.php
+++ database/migrations/2021_01_18_151351_create_transfer_request_files_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFileTransferFilesTable extends Migration
+class CreateTransferRequestFilesTable extends Migration
 {
     /**
      * Run the migrations.
