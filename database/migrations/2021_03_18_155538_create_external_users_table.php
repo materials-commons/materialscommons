838fe9951860fb6bb68b22b2d6ea4363b6d31283
gtarcea <glenn.tarcea@gmail.com>
2021-03-18 13:58:00 -0400

    Add back migrations needed for author ordering

---
+++ database/migrations/2021_03_18_155538_create_external_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExternalUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('external_users', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')->unique();
+            $table->string('name');
+            $table->string('email')->nullable();
+            $table->text('affiliations');
+
+            $table->unsignedBigInteger('mc_user')->nullable();
+            $table->foreign('mc_user')
+                  ->references('id')
+                  ->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('external_users');
+    }
+}
