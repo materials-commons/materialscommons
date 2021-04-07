bdc95afa0c62c55e5f5ac8e8bf7f790cf7519a52
gtarcea <glenn.tarcea@gmail.com>
2021-04-07 12:59:08 -0400

    Keep limited set of users for globus

--- app/View/Components/Projects/ShowGlobusSideNav.php
+++ app/View/Components/Projects/ShowGlobusSideNav.php
@@ -28,11 +28,11 @@ class ShowGlobusSideNav extends Component
     private function userInBeta(): bool
     {
         switch (auth()->id()) {
-            case 343: /* Reza */
-            case 316: /* Tracy Berman */
-            case 173: /* John Allison */
+            // case 343: /* Reza */
+            // case 316: /* Tracy Berman */
+            // case 173: /* John Allison */
+            // case 101: /* David Montiel */
             case 130: /* Glenn Tarcea */
-            case 101: /* David Montiel */
             case 65:  /* Brian Puchala */
                 return true;
             default:
