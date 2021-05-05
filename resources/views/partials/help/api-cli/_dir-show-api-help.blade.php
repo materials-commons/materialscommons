170fe9c135eb3ccdb2d209bfc868f25947ad8683
gtarcea <glenn.tarcea@gmail.com>
2021-05-05 10:09:37 -0400

    Fix formatting

--- resources/views/partials/help/api-cli/_dir-show-api-help.blade.php
+++ resources/views/partials/help/api-cli/_dir-show-api-help.blade.php
@@ -6,13 +6,13 @@ dir_listing = c.list_directory({{$project->id}}, {{$directory->id}})
 dir_list = c.list_directory({{$project->id}}, {{$directory->path}})
 
 @if ($directory->path != "/")
-        # Create a new directory
-        new_dir = c.create_directory({{$project->id}}, {{$directory->id}}, {{$directory->directory_id}})
+# Create a new directory
+new_dir = c.create_directory({{$project->id}}, {{$directory->id}}, {{$directory->directory_id}})
 
-        # Rename directory
-        updated_dir = c.rename_directory({{$project->id}}, {{$directory->id}}, "new-dir-name")
+# Rename directory
+updated_dir = c.rename_directory({{$project->id}}, {{$directory->id}}, "new-dir-name")
 
-        # Delete directory
-        c.delete_directory({{$project->id}}, {{$directory->id}})
-    @endif
+# Delete directory
+c.delete_directory({{$project->id}}, {{$directory->id}})
+@endif
 </pre>
