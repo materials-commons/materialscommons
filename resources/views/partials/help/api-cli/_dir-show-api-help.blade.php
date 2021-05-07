026360c2fb2c06eced042d576c6fa3465d0e5415
gtarcea <glenn.tarcea@gmail.com>
2021-05-07 13:29:08 -0400

    Fix api call to create a directory

--- resources/views/partials/help/api-cli/_dir-show-api-help.blade.php
+++ resources/views/partials/help/api-cli/_dir-show-api-help.blade.php
@@ -6,13 +6,13 @@ dir_listing = c.list_directory({{$project->id}}, {{$directory->id}})
 dir_list = c.list_directory({{$project->id}}, "{{$directory->path}}")
 
 @if ($directory->path != "/")
-# Create a new directory
-new_dir = c.create_directory({{$project->id}}, {{$directory->id}}, {{$directory->directory_id}})
+        # Create a new directory named dir1 that is a child of the current directory
+        new_dir = c.create_directory({{$project->id}}, "dir1", {{$directory->directory_id}})
 
-# Rename directory
-updated_dir = c.rename_directory({{$project->id}}, {{$directory->id}}, "new-dir-name")
+        # Rename directory
+        updated_dir = c.rename_directory({{$project->id}}, {{$directory->id}}, "new-dir-name")
 
-# Delete directory
-c.delete_directory({{$project->id}}, {{$directory->id}})
-@endif
+        # Delete directory
+        c.delete_directory({{$project->id}}, {{$directory->id}})
+    @endif
 </pre>
