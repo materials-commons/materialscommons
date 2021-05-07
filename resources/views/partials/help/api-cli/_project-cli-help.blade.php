eda1ead842331e88ca5fc3c3d6d568e58b489e46
gtarcea <glenn.tarcea@gmail.com>
2021-05-07 10:46:57 -0400

    Fix docs for listing projects in cli

--- resources/views/partials/help/api-cli/_project-cli-help.blade.php
+++ resources/views/partials/help/api-cli/_project-cli-help.blade.php
@@ -1,6 +1,13 @@
+@if (isset($project))
 <pre>
 # You can optionally clone the project locally
 mc clone {{$project->id}}
 
 mc proj --id {{$project->id}}
 </pre>
+@else
+<pre>
+# List projects
+mc proj
+</pre>
+@endif
