2be24e79b4a13ab0a5b2d8a7073f7c6c4bb7a25b
gtarcea <glenn.tarcea@gmail.com>
2021-06-22 15:15:40 -0400

    WIP - Connect WebUI to the query engine    * Transform UI selection into the AST needed to run the query    * Filter out the sample matches from the query against all the potential samples in the project

--- app/Http/Controllers/Web/Entities/Mql/RunMqlQueryWebController.php
+++ app/Http/Controllers/Web/Entities/Mql/RunMqlQueryWebController.php
@@ -13,8 +13,12 @@ use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 
+
 class RunMqlQueryWebController extends Controller
 {
+    private const PROCESS_ATTR_FIELD = 3;
+    private const SAMPLE_ATTR_FIELD = 4;
+
     public function __invoke(MqlSelectionRequest $request, CreateUsedActivitiesForEntitiesAction $createUsedActivities,
         Project $project)
     {
@@ -31,7 +35,6 @@ class RunMqlQueryWebController extends Controller
 
         $entities = Entity::with(['activities', 'experiments'])
                           ->where('project_id', $project->id)
-                          ->limit(2)
                           ->get();
 
         $processAttributes = DB::table('attributes')
@@ -95,40 +98,107 @@ class RunMqlQueryWebController extends Controller
             $processTypes = $data['activities'];
         }
 
-        $processTypesQuery = $this->buildProcessTypesQuery($processTypes);
-        $processAttrs = [];
-        $sampleAttrs = [];
-    }
-
-    private function buildProcessTypesQuery($processTypes)
-    {
-        if (sizeof($processTypes) == 0) {
-            return null;
-        }
-
-        if (sizeof($processTypes) == 1) {
+        $processTypesQuery = $this->buildQuery($processTypes, function ($item) {
             return [
                 "field_name" => '',
                 "field_type" => 'sample-function',
-                "value"      => $processTypes[0],
+                "value"      => $item,
                 "operation"  => 'has-process',
             ];
+        }, function ($item) {
+            return true;
+        });
+
+        $processAttrs = $this->buildQuery($data['process_attrs'], function ($item) {
+            return [
+                "field_name" => '',
+                "field_type" => self::PROCESS_ATTR_FIELD,
+                "value"      => $item,
+                "operation"  => 'has-process',
+            ];
+        }, function ($item) {
+            return isset($item["name"]);
+        });
+
+        $sampleAttrs = $this->buildQuery($data['sample_attrs'], function ($item) {
+            return [
+                "field_name" => $item["name"],
+                "field_type" => self::SAMPLE_ATTR_FIELD,
+                "value"      => $item["value"],
+                "operation"  => $item["operator"],
+            ];
+        }, function ($item) {
+            return isset($item["name"]);
+        });
+
+        $numberOfQueries = 0;
+        if (!is_null($processTypesQuery)) {
+            $numberOfQueries++;
         }
 
-        $processTypesQuery = [
+        if (!is_null($processAttrs)) {
+            $numberOfQueries++;
+        }
+
+        if (!is_null($sampleAttrs)) {
+            $numberOfQueries++;
+        }
+
+        if ($numberOfQueries == 0) {
+            // no queries
+            return null;
+        }
+
+        if ($numberOfQueries == 1) {
+            return $this->firstNotNull($processTypesQuery, $processAttrs, $sampleAttrs);
+        }
+
+        // Two or more not null
+        $items = [$processTypesQuery, $processAttrs, $sampleAttrs];
+        $toplevelQuery = [
+            'left'  => null,
+            'right' => null,
+            'and'   => true,
+        ];
+
+        if ($numberOfQueries == 2) {
+            $toplevelQuery['left'] = $this->firstNotNull($processTypesQuery, $processAttrs, $sampleAttrs);
+            $toplevelQuery['right'] = $this->secondNotNull($processTypesQuery, $processAttrs, $sampleAttrs);
+            return $toplevelQuery;
+        }
+
+        // There are 3 parts to the query
+        $toplevelQuery['left'] = $processTypesQuery;
+        $toplevelQuery['right'] = [
+            'left'  => $processAttrs,
+            'right' => $sampleAttrs,
+            'and'   => true,
+        ];
+    }
+
+    private function buildQuery($items, $buildMatchFn, $isUsableFn)
+    {
+        if (sizeof($items) == 0) {
+            return null;
+        }
+
+        if (sizeof($items) == 1) {
+            return $buildMatchFn($items[0]);
+        }
+
+        $itemsQuery = [
             'left'  => [],
             'right' => [],
             'and'   => true,
         ];
 
-        $current = $processTypesQuery;
-        for ($i = 0; $i < sizeof($processTypes); $i++) {
-            $current['left'] = [
-                "field_name" => '',
-                "field_type" => 'sample-function',
-                "value"      => $processTypes[$i],
-                "operation"  => 'has-process',
-            ];
+        $current = $itemsQuery;
+        for ($i = 0; $i < sizeof($items); $i++) {
+            if (!$isUsableFn($items[$i])) {
+                continue;
+            }
+
+            $current['left'] = $buildMatchFn($items[$i]);
             // if we are on the second to last entry then right will also be a match statement.
             // To simplify tracking this we just set up the right side here and then exit the
             // loop.
@@ -140,13 +210,8 @@ class RunMqlQueryWebController extends Controller
             // sizeof($processTypes) - 2 == $i, then $i = 1, and $i+1 = 2, which is the third
             // entry in $processTypes and we can set this up as match statement for the right
             // side of the our AND statement.
-            if (sizeof($processTypes) - 2 == $i) {
-                $current['right'] = [
-                    "field_name" => '',
-                    "field_type" => 'sample-function',
-                    "value"      => $processTypes[$i + 1],
-                    "operation"  => 'has-process',
-                ];
+            if (sizeof($items) - 2 == $i) {
+                $current['right'] = $buildMatchFn($items[$i + 1]);
                 break;
             }
 
@@ -176,10 +241,42 @@ class RunMqlQueryWebController extends Controller
             // Point $current at the newly created AND statement
             $current = $current['right'];
         }
-        return $processTypesQuery;
+
+        return $itemsQuery;
     }
 
-    private function filterEntitiesUsingQueryResults(Collection $entities, $queryResults)
+    private function firstNotNull($first, $second, $third)
     {
+        if (!is_null($first)) {
+            return $first;
+        }
+
+        if (!is_null($second)) {
+            return $second;
+        }
+
+        return $third;
+    }
+
+    private function secondNotNull($first, $second, $third)
+    {
+        // if $first is not null then the second not null is either $second or $third
+        if (!is_null($first)) {
+            if (!is_null($second)) {
+                return $second;
+            }
+            return $third;
+        }
+
+        // if $first is null, then by definition the second not null is $third
+        return $third;
+    }
+
+    private function filterEntitiesUsingQueryResults(Collection $entities, $queryResults): Collection
+    {
+        $samples = collect($queryResults['samples']);
+        return $entities->filter(function (Entity $entity) use ($samples) {
+            return $samples->where('id', $entity->id)->count() != 0;
+        });
     }
 }
