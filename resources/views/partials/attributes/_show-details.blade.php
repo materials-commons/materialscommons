386f0cb47a2f1c9c63211d4e303d710e3164a1cf
gtarcea <glenn.tarcea@gmail.com>
2021-07-08 15:25:13 -0400

    Changes to formatting of attribute details

--- resources/views/partials/attributes/_show-details.blade.php
+++ resources/views/partials/attributes/_show-details.blade.php
@@ -1,17 +1,31 @@
 <div id="{{slugify($name)}}" class="container border rounded" hx-target="this" hx-swap="outerHTML">
-    <a href="#" hx-get="{{route('projects.attributes.close-details-by-name', [$project, $name])}}"
-       class="row mb-2 float-right mr-2">Close</a>
     <div class="row">
+        <div class="col-12">
+            <a href="#" hx-get="{{route('projects.attributes.close-details-by-name', [$project, $name])}}"
+               class="mb-2 float-right mr-2">Close</a>
+        </div>
+    </div>
+    <div class="row ml-1">
         @if($details->isNumeric)
-            Numeric Attribute - Min: {{$details->min}}, Max: {{$details->max}}, # Unique
-            Values: {{$details->uniqueCount}}
+            Numeric Attribute -
         @else
             String Attribute - # Unique Values: {{$details->uniqueCount}}
         @endif
-
     </div>
-    <div class="row">
-        Showing first {{$details->values->count()}} unique values out of {{$details->uniqueCount}}.
+    @if($details->isNumeric)
+        <div class="row">
+            <ul class="ml-4">
+                <li>
+                    Min: {{$details->min}}
+                </li>
+                <li>
+                    Max: {{$details->max}}
+                </li>
+            </ul>
+        </div>
+    @endif
+    <div class="row ml-1">
+        Showing {{$details->values->count()}} of {{$details->uniqueCount}} unique values -
     </div>
     <div class="row">
         <ul class="ml-4">
