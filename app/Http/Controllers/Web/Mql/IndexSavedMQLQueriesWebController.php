63a6b86959888c3dbf72205ceca40dee790c6dbd
gtarcea <glenn.tarcea@gmail.com>
2021-07-12 15:26:01 -0400

    Placeholder controllers for the saved queries interface

---
+++ app/Http/Controllers/Web/Mql/IndexSavedMQLQueriesWebController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Web\Mql;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IndexSavedMQLQueriesWebController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
