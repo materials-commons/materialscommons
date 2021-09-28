466730f97f3947ef3807d06071a9b2cbb8c5cf50
gtarcea <glenn.tarcea@gmail.com>
2021-09-28 11:23:33 -0400

    Don't allow datasets that are still being unpublished to be be published. Add warning explaining to the user what is happening.

--- resources/views/app/projects/datasets/show.blade.php
+++ resources/views/app/projects/datasets/show.blade.php
@@ -35,10 +35,12 @@
                     <i class="fas fa-minus-circle mr-2"></i>Unpublish
                 </a>
             @elseif($dataset->hasSelectedFiles())
-                <a class="float-right action-link mr-4"
-                   href="{{route('projects.datasets.publish', [$project, $dataset])}}">
-                    <i class="fas fa-file-export mr-2"></i>Publish
-                </a>
+                @if(!isset($dataset->cleanup_started_at))
+                    <a class="float-right action-link mr-4"
+                       href="{{route('projects.datasets.publish', [$project, $dataset])}}" disabled=true>
+                        <i class="fas fa-file-export mr-2"></i>Publish
+                    </a>
+                @endif
             @endif
 
             <div class="dropdown float-right mr-4">
@@ -60,6 +62,12 @@
         @endslot
 
         @slot('body')
+            @if(isset($dataset->cleanup_started_at))
+                <div class="msg msg-danger">
+                    Dataset cannot be published - It is still in the process of being unpublished
+                    (Unpublish started {{$dataset->cleanup_started_at->diffForHumans()}}).
+                </div>
+            @endif
             @include('app.projects.datasets._dataset-status', [
                 'defaultRoute' => route('projects.datasets.show.overview', [$project, $dataset]),
                 'filesRoute' => route('projects.datasets.show.files', [$project, $dataset]),
