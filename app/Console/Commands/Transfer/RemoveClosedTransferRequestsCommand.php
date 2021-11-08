dd5ab3d7e10433695b0f69257e557c14ed2cb074
gtarcea <glenn.tarcea@gmail.com>
2021-11-08 10:24:27 -0500

    Update transfer request process to handle conversion and removing files

--- app/Console/Commands/Transfer/RemoveClosedTransferRequestsCommand.php
+++ app/Console/Commands/Transfer/RemoveClosedTransferRequestsCommand.php
@@ -3,9 +3,13 @@
 namespace App\Console\Commands\Transfer;
 
 use App\Actions\Globus\GlobusApi;
+use App\Models\Conversion;
+use App\Models\File;
 use App\Models\TransferRequest;
+use App\Models\TransferRequestFile;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 
 class RemoveClosedTransferRequestsCommand extends Command
 {
@@ -41,7 +45,7 @@ class RemoveClosedTransferRequestsCommand extends Command
     public function handle(): int
     {
         $globusApi = GlobusApi::createGlobusApi();
-        TransferRequest::with(['globusTransfer'])->where('state', 'closed')
+        TransferRequest::with(['globusTransfer', 'transferRequestFiles.file'])->where('state', 'closed')
                        ->get()
                        ->each(function (TransferRequest $transferRequest) use ($globusApi) {
                            if (!isset($transferRequest->globusTransfer)) {
@@ -52,6 +56,32 @@ class RemoveClosedTransferRequestsCommand extends Command
                                    Log::error("Unable to delete acl");
                                }
                            }
+                           $transferRequest->transferRequestFiles->each(function (TransferRequestFile $trFile) {
+                               $existing = File::where('checksum', $trFile->file->checksum)
+                                               ->where('id', '<>', $trFile->file->id)
+                                               ->first();
+                               if (!is_null($existing)) {
+                                   $usesUuid = $existing->uuid;
+                                   $usesId = $existing->id;
+                                   if (!is_null($existing->uses_uuid)) {
+                                       $usesUuid = $existing->uses_uuid;
+                                       $usesId = $existing->uses_id;
+                                   }
+
+                                   $trFile->file->update([
+                                       'uses_uuid' => $usesUuid,
+                                       'uses_id'   => $usesId,
+                                   ]);
+                                   Storage::disk('mcfs')->delete($this->getFilePathPartialFromUid($trFile->file->uuid));
+                               }
+                               if ($trFile->file->shouldBeConverted()) {
+                                   Conversion::create([
+                                       'file_id'    => $trFile->file->id,
+                                       'project_id' => $trFile->file->project_id,
+                                       'owner_id'   => $trFile->file->owner_id,
+                                   ]);
+                               }
+                           });
                            $transferRequest->delete();
                        });
         return 0;
