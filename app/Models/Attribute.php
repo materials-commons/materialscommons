a8d9fb6dd7727a14c751cc4438728b412ebf77f6
gtarcea <gtarcea@umich.edu>
2022-02-07 10:12:40 -0500

    Add @property marked_important_at

--- app/Models/Attribute.php
+++ app/Models/Attribute.php
@@ -12,6 +12,7 @@ use Illuminate\Database\Eloquent\Model;
  * @property string $uuid
  * @property string name
  * @property string description
+ * @property mixed marked_important_at
  *
  * @mixin Builder
  */
