42d5b4897688761cdc4d1a159b9826874b381d18
gtarcea <gtarcea@umich.edu>
2022-02-14 16:42:24 -0500

    Create command to purge old log files

--- app/Console/Commands/Logs/PurgeOldLogFilesCommand.php
+++ app/Console/Commands/Logs/PurgeOldLogFilesCommand.php
@@ -2,7 +2,10 @@
 
 namespace App\Console\Commands\Logs;
 
+use App\Models\EtlRun;
+use App\Models\TransferRequest;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
 
 class PurgeOldLogFilesCommand extends Command
 {
@@ -37,6 +40,33 @@ class PurgeOldLogFilesCommand extends Command
      */
     public function handle()
     {
+        $this->purgeEtlLogs();
+        $this->purgeMCBridgeLogs();
         return 0;
+    }
+
+    private function purgeEtlLogs()
+    {
+        EtlRun::getOldEtlLogs()->each(function(EtlRun $etlRun) {
+            $etlRun->deleteLog();
+            $etlRun->delete();
+        });
+    }
+
+    private function purgeMCBridgeLogs()
+    {
+        Storage::disk('mcfs')->path("bridge_logs/");
+        collect(Storage::disk('mcfs')->allFiles('bridge_logs'))->each(function ($file) {
+            $info = pathinfo(Storage::disk('mcfs')->path($file));
+
+            // Log file is stored with the uuid as the filename (part without the extension).
+            $uuid = $info['filename'];
+            $transferRequest = TransferRequest::where('uuid', $uuid)->first();
+
+            // Only delete log file if there is not a transfer request associated with it.
+            if (is_null($transferRequest)) {
+                Storage::disk('mcfs')->delete($file);
+            }
+        });
     }
 }
