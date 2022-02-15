7b75829834fe143c060e328f2d7048044066abc6
gtarcea <gtarcea@umich.edu>
2022-02-15 12:31:10 -0500

    Remove beta site and remove references of what to run from the beta site

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,68 +26,61 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        if (config('app.beta') != 1) {
-            $schedule->command("mc:process-finished-globus-tasks --background")
-                     ->everyMinute()
-                     ->runInBackground()
-                     ->withoutOverlapping();
+        $schedule->command("mc:process-finished-globus-tasks --background")
+                 ->everyMinute()
+                 ->runInBackground()
+                 ->withoutOverlapping();
 
-            // This command runs periodically to clean up any globus requests that didn't properly close.
-            $schedule->command('mc-transfer:remove-closed-transfer-requests')
-                     ->everyFiveMinutes()
-                     ->runInBackground()
-                     ->withoutOverlapping();
+        // This command runs periodically to clean up any globus requests that didn't properly close.
+        $schedule->command('mc-transfer:remove-closed-transfer-requests')
+                 ->everyFiveMinutes()
+                 ->runInBackground()
+                 ->withoutOverlapping();
 
-            // Delete old unverified accounts.
-            $schedule->command('mc:delete-unverified-accounts')
-                     ->daily()
-                     ->at('02:00')
-                     ->runInBackground();
+        // Delete old unverified accounts.
+        $schedule->command('mc:delete-unverified-accounts')
+                 ->daily()
+                 ->at('02:00')
+                 ->runInBackground();
 
-            // Cleanup expired directories in the trashcan
-            $schedule->command("mc:delete-expired-trashcan-directories")
-                     ->everyFifteenMinutes()
-                     ->runInBackground()
-                     ->withoutOverlapping();
+        // Cleanup expired directories in the trashcan
+        $schedule->command("mc:delete-expired-trashcan-directories")
+                 ->everyFifteenMinutes()
+                 ->runInBackground()
+                 ->withoutOverlapping();
 
-            // Cleanup expired files in the trashcan
-            $schedule->command("mc:delete-expired-trashcan-files")
-                     ->everyFifteenMinutes()
-                     ->runInBackground()
-                     ->withoutOverlapping();
+        // Cleanup expired files in the trashcan
+        $schedule->command("mc:delete-expired-trashcan-files")
+                 ->everyFifteenMinutes()
+                 ->runInBackground()
+                 ->withoutOverlapping();
 
-            // Cleanup expired projects in the trashcan
-            $schedule->command("mc:delete-expired-trashcan-projects")
-                     ->everyFifteenMinutes()
-                     ->runInBackground()
-                     ->withoutOverlapping();
+        // Cleanup expired projects in the trashcan
+        $schedule->command("mc:delete-expired-trashcan-projects")
+                 ->everyFifteenMinutes()
+                 ->runInBackground()
+                 ->withoutOverlapping();
 
-            // Queue file conversions
-            $schedule->command("mc:run-conversion-on-files")
-                     ->everyFifteenMinutes()
-                     ->runInBackground()
-                     ->withoutOverlapping();
+        // Queue file conversions
+        $schedule->command("mc:run-conversion-on-files")
+                 ->everyFifteenMinutes()
+                 ->runInBackground()
+                 ->withoutOverlapping();
 
-            // Delete the actual files, where appropriate
-            $schedule->command("mc:delete-tbd-files")
-                     ->everyFiveMinutes()
-                     ->runInBackground()
-                     ->withoutOverlapping();
+        // Delete the actual files, where appropriate
+        $schedule->command("mc:delete-tbd-files")
+                 ->everyFiveMinutes()
+                 ->runInBackground()
+                 ->withoutOverlapping();
 
-            $schedule->command("mc-logs:purge-old-log-files")
-                     ->everyFifteenMinutes()
-                     ->runInBackground()
-                     ->withoutOverlapping();
-        }
-
-        if (config('backup.backup.run_backups') != 0) {
-            if (config('app.env') == 'production') {
-                $schedule->command('backup:clean')->daily()->at('01:00');
-                $schedule->command('backup:run')->daily()->at('01:30');
-            }
-        }
+        $schedule->command("mc-logs:purge-old-log-files")
+                 ->everyFifteenMinutes()
+                 ->runInBackground()
+                 ->withoutOverlapping();
 
         if (config('app.env') == 'production') {
+            $schedule->command('backup:clean')->daily()->at('01:00');
+            $schedule->command('backup:run')->daily()->at('01:30');
             $schedule->command('mc:generate-site-map')->daily()->at('3:00');
             $schedule->command('mc:generate-usage-statistics')->monthlyOn(1, '02:00');
         }
