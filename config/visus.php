e39ae9d13a3ca7bcd62a61e9c20640fd6f717bd9
gtarcea <gtarcea@umich.edu>
2022-03-02 10:38:23 -0500

    Fix URL for visus integration.

--- config/visus.php
+++ config/visus.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'url' => env('VISUS_URL', 'http://localhost:8080/viewer/viewer.html?server=http%3A%2F%2Flocalhost:8080%2Fmod_visus%3F&dataset=')
+    'url' => env('VISUS_URL', 'http://localhost:8080/viewer/viewer.html?server=http%3A%2F%2Flocalhost:8080%2Fmod_visus%3F&amp;dataset=')
 ];
