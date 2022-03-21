f5b247c51bbcd2fcf3cf18868820eaa93dc0e043
gtarcea <gtarcea@umich.edu>
2022-03-21 16:24:50 -0400

    Finish test and fix bug for replicating files in a dataset and correctly setting up the replicated directory structure.

--- tests/Feature/Actions/Datasets/CreateDatasetFilesTableActionTest.php
+++ tests/Feature/Actions/Datasets/CreateDatasetFilesTableActionTest.php
@@ -25,9 +25,13 @@ class CreateDatasetFilesTableActionTest extends TestCase
 
         $d1 = ProjectFactory::createDirectory($project, $project->rootDir, "d1");
         $f1 = ProjectFactory::createFakeFile($project, $d1, "f1.txt");
+        $d1d2 = ProjectFactory::createDirectory($project, $d1, "d2");
+        $d1d2d3 = ProjectFactory::createDirectory($project, $d1d2, "d3");
+        $f2 = ProjectFactory::createFakeFile($project, $d1d2d3, "f2.txt");
+
         $fileSelection = [
-            'include_files' => ['/d1/f1.txt'],
-            'include_dirs'  => [],
+            'include_files' => [],
+            'include_dirs'  => ['/'],
             'exclude_files' => [],
             'exclude_dirs'  => [],
         ];
@@ -35,10 +39,74 @@ class CreateDatasetFilesTableActionTest extends TestCase
 
         $createDatasetFilesTableAction = new CreateDatasetFilesTableAction();
         $createDatasetFilesTableAction->execute($dataset);
-        File::where('dataset_id', $dataset->id)->get()->each(function(File $file) {
-            echo "{$file->name}, {$file->path}, {$file->directory_id}, {$file->mime_type}\n";
-        });
 
-        $this->assertTrue(true);
+        //
+        // If debugging is needed then this line should be uncommented. It will print out the list of files and directories
+        // and allow you to see if everything is wired up correctly.
+        //
+        // File::where('dataset_id', $dataset->id)->get()->each(function(File $file) {
+        //     echo "name = {$file->name}, path = {$file->path}, id = {$file->id}, dir_id = {$file->directory_id}, mime_type = {$file->mime_type}\n";
+        // });
+        //
+
+        $replicatedRoot = $this->getDir('/', $project->id, $dataset->id);
+        $this->assertNotNull($replicatedRoot);
+
+        $replicatedD1 = $this->getDirWithParent('/d1', $project->id, $replicatedRoot->id, $dataset->id);
+        $this->assertNotNull($replicatedD1);
+
+        $replicatedD1D2 = $this->getDirWithParent('/d1/d2', $project->id, $replicatedD1->id, $dataset->id);
+        $this->assertNotNull($replicatedD1D2);
+
+        $replicatedD1D2D3 = $this->getDirWithParent('/d1/d2/d3', $project->id, $replicatedD1D2->id, $dataset->id);
+        $this->assertNotNull($replicatedD1D2D3);
+
+        $d3ReplicatedDir = File::where('dataset_id', $dataset->id)
+                               ->where('path', '/d1/d2/d3')
+                               ->where('mime_type', 'directory')
+                               ->whereNull('deleted_at')
+                               ->where('project_id', $project->id)
+                               ->where('current', true)
+                               ->first();
+        $this->assertNotNull($d3ReplicatedDir);
+
+        $f2Replicated = $this->getFile("f2.txt", $project->id, $d3ReplicatedDir->id, $dataset->id);
+        $this->assertNotNull($f2Replicated);
+
+        $f1Replicated = $this->getFile('f1', $project->id, $replicatedD1->id, $dataset->id);
+        $this->assertNotNull($f1Replicated);
+    }
+
+    private function getDir($path, $projectId, $datasetId)
+    {
+        return File::where('dataset_id', $datasetId)
+                   ->where('path', $path)
+                   ->where('mime_type', 'directory')
+                   ->whereNull('deleted_at')
+                   ->where('project_id', $projectId)
+                   ->where('current', true)
+                   ->first();
+    }
+
+    private function getDirWithParent($path, $projectId, $directoryId, $datasetId)
+    {
+        return File::where('dataset_id', $datasetId)
+                   ->where('path', $path)
+                   ->where('mime_type', 'directory')
+                   ->whereNull('deleted_at')
+                   ->where('project_id', $projectId)
+                   ->where('directory_id', $directoryId)
+                   ->where('current', true)
+                   ->first();
+    }
+
+    private function getFile($name, $projectId, $directoryId, $datasetId)
+    {
+        return File::where('dataset_id', $datasetId)
+                   ->where('directory_id', $directoryId)
+                   ->whereNull('deleted_at')
+                   ->where('project_id', $projectId)
+                   ->where('current', true)
+                   ->first();
     }
 }
