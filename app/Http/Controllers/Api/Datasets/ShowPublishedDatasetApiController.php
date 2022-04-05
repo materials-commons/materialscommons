7b70aa6bf18ba9d026e67ef096251854baaf6144
gtarcea <gtarcea@umich.edu>
2022-04-05 12:42:10 -0400

    Updates to return rootDir (some places also updated to return tags and owner)

--- app/Http/Controllers/Api/Datasets/ShowPublishedDatasetApiController.php
+++ app/Http/Controllers/Api/Datasets/ShowPublishedDatasetApiController.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers\Api\Datasets;
 
 use App\Http\Controllers\Controller;
-use App\Http\Queries\Datasets\SingleDatasetQuery;
 use App\Http\Resources\Datasets\DatasetResource;
+use App\Models\Dataset;
 
 class ShowPublishedDatasetApiController extends Controller
 {
-    public function __invoke(SingleDatasetQuery $query)
+    public function __invoke($datasetId)
     {
-        $dataset = $query->first();
+        $dataset = Dataset::with(['owner', 'tags', 'rootDir'])
+                          ->withCounts()
+                          ->where('id', $datasetId);
+        // if published_at is null then this dataset is not in a published state.
         abort_if(is_null($dataset->published_at), 404);
         return new DatasetResource($dataset);
     }
