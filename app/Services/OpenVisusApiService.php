d5829b272dca3cd046e7750fc8503b22cc175575
gtarcea <gtarcea@umich.edu>
2022-04-07 11:10:06 -0400

    Start scaffolding out the rest of the OpenVisus integration

--- app/Services/OpenVisusApiService.php
+++ app/Services/OpenVisusApiService.php
@@ -2,7 +2,11 @@
 
 namespace App\Services;
 
+use App\Helpers\PathHelpers;
+use App\Models\Dataset;
+use App\Models\Project;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 
 class OpenVisusApiService
 {
@@ -19,6 +23,47 @@ class OpenVisusApiService
 
     public static function visusDatasetUrl($visusDataset)
     {
-        return config('visus.url').$visusDataset;
+        $url = config('visus.url');
+        if (Str::endsWith($url, "/")) {
+            return $url.$visusDataset;
+        }
+
+        return $url."/{$visusDataset}";
+    }
+
+    public static function addDatasetToOpenVisus(Dataset $dataset)
+    {
+        // $doc = simplexml_load_file('/home/gtarcea/visus/datasets/datasets.config');
+        // $datasets = $doc->datasets;
+        // $newData = $datasets->addChild('dataset');
+        // $newData->addAttribute('name', 'test');
+        // $newData->addAttribute('url', '/datasets/test/visus.idx');
+
+        // $newData2 = $datasets->addChild('dataset');
+        // $newData2->addAttribute('name', 'test2');
+        // $newData2->addAttribute('url', '/datasets/test2/visus.idx');
+
+        // $dom = new DOMDocument;
+        // $dom->preserveWhiteSpace = FALSE;
+        // $dom->loadXML($doc->saveXML());
+        // $dom->formatOutput = true;
+        // // $doc->preserveWhiteSpace = false;
+        // // $doc->formatOutput = true;
+        // $dom->save('/home/gtarcea/datasets-new.config');
+    }
+
+    private static function idxPathForDataset(Dataset $dataset): string
+    {
+        return PathHelpers::joinPaths(config('visus.idx_path'), "/datasets/{$dataset->uuid}");
+    }
+
+    public static function addProjectToOpenVisus(Project $project)
+    {
+
+    }
+
+    private static function idxPathForProject(Project $project): string
+    {
+        return PathHelpers::joinPaths(config('visus.idx_path'), "/projects/{$project->uuid}");
     }
 }
--- config/visus.php
+++ config/visus.php
@@ -1,5 +1,7 @@
 <?php
 
 return [
-    'url' => env('VISUS_URL', 'http://localhost:8080/viewer/viewer.html?server=http%3A%2F%2Flocalhost:8080%2Fmod_visus%3F&amp;dataset=')
+    'url'      => env('VISUS_URL',
+        'http://localhost:8080/viewer/viewer.html?server=http%3A%2F%2Flocalhost:8080%2Fmod_visus%3F&amp;dataset='),
+    'idx_path' => env('VISUS_IDX_PATH', env('MCFS_DIR')."/open_visus"),
 ];
