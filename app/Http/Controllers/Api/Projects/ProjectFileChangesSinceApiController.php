690103023e201c4e6c6e74126e13ab73278dc194
gtarcea <gtarcea@umich.edu>
2022-06-21 09:51:34 -0400

    First version of project file changes

--- app/Http/Controllers/Api/Projects/ProjectFileChangesSinceApiController.php
+++ app/Http/Controllers/Api/Projects/ProjectFileChangesSinceApiController.php
@@ -15,15 +15,27 @@ class ProjectFileChangesSinceApiController extends Controller
         $validatedData = $request->validate([
             'since' => 'date_format:Y-m-d H:i:s'
         ]);
-        return FileResource::collection(File::query()
-                                            ->with(['owner', 'directory'])
-                                            ->where('created_at', '>', $validatedData['since'])
-                                            ->where('project_id', $project->id)
-                                            ->whereNull('deleted_at')
-                                            ->where('current', true)
-                                            ->whereNull('dataset_id')
-                                            ->where('mime_type', '<>', 'directory')
-                                            ->orderBy('created_at')
-                                            ->get());
+//        return FileResource::collection(File::query()
+//                                            ->with(['owner', 'directory'])
+//                                            ->where('created_at', '>', $validatedData['since'])
+//                                            ->where('project_id', $project->id)
+//                                            ->whereNull('deleted_at')
+//                                            ->where('current', true)
+//                                            ->whereNull('dataset_id')
+//                                            ->where('mime_type', '<>', 'directory')
+//                                            ->limit(20)
+//                                            ->orderBy('created_at')
+//                                            ->get());
+
+        return File::query()
+                   ->with(['owner', 'directory'])
+                   ->where('created_at', '>', $validatedData['since'])
+                   ->where('project_id', $project->id)
+                   ->whereNull('deleted_at')
+                   ->where('current', true)
+                   ->whereNull('dataset_id')
+                   ->where('mime_type', '<>', 'directory')
+                   ->orderBy('created_at')
+                   ->jsonPaginate();
     }
 }
