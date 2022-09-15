43e9f5df420d7d5a38afc976a67f6d54bf38421a
gtarcea <glenn.tarcea@gmail.com>
2022-09-15 15:21:18 -0400

    Small change so placeholder test will just pass until tests are filled out

--- tests/Feature/Actions/Files/CopyFileActionTest.php
+++ tests/Feature/Actions/Files/CopyFileActionTest.php
@@ -2,8 +2,6 @@
 
 namespace Tests\Feature\Actions\Files;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class CopyFileActionTest extends TestCase
@@ -15,8 +13,6 @@ class CopyFileActionTest extends TestCase
      */
     public function test_example()
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $this->assertTrue(true);
     }
 }
