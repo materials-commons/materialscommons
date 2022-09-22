b2822df628f67505bb6ef704627a1e2e71b09123
gtarcea <gtarcea@umich.edu>
2022-09-22 15:38:34 -0400

    First implementation and test

--- app/Actions/Directories/CopyDirectoryAction.php
+++ app/Actions/Directories/CopyDirectoryAction.php
@@ -2,37 +2,56 @@
 
 namespace App\Actions\Directories;
 
+use App\Helpers\PathHelpers;
 use App\Models\File;
+use App\Models\Project;
 use App\Models\User;
+use App\Traits\CopyFiles;
 
 class CopyDirectoryAction
 {
     use ChildDirs;
+    use CopyFiles;
 
-    public function execute(File $dirToCopy, File $toDir, User $user, $newName): bool
+    private array $dirsByPath = [];
+    private Project $project;
+
+    public function execute(File $dirToCopy, File $toDir, User $user): bool
     {
-        return false;
+        $this->project = Project::findOrFail($toDir->project_id);
+        return $this->copyToDir($dirToCopy, $toDir, $user);
     }
 
-    private function copyToDir(File $dirToCopy, File $toDir, User $user, $newName): bool
+    private function copyToDir(File $dirToCopy, File $toDir, User $user): bool
     {
         $dirs = $this->recursivelyRetrieveAllSubdirs($dirToCopy);
+
+        // Now that we have all subdirectories, prepend $dirToCopy so that
+        // we get the complete list of directories to copy files from
+        $dirs->prepend($dirToCopy);
         foreach ($dirs as $dir) {
-            $newDir = $this->createDir($dir, $toDir);
-            if (!$this->copyAllFilesInDir($dir, $newDir, $user, $newName)) {
+            $newDir = $this->getDirectoryOrCreateIfDoesNotExist($toDir, $dir->path, $this->project);
+            if (!$this->copyAllFilesInDir($dir, $newDir, $user)) {
                 return false;
             }
         }
         return true;
     }
 
-    private function copyAllFilesInDir(File $fromDir, File $toDir, User $user, $newName): bool
+    private function copyAllFilesInDir(File $fromDir, File $toDir, User $user): bool
     {
-        return false;
-    }
-
-    private function createDir(File $dir, File $toDir): File
-    {
-        return $dir;
+        echo "copyAllFilesInDir {$fromDir->path}, {$toDir->path}\n";
+        $cursor = File::query()
+                      ->where('directory_id', $fromDir)
+                      ->where('mime_type', '<>', 'directory')
+                      ->where('current', true)
+                      ->whereNull('deleted_at')
+                      ->whereNull('dataset_id')
+                      ->cursor();
+        foreach($cursor as $file) {
+            echo "Copying: {$file->name}\n";
+            $this->importFileIntoDir($toDir, $file);
+        }
+        return true;
     }
 }
--- tests/Feature/Actions/Directories/CopyDirectoryActionTest.php
+++ tests/Feature/Actions/Directories/CopyDirectoryActionTest.php
@@ -15,10 +15,24 @@ class CopyDirectoryActionTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function it_should_copy_a_directory_to_another_directory_and_all_have_correct_attributes()
+    public function it_should_copy_a_simple_directory_to_another_directory_and_all_have_correct_attributes()
     {
+        $user = User::factory()->create();
+        $project = ProjectFactory::ownedBy($user)->create();
+
+        $d1 = ProjectFactory::createDirectory($project, $project->rootDir, "d1");
+        $d1File = ProjectFactory::createFakeFile($project, $d1, "file.txt");
+
+        $destDir = ProjectFactory::createDirectory($project, $project->rootDir, "destdir");
+
         $copyDirAction = new CopyDirectoryAction();
-//        $copyDirAction->execute();
+        $this->assertTrue($copyDirAction->execute($d1, $destDir, $user));
+        $this->assertEquals(1, File::where('directory_id', $destDir->id)->count());
+    }
+
+    /** @test */
+    public function it_should_copy_a_deeper_directory_to_another_directory()
+    {
         $this->fail("not implemented");
     }
 
