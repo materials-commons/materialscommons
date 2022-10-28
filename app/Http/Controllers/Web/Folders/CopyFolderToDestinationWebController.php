c19b84b4de440950f22c09ed4ed144ca04e01592
gtarcea <gtarcea@umich.edu>
2022-10-28 15:36:20 -0400

    Change controller to submit a job to copy a folder

--- app/Http/Controllers/Web/Folders/CopyFolderToDestinationWebController.php
+++ app/Http/Controllers/Web/Folders/CopyFolderToDestinationWebController.php
@@ -4,20 +4,19 @@ namespace App\Http\Controllers\Web\Folders;
 
 use App\Actions\Directories\CopyDirectoryAction;
 use App\Http\Controllers\Controller;
+use App\Jobs\Folders\CopyFolderJob;
 use App\Models\File;
 use App\Models\Project;
-use Illuminate\Http\Request;
 
 class CopyFolderToDestinationWebController extends Controller
 {
     public function __invoke(CopyDirectoryAction $copyDirectoryAction, Project $project, File $fromFolder,
                              File                $toFolder)
     {
-        // run in the background
-        $copyDirectoryAction->execute($fromFolder, $toFolder, auth()->user());
-
+        // Start copy in the background and then return to the folder that contains the folder we are copying. Let
+        // the user know the copy will be done in the background.
+        CopyFolderJob::dispatch($fromFolder, $toFolder, auth()->user())->onQueue('globus');
         flash("Copying directory will take place in the background")->info();
-        // Start move in the background and then return to the folder that contains the folder we are copying.
         return redirect(route('projects.folders.show', [$fromFolder->project_id, $fromFolder->directory_id]));
     }
 }
