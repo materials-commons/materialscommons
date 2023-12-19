c3d1f4551d7070280f91bb2007841edc6835d464
gtarcea <gtarcea@umich.edu>
2023-12-19 16:00:07 -0500

    Start laying out the new query interface for samples/computations

--- resources/views/partials/entities/_entities-with-used-activities-table.blade.php
+++ resources/views/partials/entities/_entities-with-used-activities-table.blade.php
@@ -1,26 +1,58 @@
-@if(Request::routeIs('projects.entities.*'))
-    <div id="mql-query-builder" x-data="initMQLBuilder()">
-        <div id="open-query-builder" x-show="!showBuilder">
-            <a href="#" @click="toggleShowBuilder()">Open Query Builder</a>
-            <p>
-                Query for matching samples by process type and attributes.
-            </p>
-        </div>
-        <div id="query-builder" style="display: none" x-show="showBuilder">
-            @include('partials.mql._query-builder')
-        </div>
-    </div>
-    <br>
+{{--@if(Request::routeIs('projects.entities.*'))--}}
+{{--    <div id="mql-query-builder" x-data="initMQLBuilder()">--}}
+{{--        <div id="open-query-builder" x-show="!showBuilder">--}}
+{{--            <a href="#" @click="toggleShowBuilder()">Open Query Builder</a>--}}
+{{--            <p>--}}
+{{--                Query for matching samples by process type and attributes.--}}
+{{--            </p>--}}
+{{--        </div>--}}
+{{--        <div id="query-builder" style="display: none" x-show="showBuilder">--}}
+{{--            @include('partials.mql._query-builder')--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--    <br>--}}
+{{--@endif--}}
+{{--<x-mql.query-builder/>--}}
+@if($category == "computational")
+    <h4>Show Computations</h4>
+@else
+    <h4>Show Samples</h4>
 @endif
-<x-mql.query-builder/>
 <div class="row">
     @if($category == "computational")
-        <span class="mr-2"><i class="fa fas fa-filter ml-2 mr-2"></i>Show Computations In:</span>
+        <span class="mr-2"><i class="fa fas fa-filter ml-2 mr-2"></i>In Activity:</span>
     @else
-        <span class="mr-2"><i class="fa fas fa-filter ml-2 mr-2"></i>Show Samples In:</span>
+        <span class="mr-2"><i class="fa fas fa-filter ml-2 mr-2"></i>In Process:</span>
     @endif
+    <select id="activities">
+        <option value=""></option>
+        @foreach($activities as $activity)
+            <option value="{{$activity->name}}">{{$activity->name}}</option>
+        @endforeach
+    </select>
     <div id="filter">
     </div>
+    @if ($category == "computational")
+        <span class="mr-2 ml-3">Having Activity Attribute:</span>
+    @else
+        <span class="mr-2 ml-3">Having Process Attribute:</span>
+    @endif
+    <select id="activity-attributes">
+        @foreach($processAttributes as $attr)
+            <option value="{{$attr->name}}">{{$attr->name}}</option>
+        @endforeach
+    </select>
+
+    @if ($category == "computational")
+        <span class="mr-2 ml-3">Having Computation Attribute:</span>
+    @else
+        <span class="mr-2 ml-3">Having Sample Attribute:</span>
+    @endif
+    <select id="entity-attributes">
+        @foreach($sampleAttributes as $attr)
+            <option value="{{$attr->name}}">{{$attr->name}}</option>
+        @endforeach
+    </select>
 </div>
 <br/>
 <br/>
@@ -80,37 +112,21 @@
             hasExperiment = true;
         @endif
 
-        function createFilter() {
-            console.log('createFilter called');
-            let api = $('#entities-with-used-activities').DataTable();
-            let select = $('<select/>').appendTo('#filter').on('change', function () {
-                let columns = api.columns().flatten();
-                let selected = $(this).val();
-                if (selected === '') {
-                    api.search('').columns().search('').draw();
-                    return;
-                }
-                for (let i = 0; i < columns.length; i++) {
-                    let colHeader = api.column(i).header().textContent;
-                    if (selected === colHeader) {
-                        api.search('').columns().search('').draw();
-                        api.column(i).search(`^X$`, true, false).draw();
-                        break;
-                    }
-                }
-            });
-            select.append($(`<option value=""></option>`));
-            api.columns().every(function (idx) {
-                if (idx === 0) {
-                    return;
-                }
+        function setupInProcessingStep() {
 
-                if (hasExperiment && idx === 1) {
-                    return;
-                }
-                let column = this;
-                let headerText = column.header().textContent;
-                select.append($(`<option value="${headerText}">${headerText}</option>`));
+        }
+
+        function setupHavingEntityAttribute() {
+            let select = $('#entity-attributes').on('change', function () {
+                let value = $(this).val();
+                console.log(`entity attribute chosen ${value}`);
+            });
+        }
+
+        function setupHavingActivityAttribute() {
+            let select = $('#activity-attributes').on('change', function () {
+                let value = $(this).val();
+                console.log(`activity attribute chosen ${value}`);
             });
         }
 
@@ -118,10 +134,9 @@
             $('#entities-with-used-activities').DataTable({
                 pageLength: 100,
                 scrollX: true,
-                initCompleteXXX: function () {
-                    console.log('initCompleteXXX called');
+                initComplete: function () {
                     let api = this.api();
-                    let select = $('<select/>').appendTo('#filter').on('change', function () {
+                    $('#activities').on('change', function () {
                         let columns = api.columns().flatten();
                         let selected = $(this).val();
                         if (selected === '') {
@@ -137,44 +152,24 @@
                             }
                         }
                     });
-                    select.append($(`<option value=""></option>`));
-                    this.api().columns().every(function (idx) {
-                        if (idx === 0) {
-                            return;
-                        }
-
-                        if (hasExperiment && idx === 1) {
-                            return;
-                        }
-                        let column = this;
-                        let headerText = column.header().textContent;
-                        select.append($(`<option value="${headerText}">${headerText}</option>`));
-                    });
+                    // select.append($(`<option value=""></option>`));
+                    // api.columns().every(function (idx) {
+                    //     if (idx === 0) {
+                    //         return;
+                    //     }
+                    //
+                    //     if (hasExperiment && idx === 1) {
+                    //         return;
+                    //     }
+                    //     let column = this;
+                    //     let headerText = column.header().textContent;
+                    //     select.append($(`<option value="${headerText}">${headerText}</option>`));
+                    // });
                 },
             });
 
-            createFilter();
+            setupHavingActivityAttribute();
+            setupHavingEntityAttribute();
         });
-        htmx.on('htmx:after-settle', (evt) => {
-            if (evt.target.id === "mql-query") {
-                mcutil.autosizeTextareas();
-            }
-        });
-
-        function initMQLBuilder() {
-            return {
-                showBuilder: false,
-                showSavedQueries: false,
-                toggleShowBuilder() {
-                    this.showBuilder = !this.showBuilder;
-                    if (this.showBuilder) {
-                        mcutil.autosizeTextareas();
-                    }
-                },
-                toggleShowSavedQueries() {
-                    this.showSavedQueries = !this.showSavedQueries;
-                }
-            };
-        }
     </script>
 @endpush
