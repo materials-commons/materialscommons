f67b1a14cd7578d745ea3fe8a8a67ac6161b3b6a
gtarcea <gtarcea@umich.edu>
2023-12-20 12:48:00 -0500

    Change view to include entity name for each activity/attribute

--- database/migrations/2023_12_19_211932_create_activity_attrs_by_proj_exp_view.php
+++ database/migrations/2023_12_19_211932_create_activity_attrs_by_proj_exp_view.php
@@ -11,11 +11,13 @@ return new class extends Migration {
     {
         DB::statement("
         create view activity_attrs_by_proj_exp as
-             select act.name as activity_name, act.project_id, e2a.experiment_id, a.name as attribute_name, json_extract(av.val, '$.value') as attribute_value, av.unit as attribute_unit
+             select act.name as activity_name, e.name as entity_name, act.project_id, e2a.experiment_id, a.name as attribute_name, json_extract(av.val, '$.value') as attribute_value, av.unit as attribute_unit
              from activities act
-                 join experiment2activity e2a on act.id = e2a.activity_id
-                 join attributes a on a.attributable_id = act.id and attributable_type = 'App\\\Models\\\Activity'
-                 join attribute_values av on av.attribute_id = a.id
+                join experiment2activity e2a on act.id = e2a.activity_id
+                join activity2entity a2e on act.id = a2e.activity_id
+                join entities e on a2e.entity_id = e.id
+                join attributes a on a.attributable_id = act.id and attributable_type = 'App\\\Models\\\Activity'
+                join attribute_values av on av.attribute_id = a.id
         ");
     }
 
