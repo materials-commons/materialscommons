3fb25880942e0c540edb326e19e6fd5990882d6a
gtarcea <gtarcea@umich.edu>
2024-01-23 16:12:29 -0500

    Add getting the Google Sheet title from the entered URL like in create

--- resources/views/app/projects/experiments/reload.blade.php
+++ resources/views/app/projects/experiments/reload.blade.php
@@ -38,7 +38,12 @@
                         </div>
                         <br/>
                         <label for="url-id">Reload Experiment From Google Sheet</label>
-                        <input class="form-control" name="sheet_url" type="url" placeholder="Google Sheet URL.."
+                        <input class="form-control"
+                               hx-get="{{route('projects.files.sheets.resolve-google-sheet', [$project])}}"
+                               hx-target="#google-sheet-title"
+                               hx-indicator=".htmx-indicator"
+                               hx-trigger="keyup changed delay:500ms"
+                               name="sheet_url" type="url" placeholder="Google Sheet URL.."
                                id="url-id">
                         <span class="htmx-indicator"><i class="fas fa-spinner fa-spin"></i></span>
                         <div id="google-sheet-title"></div>
