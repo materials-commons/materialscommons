9b71d79596fbcc767301b539075637fcede08a7c
gtarcea <gtarcea@umich.edu>
2024-02-22 12:25:00 -0500

    Bring ListMissingFilesCommand over from mcfs-changes, and enhance it with extra attributes to show for missing files.

---
+++ app/Console/Commands/Admin/ListMissingFilesCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands\Admin;
+
+use App\Models\File;
+use Illuminate\Console\Command;
+
+class ListMissingFilesCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'mc-admin:list-missing-files';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        ini_set("memory_limit", "4096M");
+        $missingCount = 0;
+        foreach (File::cursor() as $f) {
+            if (!$f->realFileExists()) {
+                $usesUuid = "no-uses-uuid";
+                if (!blank($f->uses_uuid)) {
+                    $usesUuid = $f->uses_uuid;
+                }
+
+                echo "{$f->name}:{$f->id}:{$f->directory_id}:{$f->project_id}:{$f->dataset_id}:{$f->mcfsPath()}:{$usesUuid}:{$f->checksum}\n";
+                $missingCount++;
+            }
+        }
+        echo "\n\nTotal Missing: {$missingCount}\n";
+    }
+}
