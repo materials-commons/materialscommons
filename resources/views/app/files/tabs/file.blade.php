723b146d0091c1f08b94012015c286b1f7fed994
gtarcea <gtarcea@umich.edu>
2024-04-02 15:48:43 -0400

    Prototype adding tags to files

--- resources/views/app/files/tabs/file.blade.php
+++ resources/views/app/files/tabs/file.blade.php
@@ -6,6 +6,33 @@
     <span class="ml-3 fs-10 grey-5">Mediatype: {{$file->mime_type}}</span>
     <span class="ml-3 fs-10 grey-5">Size: {{$file->toHumanBytes()}}</span>
 </x-show-standard-details>
+{{--<form>--}}
+{{--    <div class="form-group">--}}
+{{--        <label>Tags <a href="#" class="ml-3 action-linkx">edit</a></label>--}}
+{{--        <ul class="list-inline">--}}
+{{--            <li class="list-inline-item mt-1">--}}
+{{--                <a class="badge badge-success fs-11 td-none">--}}
+{{--                    Hello--}}
+{{--                </a>--}}
+{{--            </li>--}}
+{{--        </ul>--}}
+{{--    </div>--}}
+
+{{--    <div class="form-group">--}}
+{{--        <label for="tags">Tags</label>--}}
+{{--        <input class="form-control" id="tags" name="tags" value="">--}}
+{{--    </div>--}}
+{{--</form>--}}
 <hr>
 <br>
 @include('partials.files._display-file', [ 'displayRoute' => route('projects.files.display', [$project, $file]) ])
+
+{{--@push('scripts')--}}
+{{--    <script>--}}
+{{--        $(document).ready(() => {--}}
+{{--            console.log('here');--}}
+{{--            let tagsInput = document.querySelector('#tags');--}}
+{{--            new Tagify(tagsInput);--}}
+{{--        });--}}
+{{--    </script>--}}
+{{--@endpush--}}
