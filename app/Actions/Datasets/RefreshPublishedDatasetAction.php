b17ce05f481f575040a0fb2afa711a54b1f5eead
gtarcea <gtarcea@umich.edu>
2024-04-29 14:30:36 -0400

    Fix race condition in refresh where it unpublishes and then publishes a dataset, but since the job chains are separate, it means publish could be started before unpublish has finished.
    This was fixed by adding a flag to the unpublish job that checks to see if we are publishing, and adds publishing as a step in the unpublish chain.

--- app/Actions/Datasets/RefreshPublishedDatasetAction.php
+++ app/Actions/Datasets/RefreshPublishedDatasetAction.php
@@ -10,14 +10,6 @@ class RefreshPublishedDatasetAction
     public function execute(Dataset $dataset, User $user)
     {
         $unpublishDatasetAction = new UnpublishDatasetAction();
-        $unpublishDatasetAction($dataset, $user);
-        $this->publishDataset($dataset, $user);
-    }
-
-
-    private function publishDataset(Dataset $dataset, $user)
-    {
-        $publishAction = new PublishDatasetAction2();
-        $publishAction->execute($dataset, $user);
+        $unpublishDatasetAction($dataset, $user, true);
     }
 }
