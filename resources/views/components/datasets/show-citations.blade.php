954becc17a7d7d2f45e91d250a01510f6f90d7e4
gtarcea <gtarcea@umich.edu>
2024-07-10 17:10:27 -0400

    Prototype showing citation counts.

---
+++ resources/views/components/datasets/show-citations.blade.php
@@ -0,0 +1,7 @@
+<div class="form-group mt-2">
+    <label for="citations">Citations</label>
+    <ul class="list-unstyled" style="font-weight: normal">
+        <li>For Dataset: <a href="#">10</a></li>
+        <li>Paper - Phase field benchmark problems for nucleation: <a href="#">15</a></li>
+    </ul>
+</div>
