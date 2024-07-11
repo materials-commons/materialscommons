88515a24812424208d33577d6630f0a17e62c5f6
gtarcea <gtarcea@umich.edu>
2024-07-11 13:54:17 -0400

    Update uploads

--- resources/views/app/projects/datasets/upload-files.blade.php
+++ resources/views/app/projects/datasets/upload-files.blade.php
@@ -34,19 +34,38 @@
             dataset: "{{$dataset->id}}",
             file: "{{$directory->id}}"
         });
-        const uppy = Uppy({
+        const uppy = new Uppy({
             restrictions: {
                 maxFileSize: 250 * 1024 * 1024,
+            },
+            onBeforeFileAdded: (currentFile, files) => {
+                if (currentFile.data.webkitRelativePath === "") {
+                    return currentFile;
+                }
+
+                const modifiedFile = {
+                    ...currentFile,
+                };
+
+                modifiedFile.meta.name = currentFile.data.webkitRelativePath;
+
+                return modifiedFile;
             }
         }).use(UppyDashboard, {
             trigger: '#file-upload',
             inline: true,
             showProgressDetails: true,
             proudlyDisplayPoweredByUppy: false,
-        }).use(UppyXHRUpload, {endpoint: r});
+            fileManagerSelectionType: "both",
+        }).use(UppyXHRUpload, {endpoint: r, formData: true});
 
         uppy.on('file-added', () => {
             uppy.setMeta({_token: csrf});
+            if (f.data.webkitRelativePath === "") {
+                uppy.setFileMeta(f.id, {"relativePath": ""});
+            } else {
+                uppy.setFileMeta(f.id, {"relativePath": f.data.webkitRelativePath});
+            }
         });
     </script>
 @endpush
