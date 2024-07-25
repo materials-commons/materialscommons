02494a24eaa4f984deea6e898eba4691d20aa68c
gtarcea <gtarcea@umich.edu>
2024-07-25 11:35:41 -0400

    Use is-referenced-by-count to get the citations count.

--- resources/views/components/datasets/show-citations.blade.php
+++ resources/views/components/datasets/show-citations.blade.php
@@ -6,16 +6,22 @@
                 <table class="table table-sm table-hover ml-3">
                     <thead>
                     <tr>
-                        <th>Dataset/Paper</th>
+                        <th>Title</th>
                         <th>Citations</th>
                     </tr>
                     </thead>
                     <tbody>
                     @if(isset($citations->dataset->status))
+                        @php
+                            $refCount = 0;
+                            if (isset($citations->dataset->message->{"is-referenced-by-count"})) {
+                                $refCount = $citations->dataset->message->{"is-referenced-by-count"};
+                            }
+                        @endphp
                         <tr>
-                            <td class="font-weight-normal">Dataset citations</td>
-                            {{--                            <td><a href="{{route('public.datasets.citations.dataset', [$dataset])}}">{{count($citations->dataset->message->reference)}}</a></td>--}}
-                            <td class="font-weight-normal">{{count($citations->dataset->message->reference)}}</td>
+                            <td class="font-weight-normal">Dataset</td>
+                            {{--<td><a href="{{route('public.datasets.citations.dataset', [$dataset])}}">{{$refCount}}</a></td>--}}
+                            <td class="font-weight-normal">{{$refCount}}</td>
                         </tr>
                     @else
                         <tr>
@@ -27,10 +33,16 @@
                         @foreach($citations->papers as $paper)
                             <tr>
                                 <td class="font-weight-normal">{{$paper->message->title[0]}}</td>
-                                {{--                                <td>--}}
-                                {{--                                    <a href="{{route('public.datasets.citations.papers', ['dataset' => $dataset, 'doi' => $paper->message->DOI])}}">{{count($paper->message->reference)}}</a>--}}
-                                {{--                                </td>--}}
-                                <td class="font-weight-normal">{{count($paper->message->reference)}}</td>
+                                @php
+                                    $refCount = 0;
+                                    if (isset($paper->message->{"is-referenced-by-count"})) {
+                                        $refCount = $paper->message->{"is-referenced-by-count"};
+                                    }
+                                @endphp
+                                {{--<td>--}}
+                                {{--   <a href="{{route('public.datasets.citations.papers', ['dataset' => $dataset, 'doi' => $paper->message->DOI])}}">{{$refCount}}</a>--}}
+                                {{--</td>--}}
+                                <td class="font-weight-normal">{{$refCount}}</td>
                             </tr>
                         @endforeach
                     @endif
@@ -40,7 +52,7 @@
         </div>
     @else
         <ul class="list-unstyled" style="font-weight: normal">
-            <li>No citations found for this dataset or associated papers</li>
+            <li>No citations data found for this dataset or it's associated papers</li>
         </ul>
     @endif
 </div>
