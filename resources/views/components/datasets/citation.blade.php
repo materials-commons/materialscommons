a17c7240278a5ad4b279c160d328769eb405f892
gtarcea <gtarcea@umich.edu>
2024-08-15 16:30:40 -0400

    Fix citation formatting according to feedback.

--- resources/views/components/datasets/citation.blade.php
+++ resources/views/components/datasets/citation.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
     <div class="col-7">
-        {{$dataset->owner->name}} et al. "{{$dataset->name}}", Materials Commons, dataset
-        ({{$dataset->published_at->year}}), doi:{{Illuminate\Support\Str::of($dataset->doi)->after('doi:')->trim()}}
+        {{$dataset->owner->name}}, et al. "{{$dataset->name}}", Materials Commons, dataset
+        ({{$dataset->published_at->year}}). doi:{{Illuminate\Support\Str::of($dataset->doi)->after('doi:')->trim()}}
     </div>
 </div>
