f6db64e87686695229e454709f27e5654feae0dd
gtarcea <gtarcea@umich.edu>
2024-09-11 16:32:39 -0400

    Start creating the overview page for the data explorer

--- resources/views/app/projects/datahq/index.blade.php
+++ resources/views/app/projects/datahq/index.blade.php
@@ -17,29 +17,9 @@
         </x-slot:header>
 
         <x-slot:body>
-            <div class="row">
-                <div class="offset-2 col-8">
-                    <div class="card bg-light" style="border-color: #b3c2d9">
-                        <div class="card-body">
-                            <h5 class="card-title"><strong>Data Explorer</strong></h5>
-                            <hr/>
-                            <ul>
-                                <li>Should this page start with an overview of the data?</li>
-                                <li>Or... Should it start at a default, such as View: Samples, Select Data For:
-                                    Project?
-                                </li>
-                                <li>Or should we show help here?</li>
-                            </ul>
-                            <p class="card-text">
-                                The data explorer helps you to understand your data. Start by selecting
-                                the type of data you want to look at in the "Get" dropdown. You will be
-                                able to choose "Samples", "Computations" or "Processes". Then explore
-                                the related data. You can build our a query to look at the data
-                                in different ways, and explore the data in both table and chart formats.
-                            </p>
-                        </div>
-                    </div>
-                </div>
+            @include('app.projects.datahq.pages.tabs')
+            <div class="mt-2">
+                Put details here
             </div>
         </x-slot:body>
     </x-card>
--- resources/views/app/projects/datahq/pages/tabs.blade.php
+++ resources/views/app/projects/datahq/pages/tabs.blade.php
@@ -7,17 +7,33 @@
     </li>
 
     <li class="nav-item">
+        <a class="nav-link no-underline {{setActiveNavByName('projects.datahq.resultsxx')}}"
+           href="{{route('projects.datahq.results', [$project])}}">
+            Computations
+        </a>
+    </li>
+
+    <li class="nav-item">
         <a class="nav-link no-underline  {{setActiveNavByName('projects.datahq.index')}}"
            href="{{route('projects.datahq.index', [$project])}}">
             Processes
         </a>
     </li>
+
     <li class="nav-item">
         <a class="nav-link no-underline {{setActiveNavByName('projects.datahq.entities')}}"
            href="{{route('projects.datahq.entities', [$project])}}">
             Sample Attributes
         </a>
     </li>
+
+    <li class="nav-item">
+        <a class="nav-link no-underline {{setActiveNavByName('projects.datahq.entitiesxx')}}"
+           href="{{route('projects.datahq.entities', [$project])}}">
+            Computation Attributes
+        </a>
+    </li>
+
     <li class="nav-item">
         <a class="nav-link no-underline" href="#">
             Process Attributes
