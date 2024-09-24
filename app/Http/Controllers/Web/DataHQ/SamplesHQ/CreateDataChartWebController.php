ef57dde588921f19b9ae8668a5459e7ee62c50a3
gtarcea <gtarcea@umich.edu>
2024-09-24 15:40:51 -0400

    Rename class to what it does

--- app/Http/Controllers/Web/DataHQ/SamplesHQ/CreateDataChartWebController.php
+++ app/Http/Controllers/Web/DataHQ/SamplesHQ/CreateDataChartWebController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Project;
 use Illuminate\Http\Request;
 
-class CreateDataView extends Controller
+class CreateDataChartWebController extends Controller
 {
     /**
      * Handle the incoming request.
