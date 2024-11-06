51273609546e20b2daecfc09f4a536db9e836e63
gtarcea <gtarcea@umich.edu>
2024-11-06 14:49:16 -0500

    Start creating components and migrations for integrating livewire into datahq

---
+++ database/migrations/2024_11_04_175526_create_datahq_charts_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('datahq_charts', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')->unique();
+
+            $table->string('name');
+            $table->string('description');
+            $table->string('attributes')->nullable();
+            $table->string('chart_type')->nullable();
+            $table->string('mql')->nullable();
+
+            $table->foreignId('user_id')
+                  ->constrained()
+                  ->onDelete('cascade');
+
+            $table->foreignId('project_id')
+                  ->constrained()
+                  ->onDelete('cascade');
+
+            $table->foreignId('experiment_id')
+                ->nullable()
+                ->constrained()
+                ->onDelete('cascade');
+
+            $table->foreignId('dataset_id')
+                ->nullable()
+                ->constrained()
+                ->onDelete('cascade');
+
+            $table->datetime('shared_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('charts');
+    }
+};
