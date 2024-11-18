7803b9c5b578a90ec500e29656d8b85ab446682c
gtarcea <gtarcea@umich.edu>
2024-11-18 16:26:11 -0500

    Add support for clicking on data point in a chart and going to the sample details page.

--- resources/views/components/datahq/explorer/show-subview-chart.blade.php
+++ resources/views/components/datahq/explorer/show-subview-chart.blade.php
@@ -115,15 +115,18 @@
                             let x = [];
                             let y = [];
                             let text = [];
+                            let experimentIds = [];
                             resp.data.forEach(function (item) {
                                 x.push(item.x);
                                 y.push(item.y);
                                 text.push(item.entity);
+                                experimentIds.push(item.experiment_id);
                             });
                             this.chartData.push({
                                 x: x,
                                 y: y,
                                 text: text,
+                                experimentIds: experimentIds,
                                 mode: 'markers',
                                 type: 'scatter',
                             });
@@ -159,7 +162,15 @@
 
                         e.on('plotly_click', function (data) {
                             let point = data.points[0];
-                            window.open('https://www.google.com', '_blank');
+                            let pointIndex = point.pointIndex;
+                            let experimentId = point.data.experimentIds[pointIndex + 1];
+                            let entity = point.data.text[pointIndex + 1];
+                            let r = route('projects.experiments.entities.by-name.spread', {
+                                project: "{{$project->id}}",
+                                experiment: experimentId,
+                                name: entity,
+                            });
+                            window.open(r, '_blank');
                         });
                     },
                 }
