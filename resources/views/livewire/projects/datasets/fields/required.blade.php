09ccade243f4552e81ee4de4bd8fd6ad28855f06
gtarcea <gtarcea@umich.edu>
2025-01-24 11:44:25 -0500

    Add tags

--- resources/views/livewire/projects/datasets/fields/required.blade.php
+++ resources/views/livewire/projects/datasets/fields/required.blade.php
@@ -21,6 +21,32 @@
             <li x-sort:item>GHI</li>
         </ul>
 
+        <div class="form-group">
+            <label for="tags">Tags</label>
+            <input class="form-control" id="tags" name="tags" value="tag1, tag2">
+        </div>
+
         <button type="submit" class="btn btn-primary">Save</button>
     </form>
 </div>
+
+@script
+<script>
+    (function () {
+        let tagify;
+
+        document.addEventListener('livewire:navigating', () => {
+            console.log('wire navigating');
+            if (tagify) {
+                console.log('    destroying tagify');
+                tagify.destroy();
+            }
+        });
+
+        document.addEventListener('livewire:navigated', () => {
+            let tagsInput = document.querySelector('#tags');
+            tagify = new Tagify(tagsInput);
+        }, {once: true})
+    })();
+</script>
+@endscript
