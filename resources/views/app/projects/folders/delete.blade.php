dc81a2633f3190b692d780699625855cd654e090
gtarcea <gtarcea@umich.edu>
2025-03-28 12:23:46 -0400

    For delete dir prompt show path and remove description

--- resources/views/app/projects/folders/delete.blade.php
+++ resources/views/app/projects/folders/delete.blade.php
@@ -19,13 +19,13 @@
 
                 <div class="form-group">
                     <label for="name">Name</label>
-                    <input class="form-control" id="name" value="{{$dir->name}}" name="name" readonly>
+                    <input class="form-control" id="name" value="{{$dir->path}}" name="name" readonly>
                 </div>
-                <div class="form-group">
-                    <label for="description">Description</label>
-                    <textarea class="form-control" id="description"
-                              name="description" readonly>{{$dir->description}}</textarea>
-                </div>
+                {{--                <div class="form-group">--}}
+                {{--                    <label for="description">Description</label>--}}
+                {{--                    <textarea class="form-control" id="description"--}}
+                {{--                              name="description" readonly>{{$dir->description}}</textarea>--}}
+                {{--                </div>--}}
                 <div class="float-right">
                     <a href="{{route('projects.folders.show', [$project, $dir])}}" class="action-link mr-3">
                         Cancel
