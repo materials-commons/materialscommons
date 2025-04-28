60bac632a751444fd12a8f6902b6098991443c63
gtarcea <gtarcea@umich.edu>
2025-04-28 15:34:42 -0400

    rootDir wasn't ignoring dataset and deleted directories.

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -154,6 +154,8 @@ class Project extends Model implements Searchable
     public function rootDir()
     {
         return $this->hasOne(File::class, 'project_id')
+            ->whereNull('dataset_id')
+            ->whereNull('deleted_at')
                     ->where('path', '/');
     }
 
