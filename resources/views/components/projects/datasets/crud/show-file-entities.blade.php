4998463a5208b569171153fd68e624befdbde3e0
gtarcea <gtarcea@umich.edu>
2025-05-09 11:17:14 -0400

    Formatting for showing samples/computations associated with a file. Added links to see the details for each of the samples/computations.

--- resources/views/components/projects/datasets/crud/show-file-entities.blade.php
+++ resources/views/components/projects/datasets/crud/show-file-entities.blade.php
@@ -2,11 +2,11 @@
 <div>
     @if(!is_null($fileSamples))
         <a class="action-link ml-2" data-toggle="collapse" href="#file-samples-{{ $file->id }}" role="button"
-           aria-expanded="false" aria-controls="file-samples-{{ $file->id }}">Samples</a>
+           aria-expanded="false" aria-controls="file-samples-{{ $file->id }}">Show Samples</a>
     @endif
     @if(!is_null($fileComputations))
         <a class="action-link ml-2" data-toggle="collapse" href="#file-computations-{{ $file->id }}" role="button"
-           aria-expanded="false" aria-controls="file-computations-{{ $file->id }}">Computations</a>
+           aria-expanded="false" aria-controls="file-computations-{{ $file->id }}">Show Computations</a>
     @endif
     <div class="row">
         @if(!is_null($fileSamples))
@@ -15,7 +15,10 @@
                     <div class="card card-body">
                         <h5>Samples</h5>
                         @foreach($fileSamples as $sample)
-                            {{$sample['entity_name']}}
+                            <a class="ml-2"
+                               href="{{route('projects.entities.show-spread', [$file->project_id, $sample['entity_id']])}}">
+                                {{$sample['entity_name']}}
+                            </a>
                         @endforeach
                     </div>
                 </div>
@@ -25,9 +28,12 @@
             <div class="col">
                 <div class="collapse multi-collapse" id="file-computations-{{ $file->id }}">
                     <div class="card card-body">
-                        <h3>Computations</h3>
+                        <h5>Computations</h5>
                         @foreach($fileComputations as $computation)
-                            {{$computation['entity_name']}}
+                            <a class="ml-2"
+                               href="{{route('projects.computations.entities.show-spread', [$file->project_id, $computation['entity_id']])}}">
+                                {{$computation['entity_name']}}
+                            </a>
                         @endforeach
                     </div>
                 </div>
