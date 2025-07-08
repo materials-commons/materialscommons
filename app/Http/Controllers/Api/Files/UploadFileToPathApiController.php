2e28a902ce4ee699d7127409db297ad8b4941e5b
gtarcea <gtarcea@umich.edu>
2025-07-08 11:26:54 -0400

    Add new api for uploading a file to a path

---
+++ app/Http/Controllers/Api/Files/UploadFileToPathApiController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api\Files;
+
+use App\Actions\Files\CreateFileAction;
+use App\Actions\Files\CreateFilesAction;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Files\FileResource;
+use App\Models\Project;
+use App\Models\ScriptTrigger;
+use App\Traits\CreateDirectories;
+use Illuminate\Http\Request;
+use function collect;
+
+class UploadFileToPathApiController extends Controller
+{
+    use CreateDirectories;
+
+    public function __invoke(Request $request, Project $project)
+    {
+        $validated = $request->validate([
+            'file' => 'required|file',
+            'path' => 'required|string',
+        ]);
+
+        $project->load('rootDir');
+
+        $triggers = ScriptTrigger::getProjectTriggers($project);
+        $path = $validated['path'];
+        $dirPath = dirname($path);
+        $fileName = basename($path);
+        $dirToUse = $this->getDirectoryOrCreateIfDoesNotExist($project->rootDir, $dirPath, $project);
+        $createFileAction = new CreateFileAction($triggers);
+        $f = $createFileAction($project, $dirToUse, '', $validated['file'], $fileName);
+        return new FileResource($f);
+    }
+}
