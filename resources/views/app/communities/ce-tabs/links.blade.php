a48092907ca82bca8b4fc515a8e5ad1dfb0248a7
gtarcea <gtarcea@umich.edu>
2025-08-01 07:22:32 -0400

    fix communities

--- resources/views/app/communities/ce-tabs/links.blade.php
+++ resources/views/app/communities/ce-tabs/links.blade.php
@@ -1,4 +1,4 @@
-@component('components.card')
+@component('components.card-white')
     @slot('header')
         Recommend Practices Links
         <a class="float-right action-link mr-2" href="{{route('communities.links.create', [$community])}}">
