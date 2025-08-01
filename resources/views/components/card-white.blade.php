ca55bd5fc1e43e2823bc5e73ff44d439064b66fa
gtarcea <gtarcea@umich.edu>
2025-07-31 21:03:48 -0400

    Fix dataset files tab

--- resources/views/components/card-white.blade.php
+++ resources/views/components/card-white.blade.php
@@ -2,7 +2,7 @@
     <div class="card-header">
         <h5>{{$header}}</h5>
     </div>
-    <div class="card-body">
+    <div class="card-body" style="background-color:#ffffff">
         {{$body}}
     </div>
 </div>
