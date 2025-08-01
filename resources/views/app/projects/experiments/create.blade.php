bae785bd27fe6312c6bd1b4475a10b19efa1a9f0
gtarcea <gtarcea@umich.edu>
2025-08-01 10:42:28 -0400

    Overflow works, but cards loose their rounding

--- resources/views/app/projects/experiments/create.blade.php
+++ resources/views/app/projects/experiments/create.blade.php
@@ -7,106 +7,276 @@
 @stop
 
 @section('content')
-    @component('components.card')
-        @slot('header')
-            Create Study
-        @endslot
-
-        @slot('body')
+    <x-card>
+        <x-slot:header>Create Study</x-slot:header>
+        <x-slot:body>
+            {{--            <x-card-container>--}}
             @include('app.projects.experiments._overview')
+
             <form method="post"
                   action="{{route('projects.experiments.store', [$project, 'show-overview' => request()->input('show-overview', false)])}}"
                   id="experiment-create">
                 @csrf
-                <div class="form-group">
-                    <label for="name">Name</label>
-                    <input class="form-control" id="name" name="name" type="text" value="{{old('name')}}"
-                           placeholder="Name...">
-                </div>
-                <div class="form-group">
-                    <label for="summary">Summary</label>
-                    <input class="form-control" id="summary" name="summary" type="text" value="{{old('summary')}}"
-                           placeholder="Summary...">
-                </div>
-                <div class="form-group">
-                    <label for="description">Description</label>
-                    <textarea class="form-control" id="description" name="description" type="text"
-                              placeholder="Description...">{{old('description')}}</textarea>
-                </div>
+                <div class="row">
+                    {{-- Left Column --}}
+                    <div class="col-lg-4 col-sm-12 col-md-6">
+                        <x-inner-card>
+                            <x-slot:title>Study Details</x-slot:title>
+                            <x-slot:body>
+                                <div class="form-group mb-3">
+                                    <label for="name">Name</label>
+                                    <input class="form-control" id="name" name="name" type="text"
+                                           value="{{old('name')}}"
+                                           placeholder="Name...">
+                                </div>
+                                <div class="form-group mb-3">
+                                    <label for="summary">Summary</label>
+                                    <input class="form-control" id="summary" name="summary" type="text"
+                                           value="{{old('summary')}}"
+                                           placeholder="Summary...">
+                                </div>
+                                <div class="form-group">
+                                    <label for="description">Description</label>
+                                    <textarea class="form-control" id="description" name="description" type="text"
+                                              placeholder="Description...">{{old('description')}}</textarea>
+                                </div>
+                            </x-slot:body>
+                        </x-inner-card>
+                    </div>
 
-                <hr>
-                <div class="form-group">
-                    <p style="margin-top: 8px">
-                        Materials Commons can <b>optionally</b> import a spreadsheet to create your study. This
-                        will set up the
-                        processes, samples and files. You can have it load a spreadsheet you've uploaded to this
-                        project, or you can specify a Google Sheet.
-                    </p>
-                    <p>
-                        To see the format for the spreadsheet please read the
-                        <a href="{{makeHelpUrl("reference/spreadsheets")}}" target="_blank">documentation</a>.
-                    </p>
-
-                    <hr/>
-
-                    <div class="row">
-                        <div class="col-6" style="border-right: 2px solid black">
-                            <h4>Using Google Sheet</h4>
-                            @include('app.projects.experiments.partials._new-google-sheet')
-                            @if($sheets->count() !== 0)
-                                @include('app.projects.experiments.partials._existing-google-sheet')
-                            @endif
-                        </div>
-                        <div class="col-6">
-                            <h4><b>OR</b> Using Existing Excel File</h4>
-                            @include('app.projects.experiments.partials._existing-excel-file')
-                        </div>
-                        <br>
-                        <div class="row">
-                            <div class="col-10">
-                                <br/>
-                                <p>
-                                    <b>If loading from a Google Sheet, you must set the share permissions to "Anyone
-                                        with the link" under General Access in the share popup.</b>
+                    {{-- Right Column --}}
+                    <div class="col-lg-8 col-sm-12 col-md-6">
+                        <x-card>
+                            <x-slot:header>
+                                Spreadsheet Import Options
+                            </x-slot:header>
+                            <x-slot:body>
+                                <p class="mb-3">
+                                    Import data from a spreadsheet to automatically set up your study.
+                                    <a href="{{makeHelpUrl("reference/spreadsheets")}}" target="_blank" class="ms-2">
+                                        <i class="fas fa-book-open"></i> View Format
+                                    </a>
                                 </p>
-                                <div class="container">
-                                    <div class="row">
-                                        <div class="col-lg-6">
-                                            <div class="text-center">
-                                                <img src="{{asset('images/google-sheets-share.png')}}"
-                                                     class="img-fluid">
-                                            </div>
-                                        </div>
+
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <x-inner-card>
+                                            <x-slot:title>
+                                                <i class="fas fa-file-excel me-2"></i> Excel File
+                                            </x-slot:title>
+                                            <x-slot:body>
+                                                <label for="file_id">Select spreadsheet</label>
+                                                <select name="file_id" class="selectpicker w-100"
+                                                        data-live-search="true"
+                                                        data-style="btn-light no-tt"
+                                                        title="Select Spreadsheet">
+                                                    <option value=""></option>
+                                                    @foreach($excelFiles as $f)
+                                                        <option data-tokens="{{$f->id}}"
+                                                                value="{{$f->id}}">
+                                                            {{$f->directory->path === "/" ? "" : $f->directory->path}}
+                                                            /{{$f->name}}
+                                                        </option>
+                                                    @endforeach
+                                                </select>
+                                            </x-slot:body>
+                                        </x-inner-card>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
-                    <hr>
 
-                    <input hidden id="project_id" name="project_id" value="{{$project->id}}">
-                    <div class="float-right">
-                        <a href="{{route('projects.show', ['project' => $project->id])}}"
-                           class="action-link danger mr-3">
-                            Cancel
-                        </a>
+                                <div class="text-center my-3">
+                                    <span class="px-3 text-muted">OR</span>
+                                </div>
 
-                        <a class="action-link mr-3" href="#"
-                           onclick="document.getElementById('experiment-create').submit()">
-                            Create
-                        </a>
+                                <div class="row">
+                                    {{-- Google Sheets Option --}}
+                                    <div class="col-12">
+                                        <x-inner-card>
+                                            <x-slot:title>
+                                                <i class="fab fa-google me-2"></i> Google Sheets
+                                            </x-slot:title>
+                                            <x-slot:body>
 
-                        {{--                    <a class="action-link" href="#" onclick="createAndAddFiles()">--}}
-                        {{--                        Create And Add Files--}}
-                        {{--                    </a>--}}
+                                                <div class="mb-3">
+                                                    <label for="url-id">New Sheet URL</label>
+                                                    <div class="input-group">
+                                                        <input class="form-control"
+                                                               hx-get="{{route('projects.files.sheets.resolve-google-sheet', [$project])}}"
+                                                               hx-target="#google-sheet-title"
+                                                               hx-indicator=".htmx-indicator"
+                                                               hx-trigger="keyup changed delay:500ms"
+                                                               name="sheet_url" type="url"
+                                                               placeholder="Paste URL here..."
+                                                               id="url-id">
+                                                        <span class="htmx-indicator input-group-text"><i
+                                                                class="fas fa-spinner fa-spin"></i></span>
+                                                    </div>
+                                                    <div id="google-sheet-title" class="small mt-1"></div>
+                                                </div>
+
+                                                @if($sheets->count() !== 0)
+                                                    <div class="mt-3">
+                                                        <label for="sheet_id">Or use existing sheet</label>
+                                                        <select name="sheet_id" class="selectpicker w-100"
+                                                                data-live-search="true"
+                                                                data-style="btn-light no-tt"
+                                                                title="Select Google Sheet">
+                                                            <option value=""></option>
+                                                            @foreach($sheets as $s)
+                                                                <option data-tokens="{{$s->id}}"
+                                                                        value="{{$s->id}}">{{$s->title}}</option>
+                                                            @endforeach
+                                                        </select>
+                                                    </div>
+                                                @endif
+                                                <div class="alert alert-info mt-3">
+                                                    <div class="d-flex align-items-center">
+                                                        <div>
+                                                            <p>
+                                                                <i class="fas fa-info-circle fa-lg me-3"></i>
+                                                                <strong>Important:</strong>
+                                                                Set sharing permissions to "Anyone with the link"
+                                                                under General Access for the Google Sheet to be accessible.
+                                                            </p>
+                                                            <img src="{{asset('images/google-sheets-share.png')}}"
+                                                                 class="img-fluid mt-2 rounded shadow-sm"
+                                                                 style="max-width: 300px"
+                                                                 alt="Google Sheets sharing settings">
+                                                        </div>
+
+                                                    </div>
+                                                </div>
+                                            </x-slot:body>
+                                        </x-inner-card>
+                                    </div>
+                                </div>
+                            </x-slot:body>
+                        </x-card>
                     </div>
                 </div>
+                <input hidden id="project_id" name="project_id" value="{{$project->id}}">
+
+                <div class="float-right">
+                    <a href="{{route('projects.show', ['project' => $project->id])}}"
+                       class="action-link danger mr-3">
+                        Cancel
+                    </a>
+
+                    <a class="action-link mr-3" href="#"
+                       onclick="document.getElementById('experiment-create').submit()">
+                        Create
+                    </a>
+                </div>
             </form>
-        @endslot
-    @endcomponent
+            {{--            </x-card-container>--}}
+        </x-slot:body>
+    </x-card>
 
     @include('common.errors')
 @endsection
+
+@push('scripts')
+    <script>
+        $(document).ready(() => {
+            $('.selectpicker').selectpicker({
+                // container: 'body',
+                dropupAuto: false
+            });
+        });
+    </script>
+@endpush
+
+{{--@push('styles')--}}
+{{--    <style>--}}
+{{--        .bs-select-2 .dropdown-menu {--}}
+{{--            transform: none !important;--}}
+{{--            position: fixed !important;--}}
+{{--            z-index: 1060 !important;--}}
+{{--            min-width: 100% !important;--}}
+{{--            width: auto !important;--}}
+{{--        }--}}
+
+{{--        .bootstrap-select.show .dropdown-menu {--}}
+{{--            display: block !important;--}}
+{{--        }--}}
+
+{{--        /* Keep the border radius intact */--}}
+{{--        .card {--}}
+{{--            overflow: hidden;--}}
+{{--        }--}}
+{{--        .inner-card {--}}
+{{--            overflow: hidden;--}}
+{{--        }--}}
+{{--    </style>--}}
+{{--@endpush--}}
+
+
+{{--@push('styles')--}}
+{{--    <style>--}}
+{{--        .bs-select-2 .dropdown-menu {--}}
+{{--            z-index: 1060 !important;--}}
+{{--        }--}}
+{{--    </style>--}}
+{{--@endpush--}}
+
+{{--@push('styles')--}}
+{{--    <style>--}}
+{{--        .inner-card {--}}
+{{--            overflow: visible !important;--}}
+{{--        }--}}
+{{--        .card-body {--}}
+{{--            overflow: visible !important;--}}
+{{--        }--}}
+{{--        /*.bootstrap-select .dropdown-menu {*/--}}
+{{--        /*    z-index: 1060 !important;*/--}}
+{{--        /*    min-width: 100% !important;*/--}}
+{{--        /*    width: auto !important;*/--}}
+{{--        /*}*/--}}
+{{--    </style>--}}
+{{--@endpush--}}
+
+
+
+@push('styles')
+    <style>
+        .card {
+            overflow: visible;
+            /*clip-path: inset(0 round 0.375rem);*/
+
+        }
+        /*.card-body {*/
+        /*    overflow: visible;*/
+        /*}*/
+
+        /* Target only the immediate card-body children that contain selects */
+        /*.card-body .bootstrap-select {*/
+        /*    overflow: visible;*/
+        /*}*/
+
+        /*.card {*/
+        /*    overflow: visible;*/
+        /*}*/
+
+        /*.card .bs-select-2 {*/
+        /*    overflow: visible;*/
+        /*}*/
+
+        .bs-select-2 .dropdown-menu {
+            z-index: 1060 !important;
+            min-width: 100% !important;
+            width: auto !important;
+        }
+
+        /* Or target specific containers that need overflow */
+        /*.selectpicker-container {*/
+        /*    overflow: visible;*/
+        /*}*/
+
+    </style>
+@endpush
+
+
+
 
 {{--@push('scripts')--}}
 {{--    <script>--}}
