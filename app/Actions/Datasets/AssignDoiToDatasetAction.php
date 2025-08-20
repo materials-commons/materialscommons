3f8ab3fb725e0606dcdcf466e5bc77189b5b0d03
gtarcea <gtarcea@umich.edu>
2025-08-20 12:16:32 -0400

    Better checking for test.

--- app/Actions/Datasets/AssignDoiToDatasetAction.php
+++ app/Actions/Datasets/AssignDoiToDatasetAction.php
@@ -9,9 +9,16 @@ class AssignDoiToDatasetAction
 {
     public function __invoke(Dataset $dataset, $user, $publishAsTestDataset = false)
     {
-        if (empty($dataset->doi)) {
-            $doi = DOIHelpers::mintDOI($dataset->name, $user->name, $dataset->id, $publishAsTestDataset);
-            return tap($dataset)->update(['doi' => $doi])->fresh();
+        if ($publishAsTestDataset) {
+            if (empty($dataset->test_doi)) {
+                $doi = DOIHelpers::mintDOI($dataset->name, $user->name, $dataset->id, $publishAsTestDataset);
+                return tap($dataset)->update(['test_doi' => $doi])->fresh();
+            }
+        } else {
+            if (empty($dataset->doi)) {
+                $doi = DOIHelpers::mintDOI($dataset->name, $user->name, $dataset->id, $publishAsTestDataset);
+                return tap($dataset)->update(['doi' => $doi])->fresh();
+            }
         }
 
         return $dataset;
