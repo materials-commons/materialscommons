6dbac90d0dc71e4fa2c81c5be093ccf5deb90587
gtarcea <gtarcea@umich.edu>
2025-09-04 14:48:23 -0400

    Add checking a project and writing to a file the data for the looking for missing files command.

--- app/Console/Commands/Disk/CheckForMissingFilesCommand.php
+++ app/Console/Commands/Disk/CheckForMissingFilesCommand.php
@@ -12,7 +12,7 @@ class CheckForMissingFilesCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'mc-disk:check-for-missing-files';
+    protected $signature = 'mc-disk:check-for-missing-files {project_id? : Optional project id to check}';
 
     /**
      * The console command description.
@@ -28,9 +28,19 @@ class CheckForMissingFilesCommand extends Command
      */
     public function handle()
     {
+        ini_set("memory_limit", "4096M");
         $missing = 0;
         $projectsWithMissingFiles = [];
-        foreach (File::with(['directory'])->where('mime_type', '<>', 'directory')->cursor() as $file) {
+        $query = File::with(['directory'])->where('mime_type', '<>', 'directory');
+        $projectId = $this->argument('project_id');
+        if ($projectId) {
+            $query->where('project_id', $projectId);
+        }
+
+        $outPath = "/tmp/missing_files.txt";
+        $handle = fopen($outPath, "w");
+
+        foreach ($query->cursor() as $file) {
             if (!$file->realFileExists()) {
                 $missing++;
                 if (!array_key_exists($file->project_id, $projectsWithMissingFiles)) {
@@ -40,9 +50,15 @@ class CheckForMissingFilesCommand extends Command
                     $m++;
                     $projectsWithMissingFiles[$file->project_id] = $m;
                 }
-                echo "{$file->directory->path}/{$file->name} in project {$file->project_id} does not exist\n";
+                echo "{$file->directory->path}/{$file->name} ({$file->id}) in project {$file->project_id} does not exist\n";
+                if ($handle) {
+                    $content = "{$file->directory->path}/{$file->name}:{$file->id}:{$file->uuid}:{$file->uses_uuid}:{$file->created_at->toDateString()}:{$file->project_id}\n";
+                    fwrite($handle, $content);
+                }
             }
         }
+
+        fclose($handle);
         echo "Total missing files {$missing}\n";
         foreach ($projectsWithMissingFiles as $key => $value) {
             echo "Project {$key} missing {$value} files\n";
