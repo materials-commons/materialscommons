4408f5b90f07b350d1ebca305d28a0f27ba2ac97
gtarcea <gtarcea@umich.edu>
2025-09-04 21:16:47 -0400

    Create command to look for files by checksum

---
+++ app/Console/Commands/Disk/CheckMissingFilesByChecksumCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands\Disk;
+
+use App\Models\File;
+use Illuminate\Console\Command;
+
+class CheckMissingFilesByChecksumCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'mc-disk:check-missing-files-by-checksum-command {missing_files_file : file with missing files}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $missing_files_file = $this->argument('missing_files_file');
+        $this->info("Missing files file: {$missing_files_file}");
+        $handle = fopen($missing_files_file, "r");
+        $handle2 = fopen("/tmp/missing-permanently", "w");
+        while (($line = fgets($handle)) !== false) {
+           $line = trim($line);
+           $parts = explode(":", $line);
+           $fileId = $parts[1];
+           $f = File::with('directory')->findOrFail($fileId);
+           $files = File::where('checksum', $f->checksum)->get();
+           $foundUsingChecksum = false;
+           foreach ($files as $file) {
+               if ($file->realFileExists()) {
+                   $foundUsingChecksum = true;
+                   break;
+               }
+           }
+           if (!$foundUsingChecksum) {
+               echo "Missing permanently: {$f->directory->path}/{$f->name}:{$f->id}:{$f->project_id}\n";
+               fwrite($handle2, "{$line}\n");
+           }
+        }
+        fclose($handle);
+        fclose($handle2);
+    }
+}
