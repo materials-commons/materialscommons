06cf4fa8363d9ecca0150087fc38424472597b8e
gtarcea <gtarcea@umich.edu>
2025-11-04 11:44:34 -0500

    Remove unimplemented charts feature

--- routes/web.php
+++ routes/web.php
@@ -157,7 +157,6 @@ Route::middleware(['auth'])->prefix('app')->group(function () {
     require base_path('routes/web_routes/triggers_web.php');
     require base_path('routes/web_routes/admin_web.php');
     require base_path('routes/web_routes/datahq_web.php');
-    require base_path('routes/web_routes/charts_web.php');
     require base_path('routes/web_routes/google_sheets_web.php');
 
     Route::get('/getUsers', [UsersController::class, 'getUsers'])->name('get_users');
