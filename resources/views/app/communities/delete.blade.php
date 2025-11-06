a00138ffb839122e8e3b03253accb713d305856a
gtarcea <gtarcea@umich.edu>
2025-11-06 15:54:56 -0500

    fix cards

--- resources/views/app/communities/delete.blade.php
+++ resources/views/app/communities/delete.blade.php
@@ -7,43 +7,37 @@
 @stop
 
 @section('content')
-    @component('components.card')
-        @slot('header')
-            Delete Community {{$community->name}}
-        @endslot
+    <h3 class="text-center">Delete Community {{$community->name}}</h3>
+    <br/>
 
-        @slot('body')
-            <form method="post" action="{{route('communities.destroy', [$community])}}" id="community-delete">
-                @csrf
-                @method('delete')
-                <div class="mb-3">
-                    <label for="name">Name</label>
-                    <input class="form-control" id="name" name="name" value="{{$community->name}}" type="text"
-                           placeholder="Name..." readonly>
-                </div>
+    <form method="post" action="{{route('communities.destroy', [$community])}}" id="community-delete">
+        @csrf
+        @method('delete')
+        <div class="mb-3">
+            <label for="name">Name</label>
+            <input class="form-control" id="name" name="name" value="{{$community->name}}" type="text"
+                   placeholder="Name..." readonly>
+        </div>
 
-                <div class="mb-3">
-                    <label for="description">Description</label>
-                    <textarea class="form-control" id="description" name="description" type="text"
-                              placeholder="Description..." readonly>{{$community->description}}</textarea>
-                </div>
-                <div class="float-end">
-                    <a href="{{route('communities.index')}}" class="action-link danger me-3">
-                        Cancel
-                    </a>
-                    <a class="action-link" href="#"
-                       onclick="document.getElementById('community-delete').submit()">
-                        Delete
-                    </a>
-                </div>
+        <div class="mb-3">
+            <label for="description">Description</label>
+            <textarea class="form-control" id="description" name="description" type="text"
+                      placeholder="Description..." readonly>{{$community->description}}</textarea>
+        </div>
+        <div class="float-end">
+            <a href="{{route('communities.index')}}" class="action-link danger me-3">
+                Cancel
+            </a>
+            <a class="action-link" href="#"
+               onclick="document.getElementById('community-delete').submit()">
+                Delete
+            </a>
+        </div>
 
-                <div class="mb-3 form-check-inline">
-                    <label class="form-check-label me-2" for="public">Public?</label>
-                    <input type="checkbox" class="form-check-input" id="public"
-                           value="1" {{$community->public ? 'checked' : ''}} readonly>
-                </div>
-            </form>
-        @endslot
-    @endcomponent
-
+        <div class="mb-3 form-check-inline">
+            <label class="form-check-label me-2" for="public">Public?</label>
+            <input type="checkbox" class="form-check-input" id="public"
+                   value="1" {{$community->public ? 'checked' : ''}} readonly>
+        </div>
+    </form>
 @stop
--- resources/views/app/communities/files/edit.blade.php
+++ resources/views/app/communities/files/edit.blade.php
@@ -7,42 +7,38 @@
 @stop
 
 @section('content')
-    @component('components.card')
-        @slot('header')
-            Edit File {{$file->name}} In Community {{$community->name}}
-        @endslot
-        @slot('body')
-            <form method="post" action="{{route('communities.files.update-file', [$community, $file])}}"
-                  id="file-update">
-                @csrf
-                @method('put')
+    <h3 class="text-center">Edit File {{$file->name}} In Community {{$community->name}}</h3>
+    <br/>
 
-                <div class="mb-3">
-                    <label for="summary">Summary</label>
-                    <input class="form-control" id="summary" value="{{old('summary', $file->summary)}}"
-                           name="summary">
-                </div>
-                <div class="mb-3">
-                    <label for="description">Description</label>
-                    <textarea class="form-control" id="description" name="description" type="text"
-                              placeholder="Description..."
-                              required>{{old('description', $file->description)}}</textarea>
-                </div>
+    <form method="post" action="{{route('communities.files.update-file', [$community, $file])}}"
+          id="file-update">
+        @csrf
+        @method('put')
 
-                <div class="float-end">
-                    <a href="{{route('communities.files.show', [$community, $file])}}" class="action-link danger me-3">
-                        Cancel
-                    </a>
-                    <a class="action-link" href="#"
-                       onclick="document.getElementById('file-update').submit()">
-                        Update
-                    </a>
-                </div>
-            </form>
-            <br>
-            @include('partials.files._display-file', [
-                'displayRoute' => route('communities.files.display', [$community, $file])
-            ])
-        @endslot
-    @endcomponent
+        <div class="mb-3">
+            <label for="summary">Summary</label>
+            <input class="form-control" id="summary" value="{{old('summary', $file->summary)}}"
+                   name="summary">
+        </div>
+        <div class="mb-3">
+            <label for="description">Description</label>
+            <textarea class="form-control" id="description" name="description" type="text"
+                      placeholder="Description..."
+                      required>{{old('description', $file->description)}}</textarea>
+        </div>
+
+        <div class="float-end">
+            <a href="{{route('communities.files.show', [$community, $file])}}" class="action-link danger me-3">
+                Cancel
+            </a>
+            <a class="action-link" href="#"
+               onclick="document.getElementById('file-update').submit()">
+                Update
+            </a>
+        </div>
+    </form>
+    <br>
+    @include('partials.files._display-file', [
+        'displayRoute' => route('communities.files.display', [$community, $file])
+    ])
 @stop
