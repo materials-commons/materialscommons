3ccfdf35bfbfe77e1c842eaa5d65e2af45c2664a
gtarcea <gtarcea@umich.edu>
2025-12-17 09:39:24 -0500

    WIP - Create remote clients and their transfer requests migrations and model

---
+++ app/Models/RemoteClients.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RemoteClients extends Model
+{
+    /** @use HasFactory<\Database\Factories\RemoteClientsFactory> */
+    use HasFactory;
+}
---
+++ database/migrations/2025_12_17_141443_create_remote_clients_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('remote_clients', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')->unique();
+            $table->string('client_id')->unique();
+            $table->string('name');
+            $table->string('hostname');
+            $table->unsignedBigInteger('owner_id');
+            $table->foreign('owner_id')
+                  ->references('id')
+                  ->on('users')
+                  ->onDelete('cascade');
+            $table->datetime('last_seen')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('remote_clients');
+    }
+};
