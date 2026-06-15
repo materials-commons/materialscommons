40746b819d6e7df3d3480b1952ef647b0612d43e
gtarcea <gtarcea@umich.edu>
2026-06-15 15:05:38 -0400

    Create a top level component for the research overview for a project

--- resources/views/app/projects/tabs/research-overview.blade.php
+++ resources/views/app/projects/tabs/research-overview.blade.php
@@ -1,17 +1,1 @@
-@php
-    $projectDashboardTabKey = 'mc_project_dashboard_tab_' . $project->id;
-@endphp
-
-<x-projects.research-overview.header :project="$project"/>
-
-<x-projects.research-overview.kpi :project="$project"/>
-
-<x-projects.research-overview.needs-attention :project="$project" :readme="$readme"/>
-
-<x-projects.research-overview.analytics.overview :project="$project"/>
-
-<x-projects.research-overview.tabs :project="$project"/>
-
-<x-projects.research-overview.tab-content :project="$project" :readme="$readme"/>
-
-<x-projects.research-overview.tab-persistence :project="$project" :tab-key="$projectDashboardTabKey"/>
+<x-projects.research-overview.index :project="$project" :readme="$readme"/>
